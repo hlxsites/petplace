19f8c1a2e1583a2ba832af744391466d84b71429
Julien Ramboz <ramboz@users.noreply.github.com>
2023-05-05 09:35:44 -0700

    feat: add sub categories navigation to the article index pages (#52)
    Please always provide the [GitHub issue(s)](../issues) your PR is for,
    as well as test URLs where your change can be observed (before and
    after):
    
    Fix #26
    
    Test URLs:
    - Before:
    https://main--petplace--hlxsites.hlx.page/article/category/pet-care/
    - After:
    https://issue-26--petplace--hlxsites.hlx.page/article/category/pet-care/

---
+++ blocks/sub-categories/sub-categories.js
@@ -0,0 +1,33 @@
+export default async function decorate(block) {
+  const resp = await fetch('/categories.json');
+  if (!resp.ok) {
+    block.remove();
+    return;
+  }
+
+  const categories = await resp.json();
+  const childCategories = categories.data.filter((c) => c['Parent Path'] === window.location.pathname);
+  if (!childCategories.length) {
+    block.remove();
+    return;
+  }
+
+  block.innerHTML = '';
+
+  const heading = document.createElement('h2');
+  heading.textContent = 'Sub categories';
+  block.append(heading);
+  childCategories.forEach((c) => {
+    const hasDescendants = categories.data.find((d) => d['Parent Path'] === c.Path);
+    const p = document.createElement('p');
+    const link = document.createElement('a');
+    if (hasDescendants) {
+      link.classList.add('has-children');
+    }
+    link.textContent = c.Category;
+    link.href = c.Path;
+    link.style.backgroundColor = c.Color;
+    p.append(link);
+    block.append(p);
+  });
+}
