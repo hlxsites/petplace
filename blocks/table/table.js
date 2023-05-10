ffb6e6200dfadf9e64bab78701b15721a481c624
Julien Ramboz <ramboz@adobe.com>
2023-05-09 18:40:07 -0700

    feat: add basic table styles

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -17,8 +17,8 @@ export default function decorate(block) {
     [...firstRow.children].forEach((td) => {
       const th = document.createElement('th');
       th.innerHTML = td.firstElementChild.innerHTML;
-      th.setAttribute('colspan', td.getAttribute('colspan'));
-      th.setAttribute('rowspan', td.getAttribute('rowspan'));
+      th.setAttribute('colspan', td.getAttribute('colspan') || '');
+      th.setAttribute('rowspan', td.getAttribute('rowspan') || '');
       td.replaceWith(th);
     });
     const thead = document.createElement('thead');
