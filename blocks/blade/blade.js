bf314fe87fe0599ec0f2470b4e57e16e61fdd30d
Julien Ramboz <ramboz@adobe.com>
2023-05-11 14:19:02 -0700

    feat: add blade block

---
+++ blocks/blade/blade.js
@@ -0,0 +1,11 @@
+export default function decorate(block) {
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'blade-image';
+      } else {
+        div.className = 'blade-body';
+      }
+    });
+  });
+}
