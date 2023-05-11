8766d079ed897dee73481a047a1798e26ad9d396
Julien Ramboz <ramboz@adobe.com>
2023-05-11 14:59:38 -0700

    fix: create optimized image rendition for hero

--- blocks/blade/blade.js
+++ blocks/blade/blade.js
@@ -6,7 +6,11 @@ export default function decorate(block) {
       if (div.children.length === 1 && div.querySelector('picture')) {
         div.className = 'blade-image';
         const img = div.querySelector('img');
+        const { width, height } = img;
         img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]));
+        const optimizedImg = div.querySelector('img');
+        optimizedImg.setAttribute('width', 750);
+        optimizedImg.setAttribute('height', Math.round((750 / width) * height));
       } else {
         div.className = 'blade-body';
       }
