ff3730d24d0b54119f2c12c0a3e546c94626f701
Julien Ramboz <ramboz@adobe.com>
2023-05-11 14:49:22 -0700

    fix: create optimized image rendition

--- blocks/blade/blade.js
+++ blocks/blade/blade.js
@@ -1,8 +1,12 @@
+import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
+
 export default function decorate(block) {
   [...block.children].forEach((row) => {
     [...row.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) {
         div.className = 'blade-image';
+        const img = div.querySelector('img');
+        img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]));
       } else {
         div.className = 'blade-body';
       }
