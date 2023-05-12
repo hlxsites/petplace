f9c2a3134964b6fe89e9d53cedb540882d71bed7
Julien Ramboz <ramboz@adobe.com>
2023-05-11 18:17:49 -0700

    feat: article author block
    fix #42

--- blocks/article-author/article-author.js
+++ blocks/article-author/article-author.js
@@ -1,3 +1,16 @@
+import { decorateIcons, getMetadata } from '../../scripts/lib-franklin.js';
+
 export default function decorate(block) {
-  block.innerHTML = '[Article Author Placeholder]';
+  const author = getMetadata('author');
+  const date = getMetadata('publication-date');
+  block.innerHTML = `
+    <span class='icon icon-user'></span>
+    <p>${author}</p>
+    <time datetime="${date}">${date}</time>`;
+  decorateIcons(block);
+  setTimeout(() => {
+    window.requestAnimationFrame(() => {
+      block.querySelector('time').textContent = new Intl.DateTimeFormat('en-US', { dateStyle: 'long' }).format(Date.now());
+    });
+  });
 }
