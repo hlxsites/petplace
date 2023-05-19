1c27269f030fdfc46cc97825bfe49f5b09c29c4b
Travis Robison <trobison@adobe.com>
2023-05-19 14:34:04 -0600

    styles and logic for filtering and pagination

--- blocks/type-filters/type-filters.js
+++ blocks/type-filters/type-filters.js
@@ -37,19 +37,160 @@ const dogData = [
     image: 'https://www.petplace.com/static/5e1e255b1f8b3d67089f9a4202630e5c/5f007/shutterstock_757757533.jpg',
     type: 'N/A',
   },
-  // {
-  //   title: 'Labradoodle',
-  //   path: '/article/breed/pembroke-welsh-corgi',
-  //   image: '',
-  //   type: '',
-  // },
-  // {
-  //   title: 'Bloodhound',
-  //   path: '/article/breed/pembroke-welsh-corgi',
-  //   image: '',
-  //   type: '',
-  // },
+  {
+    title: 'Labradoodle',
+    path: '/article/breed/pembroke-welsh-corgi',
+    image: '',
+    type: 'Sporting',
+  },
+  {
+    title: 'Bloodhound - Sporting',
+    path: '/article/breed/pembroke-welsh-corgi',
+    image: '',
+    type: 'Sporting',
+  },
+  {
+    title: 'Bloodhound2 - Terrier',
+    path: '/article/breed/pembroke-welsh-corgi',
+    image: '',
+    type: 'Terrier',
+  }, {
+    title: 'Bloodhound3 - Non-Sporting',
+    path: '/article/breed/pembroke-welsh-corgi',
+    image: '',
+    type: 'Non-Sporting',
+  }, {
+    title: 'Bloodhound4 - Toy',
+    path: '/article/breed/pembroke-welsh-corgi',
+    image: '',
+    type: 'Toy',
+  }, {
+    title: 'Bloodhound5 - Herding',
+    path: '/article/breed/pembroke-welsh-corgi',
+    image: '',
+    type: 'Herding',
+  }, {
+    title: 'Bloodhound6 - Working',
+    path: '/article/breed/pembroke-welsh-corgi',
+    image: '',
+    type: 'Working',
+  }, {
+    title: 'Bloodhound7 - N/A',
+    path: '/article/breed/pembroke-welsh-corgi',
+    image: '',
+    type: 'N/A',
+  }, {
+    title: 'Bloodhound8 - Sporting',
+    path: '/article/breed/pembroke-welsh-corgi',
+    image: '',
+    type: 'Sporting',
+  }, {
+    title: 'Bloodhound9 - Hound',
+    path: '/article/breed/pembroke-welsh-corgi',
+    image: '',
+    type: 'Hound',
+  }, {
+    title: 'Bloodhound10 - Terrier',
+    path: '/article/breed/pembroke-welsh-corgi',
+    image: '',
+    type: 'Terrier',
+  }, {
+    title: 'Bloodhound11 - Non-Sporting',
+    path: '/article/breed/pembroke-welsh-corgi',
+    image: '',
+    type: 'Non-Sporting',
+  }, {
+    title: 'Bloodhound12 - Toy',
+    path: '/article/breed/pembroke-welsh-corgi',
+    image: '',
+    type: 'Toy',
+  }, {
+    title: 'Bloodhound13 - Herding',
+    path: '/article/breed/pembroke-welsh-corgi',
+    image: '',
+    type: 'Herding',
+  }, {
+    title: 'Bloodhound14 - Working',
+    path: '/article/breed/pembroke-welsh-corgi',
+    image: '',
+    type: 'Working',
+  }, {
+    title: 'Bloodhound15 - N/A',
+    path: '/article/breed/pembroke-welsh-corgi',
+    image: '',
+    type: 'N/A',
+  }, {
+    title: 'Bloodhound16 - Toy',
+    path: '/article/breed/pembroke-welsh-corgi',
+    image: '',
+    type: 'Toy',
+  }, {
+    title: 'Bloodhound17 - Sporting',
+    path: '/article/breed/pembroke-welsh-corgi',
+    image: '',
+    type: 'Hound',
+  },
 ];
+const filteredData = [];
+const excludedTypes = [];
+const pageLimit = 6;
+let pageIndex = 0;
+
+function buildCards(block) {
+  const cardGrid = block.querySelector('.card-grid');
+  cardGrid.innerHTML = '';
+
+  dogData
+    .filter((data) => !excludedTypes.includes(data.type))
+    .filter((data, i) => {
+      const startingPoint = pageIndex * pageLimit;
+
+      return i >= startingPoint && i < (startingPoint + pageLimit);
+    })
+
+    .forEach((data) => {
+      const item = `<div class="grid-item">
+            <div class="img-container">
+                <img src="${data.image}" alt="Dog 1">
+            </div>
+            <h3>${data.title}</h3>
+          </div>`;
+      cardGrid.innerHTML += item;
+    });
+
+  buildPagination(block);
+}
+
+function buildPagination(block) {
+  const breedFilteredData = dogData.filter((data) => !excludedTypes.includes(data.type));
+  const paginationParent = block.querySelector('.pagination');
+  paginationParent.innerHTML = '';
+
+  const paginationButtons = document.createElement('div');
+  paginationButtons.classList.add('pagination-buttons');
+
+  if (pageIndex > 0) {
+    paginationButtons.innerHTML += '<button class="prev"></button>';
+  }
+  paginationButtons.innerHTML += `Page: ${pageIndex + 1} of ${Math.ceil(breedFilteredData.length / pageLimit)}`;
+
+  if (pageIndex * pageLimit < breedFilteredData.length - pageLimit) {
+    paginationButtons.innerHTML += '<button class="next"></button>';
+  }
+
+  paginationParent.append(paginationButtons);
+
+  [...block.querySelectorAll('.pagination-buttons button')].forEach((elem) => {
+    elem.addEventListener('click', (ev) => {
+      if (ev.target.classList.contains('next')) {
+        pageIndex += 1;
+      } else {
+        pageIndex -= 1;
+      }
+      buildCards(block);
+    });
+  });
+}
 
 export default async function decorate(block) {
   block.innerHTML = `
@@ -59,7 +200,7 @@ export default async function decorate(block) {
           <h3>
             Type <span class="icon icon-chevron"></span>
           </h3>
-          <div class="filter-type is-active ">
+          <div class="filter-type is-active">
             <div class="checkbox-wrapper"><input type="checkbox" checked="" name="Sporting"><span><label for="Sporting">Sporting</label></span></div>
             <div class="checkbox-wrapper"><input type="checkbox" checked="" name="Hound"><span><label for="Hound">Hound</label></span></div>
             <div class="checkbox-wrapper"><input type="checkbox" checked="" name="Terrier"><span><label for="Terrier">Terrier</label></span></div>
@@ -73,11 +214,13 @@ export default async function decorate(block) {
       </div>
       <div class="card-container">
         <div class="card-grid"></div>
-         <div class="pagination"><div class="pagination-buttons">Page: 1 of 12<button></button></div></div>
+        <div class="pagination"></div>
+       </div>
       </div>
 
 `;
 
+  // Add click listener for the mobile filter button
   block.querySelector('.filter-btn').addEventListener('click', () => {
     const categoryFilters = block.querySelector('.category-filters');
 
@@ -89,6 +232,7 @@ export default async function decorate(block) {
   });
 
   const chevron = block.querySelector('.icon-chevron');
+  // Add click listener for the category  filter chevron button
   chevron.addEventListener('click', () => {
     const filterType = block.querySelector('.filter-type');
 
@@ -101,16 +245,24 @@ export default async function decorate(block) {
     }
   });
 
-  const cardGrid = block.querySelector('.card-grid');
+  // Add click listener for all the filter checkboxes.
+  [...block.querySelectorAll('.filter-type input')].forEach((elem) => {
+    elem.addEventListener('click', (ev) => {
+      const { target } = ev;
+      const { name } = ev.target;
+      if (target.checked) {
+        const i = excludedTypes.indexOf(name);
+        if (i > -1) {
+          excludedTypes.splice(i, 1);
+        }
+      } else {
+        excludedTypes.push(name);
+      }
 
-  dogData.forEach((data) => {
-    const item = `<div class="grid-item">
-            <div class="img-container">
-                <img src="${data.image}" alt="Dog 1">
-            </div>
-            <h3>${data.title}</h3>
-          </div>`;
-    cardGrid.innerHTML += item;
+      buildCards(block);
+    });
   });
+
+  buildCards(block);
   decorateIcons(block);
 }
