7a987e735fd8a89d9a22bab74c6107b1f4b7a36e
Travis Robison <trobison@adobe.com>
2023-05-24 14:21:26 -0600

    logic cleanup

--- blocks/paws-up/paws-up.js
+++ blocks/paws-up/paws-up.js
@@ -1,19 +1,21 @@
 import { decorateIcons } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
+  const count = 0;
+
   const form = `
   <div class="form-container"><span>How can we improve this article?</span>
-    <form action="#" data-dashlane-rid="8dbbc1903519762c" data-form-type="contact"><textarea name="feedback"
-            placeholder="Type your comment" data-dashlane-rid="184aee2caa5ea3ad"
-            data-form-type="other"></textarea><button type="submit" data-dashlane-label="true"
-            data-dashlane-rid="be3d6a500264ea13" data-form-type="action">SEND</button></form>
+    <form action="#"  data-form-type="contact">
+        <textarea name="feedback"
+            placeholder="Type your comment" data-form-type="other"></textarea>
+        <button type="submit" data-form-type="action">SEND</button></form>
   </div>`;
 
   block.innerHTML = `  
     <div class="like-post-container">
       <div class="counter">
         <span class="icon icon-paw" ></span>
-        <p>0 paws up</p>
+        <p>${count} ${count === 1 ? 'paw' : 'paws'} up</p>
       </div>
       <div class="paw-btn-container">
         <span>Was this article helpful?</span>
@@ -23,13 +25,12 @@ export default function decorate(block) {
       <img class="email-pets" src="/images/cat-dog.png" alt="email">
       <div class="sign-up-wrapper">
         <p>Get the best of PetPlace straight to your inbox.</p>
-        <div id="nav-digioh-btn"></div>
         <p>By signing up, you agree to our Terms of Service and Privacy Policy.</p>
       </div>
     </div> 
     `;
   block.querySelector('.btn-yes').addEventListener('click', () => {
-    alert('hi');
+    // Todo - Add logic
   });
 
   block.querySelector('.btn-no').addEventListener('click', () => {
