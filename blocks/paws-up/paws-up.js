da0b503572517d4d780ec7b530c6cef59e982ca2
Travis Robison <travrob1@gmail.com>
2023-05-25 13:04:14 -0600

    Issue 98 (#111)
    Paws Up (Articles LIked) Block
    Fix #98 
    
    This works is for the initial styles and some minor logic. A follow up
    story
    
    https://github.com/orgs/hlxsites/projects/5/views/1?pane=issue&itemId=29068272
    will be for doing that work once we have clearer details from customer.
    
    Test URLs:
    - Before:
    https://main--petplace--hlxsites.hlx.page/article/general/pet-care/best-ways-to-make-a-pet-first-aid-kit
    - After:
    https://issue-98--petplace--hlxsites.hlx.page/article/general/pet-care/best-ways-to-make-a-pet-first-aid-kit
    
    
    <img width="323" alt="image"
    src="https://github.com/hlxsites/petplace/assets/4155964/add49172-f9f2-4bc3-a40e-549838965ca1">
    
    ---------
    
    Co-authored-by: Travis Robison <trobison@adobe.com>

--- blocks/paws-up/paws-up.js
+++ blocks/paws-up/paws-up.js
@@ -1,3 +1,44 @@
+import { createOptimizedPicture, decorateIcons } from '../../scripts/lib-franklin.js';
+
 export default function decorate(block) {
-  block.innerHTML = '[Paws Up Placeholder]';
+  const count = 0;
+
+  const form = `
+  <div class="form-container"><span>How can we improve this article?</span>
+    <form action="#"  data-form-type="contact">
+        <textarea name="feedback"
+            placeholder="Type your comment" data-form-type="other"></textarea>
+        <button type="submit" data-form-type="action">SEND</button></form>
+  </div>`;
+
+  block.innerHTML = `  
+    <div class="like-post-container">
+      <div class="counter">
+        <span class="icon icon-paw" ></span>
+        <p>${count} ${count === 1 ? 'paw' : 'paws'} up</p>
+      </div>
+      <div class="paw-btn-container">
+        <span>Was this article helpful?</span>
+        <span class="paw-btn btn-yes">Yes</span>
+        <span class="paw-btn btn-no">No</span>
+      </div>
+      <span class="email-pets"></span>
+      <div class="sign-up-wrapper">
+        <p>Get the best of PetPlace straight to your inbox.</p>
+        <p>By signing up, you agree to our Terms of Service and Privacy Policy.</p>
+      </div>
+    </div> 
+    `;
+  const optimizedPic = createOptimizedPicture('/images/cat-dog.png', 'email');
+  block.querySelector('.email-pets').innerHTML = optimizedPic.outerHTML;
+
+  block.querySelector('.btn-yes').addEventListener('click', () => {
+    // Todo - Add logic
+  });
+
+  block.querySelector('.btn-no').addEventListener('click', () => {
+    block.querySelector('.paw-btn-container').outerHTML = form;
+  });
+
+  decorateIcons(block);
 }
