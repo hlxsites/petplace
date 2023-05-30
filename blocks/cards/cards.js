b79432807dedad061d1dba33bf9cdaaf051cdc04
Julien Ramboz <ramboz@adobe.com>
2023-05-30 14:36:51 -0700

    fix: merge conflicts

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -73,17 +73,6 @@ async function createCard(row) {
   return li;
 }
 
-function createCard(row) {
-  const li = document.createElement('li');
-  li.innerHTML = row.innerHTML;
-  [...li.children].forEach((div) => {
-    if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-    else div.className = 'cards-card-body';
-  });
-  li.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  return li;
-}
-
 export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach(async (row) => {
