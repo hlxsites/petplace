6f3d0c6a52fc836802d7d7447e8ae38d5be53e92
Julien Ramboz <ramboz@adobe.com>
2023-05-30 16:40:30 -0700

    chore: remove dead code

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,5 +1,5 @@
 import { getCategories } from '../../scripts/scripts.js';
-import { createOptimizedPicture, decorateIcons, toClassName } from '../../scripts/lib-franklin.js';
+import { createOptimizedPicture, toClassName } from '../../scripts/lib-franklin.js';
 
 async function buildPost(post) {
   const categories = await getCategories();
@@ -40,43 +40,10 @@ async function buildPost(post) {
   return postCard;
 }
 
-// async function buildBlogFeed(ul, pageNum, pagesElem) {
-//   const limit = 18;
-//   const offset = pageNum * limit;
-//   let morePages = false;
-//   const categoriesData = await fetchCategoryData();
-//   const category = [];
-//   categoriesData.forEach((element) => category.push(element.Category));
-//   const blogPosts = ffetch('/article/query-index.json').sheet('article')
-//     .filter((p) => (p.path.startsWith('/article/') && compareCategory(category, p.category)))
-//     .slice(offset, offset + limit + 1);
-//   let i = 0;
-//   const newUl = document.createElement('ul');
-//   // eslint-disable-next-line no-restricted-syntax
-//   for await (const post of blogPosts) {
-//     if (i >= limit) {
-//       // skip render, but know we have more page
-//       morePages = true;
-//       break;
-//     }
-//     const li = document.createElement('li');
-//     li.append(buildPost(post, i < 1, categoriesData));
-//     newUl.append(li);
-//     i += 1;
-//   }
-//   await decorateIcons(newUl);
-//   ul.innerHTML = newUl.innerHTML;
-// }
-
 async function createCard(row) {
   const li = document.createElement('li');
   const post = JSON.parse(row.dataset.json);
   li.append(await buildPost(post));
-  // [...li.children].forEach((div) => {
-  //   if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-  //   else div.className = 'cards-card-body';
-  // });
-  // li.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   return li;
 }
 
@@ -89,29 +56,6 @@ export default function decorate(block) {
   });
   block.innerHTML = '';
   block.append(ul);
-  // if (block.classList.contains('article')) {
-  //   // artcile cards block
-  //   block.append(ul);
-  //   const usp = new URLSearchParams(window.location.search);
-  //   const page = usp.get('page');
-  //   const pageNum = Number(!page ? '0' : page - 1);
-  //   buildBlogFeed(ul, pageNum, pagesElem);
-  // } else {
-  //   // default cards block
-  //   [...block.children].forEach((row) => {
-  //     const li = document.createElement('li');
-  //     li.innerHTML = row.innerHTML;
-  //     [...li.children].forEach((div) => {
-  //       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-  //       else div.className = 'cards-card-body';
-  //     });
-  //     ul.append(li);
-  //   });
-  //   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  //   block.textContent = '';
-  //   block.append(ul);
-  // }
-
   const observer = new MutationObserver((entries) => {
     entries.forEach((entry) => {
       entry.addedNodes.forEach(async (div) => {
