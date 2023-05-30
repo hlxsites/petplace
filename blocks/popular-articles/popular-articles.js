0b23462abb67fdf007bf2360cc4489a46f7f002b
Travis Robison <trobison@adobe.com>
2023-05-30 12:31:06 -0600

    fetching page data for popular posts

--- blocks/popular-articles/popular-articles.js
+++ blocks/popular-articles/popular-articles.js
@@ -1,6 +1,40 @@
-export default async function decorate(block) {
-  block.innerHTML = '<h2>Popular Posts</h2>';
+import { createOptimizedPicture, toClassName } from '../../scripts/lib-franklin.js';
+import { getCategory } from '../../scripts/scripts.js';
 
+async function getPopularPosts() {
+  const res = await fetch('/popular-posts');
+  const html = await res.text();
+  const tempElement = document.createElement('div');
+  tempElement.innerHTML = html;
+
+  // Get the content within the <main> tag
+  const popularPosts = tempElement.querySelector('.popularposts');
+  const foo = [...popularPosts.children].map(async (child) => {
+    const url = new URL(child.textContent.trim());
+    const res = await fetch(url.pathname);
+    const html = await res.text();
+
+    // Create a temporary element to extract the content within the <main> tag
+    const tempElement = document.createElement('div');
+    tempElement.innerHTML = html;
+
+    const catSlug = tempElement.querySelector('meta[name="category"]').content;
+    const catData = await getCategory(toClassName(catSlug));
+
+    return {
+      image: tempElement.querySelector('meta[property="og:image"]').content,
+      imageAlt: tempElement.querySelector('meta[property="og:image:alt"]').content,
+      path: url.pathname,
+      title: tempElement.querySelector('h1').textContent,
+      category: catData.Category,
+      categoryPath: catData.Path,
+    };
+  });
+
+  return await Promise.all(foo);
+}
+
+async function getTopPostsFromSlideshow() {
   const res = await fetch('/');
   const html = await res.text();
 
@@ -11,24 +45,43 @@ export default async function decorate(block) {
   // Get the content within the <main> tag
   const slideShow = tempElement.querySelector('.slideshow');
 
-  const PopularPostsData = [...slideShow.children].map((child) => ({
-    img: child.querySelector('picture').outerHTML,
-    url: child.querySelector('a').href,
+  const data = [...slideShow.children].map((child) => ({
+    image: child.querySelector('picture').outerHTML,
+    imageAlt: 'foo',
+    path: child.querySelector('a').href,
     title: child.querySelector('h1').textContent,
+    category: '[Placeholder]',
+    categoryPath: catData.Path,
   }));
-  PopularPostsData.splice(3, PopularPostsData.length);
 
-  console.log(PopularPostsData);
+  data.splice(3, data.length);
+
+  return data;
+}
+
+export default async function decorate(block) {
+  block.innerHTML = '<h2>Popular Posts</h2>';
+
+  let PopularPostsData = await getPopularPosts();
+  if (!PopularPostsData) {
+    PopularPostsData = await getTopPostsFromSlideshow;
+  }
 
   const cardWrapper = document.createElement('div');
   cardWrapper.classList.add('popular-cards-wrapper');
 
   PopularPostsData.forEach((post) => {
     const foo = `
-        <a href=" ${post.url}">
-            <div class="img-div">${post.img}</div>
-            <div class="title-div"><h3>${post.title}</h3></div>
-          </a>`;
+      <div class="popular-posts-wrapper">
+        <a href=" ${post.path}">
+            <div class="img-div">${createOptimizedPicture(post.image, post.imageAlt).outerHTML}</div>
+        </a>
+        <div class="title-div">
+            <a href="${post.categoryPath}">${post.category}</a>
+            <a href=" ${post.path}"><h3>${post.title}</h3></a>
+        </div>
+      </div>          
+`;
 
     cardWrapper.innerHTML += foo;
   });
