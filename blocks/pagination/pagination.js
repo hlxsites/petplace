a62430136d6f0744b6cc9e0b828999b93643ce55
Travis Robison <travrob1@gmail.com>
2023-05-31 13:55:39 -0600

    Issue 33 - Breed Cards and Filters (#101)
    Breed Cards, filters, and pagination
    
    <img width="592" alt="Screen Shot 2023-05-19 at 3 23 04 PM"
    src="https://github.com/hlxsites/petplace/assets/4155964/52350beb-15e7-4dbd-bc07-d3e5a5d90808">
    
    <img width="1424" alt="Screen Shot 2023-05-19 at 3 23 47 PM"
    src="https://github.com/hlxsites/petplace/assets/4155964/98bed6e1-59c0-4bbf-a753-f459f8511e69">
    
    Fix #33 
    
    Test URLs:
    - Before: https://main--petplace--hlxsites.hlx.page/article/breed/
    - After: https://issue-33--petplace--hlxsites.hlx.page/article/breed/
    
    
    Built the page and logic with hard coded test data. Now calling the
    query-index and getting back a single breed... for now
    
    ---------
    
    Co-authored-by: Travis Robison <trobison@adobe.com>
    Co-authored-by: Julien Ramboz <ramboz@adobe.com>
    Co-authored-by: Mark Frisbey <5108740+mfrisbey@users.noreply.github.com>

--- blocks/pagination/pagination.js
+++ blocks/pagination/pagination.js
@@ -1,6 +1,8 @@
+import { decorateIcons } from '../../scripts/lib-franklin.js';
+
 function renderContent(block) {
   const usp = new URLSearchParams(window.location.search);
-  const limit = Number(usp.get('limit') || 25);
+  const limit = Number(usp.get('limit') || block.dataset.limit || 25);
   const page = Number(usp.get('page') || 1);
   const cards = document.querySelector('.cards ul');
 
@@ -13,11 +15,12 @@ function renderContent(block) {
   block.innerHTML = `
     <nav aria-label="pagination">
       <ul>
-        ${page > 1 ? `<li><a href="${window.location.pathname}?${prevParams}" aria-label="Previous page"><span class="icon icon-chevron"></span></a></li>` : ''}
+        ${page > 1 ? `<li><a href="${window.location.pathname}?${prevParams}" aria-label="Previous page"><span class="icon icon-chevron-wide"></span></a></li>` : ''}
         <li><a href="#" aria-current="page" tabindex="-1">Page: ${page}${total ? ` of ${total}` : ''}</a></li>
-        ${cards?.childElementCount === limit ? `<li><a href="${window.location.pathname}?${nextParams}" aria-label="Next page"><span class="icon icon-chevron"></span></a></li>` : ''}
+        ${cards?.childElementCount === limit ? `<li><a href="${window.location.pathname}?${nextParams}" aria-label="Next page"><span class="icon icon-chevron-wide"></span></a></li>` : ''}
       </ul>
     </nav>`;
+  decorateIcons(block);
 }
 
 export default async function decorate(block) {
