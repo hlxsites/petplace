7c42e15c3b9ebab19d94e2c2542f694bfe141636
FentPams <xinyifeng94@gmail.com>
2023-06-12 23:28:36 -0700

    make anchor block generic

--- blocks/anchor/anchor.js
+++ blocks/anchor/anchor.js
@@ -1,17 +1,20 @@
 export default function decorate(block) {
+
+  // if multple rows, needed to wrap with for each loop
+  const targets = block.querySelector('.anchor>div>div').textContent.toLowerCase();
   // Select all h3 elements
-  let cities = document.querySelectorAll(".city h3");
+  let cities = document.querySelectorAll(`.${targets}`);
   // console.log(cities[0].innerText)
 
   // new div
   let newDiv = document.createElement("div");
-  newDiv.classList.add("city-anchor");
+  newDiv.classList.add("anchor-list");
 
   // append every city
-  cities.forEach((city) => {
+  cities.forEach((item) => {
       const newAnchor = document.createElement("a");
-      newAnchor.href = `#${city.id}`;
-      newAnchor.innerText = city.innerText;
+      newAnchor.href = `#${item.id}`;
+      newAnchor.innerText = item.innerText;
       newDiv.appendChild(newAnchor);
   });
 
