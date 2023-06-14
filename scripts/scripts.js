7e944e1aa1dd0c833bce30f1c223479bc868c2aa
Julien Ramboz <ramboz@users.noreply.github.com>
2023-06-14 10:35:28 -0700

    feat: redirect to 404 on missing category (#156)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -28,6 +28,16 @@ export function isMobile() {
   return window.innerWidth < 1024;
 }
 
+async function render404() {
+  const response = await fetch('/404.html');
+  const html = await response.text();
+  const doc = new DOMParser().parseFromString(html, 'text/html');
+  document.head.append(doc.querySelector('head>style'));
+  const main = document.querySelector('main');
+  main.innerHTML = doc.querySelector('main').innerHTML;
+  main.classList.add('error');
+}
+
 let categoriesPromise = null;
 async function loadCategories() {
   if (categoriesPromise) {
@@ -264,6 +274,9 @@ async function decorateTemplate(main) {
             await templateModule.loadEager(main);
           }
         } catch (error) {
+          if (error.message === '404') {
+            await render404();
+          }
           // eslint-disable-next-line no-console
           console.log(`failed to load template for ${template}`, error);
         }
