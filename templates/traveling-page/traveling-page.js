ed6b00e7ba4733afdfe6395eda7f7b4826a0b126
FentPams <xinyifeng94@gmail.com>
2023-06-14 03:07:59 -0700

    init city section grid

--- templates/traveling-page/traveling-page.js
+++ templates/traveling-page/traveling-page.js
@@ -1,21 +1,55 @@
-import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 export async function loadEager(main) {
-  
-
-  console.log("traveling eager");
-  console.log(main);
-
- const links = document.querySelectorAll("a");
-
-for (let i = 0; i < links.length; i++) {
-  links[i].setAttribute("target", "_blank");
-}
+  const links = document.querySelectorAll("a");
+  for (let i = 0; i < links.length; i++) {
+    links[i].setAttribute("target", "_blank");
+  }
 
   return false;
 }
 
 export function loadLazy() {
   console.log("traveling load lazy");
+
+
+
+  let citySections = document.querySelectorAll('.section.city');
+  citySections.forEach((section) => {
+
+    // remove column wrapper 
+    const column1 = document.querySelector(".columns.p1");
+    column1.parentNode.replaceWith(column1);
+    const column2 = document.querySelector(".columns.p2");
+    column2.parentNode.replaceWith(column2);
+    const column3 = document.querySelector(".columns.p3");
+    column3.parentNode.replaceWith(column3);
+
+    // combine city name, tags, favors, and txt1 into one div
+    const cityName = section.querySelector(".default-content-wrapper");
+    const cityTags = section.querySelector(".icon-type-wrapper");
+    const cityFavors = section.querySelector(".link-box-wrapper");
+    const cityTxt1 = section.querySelector(".columns.p1.block>div>div:nth-child(2)");
+
+    let cityIntro = document.createElement("div");
+    cityIntro.classList.add("city-intro");
+    cityIntro.appendChild(cityName);
+    cityIntro.appendChild(cityTags);
+    cityIntro.appendChild(cityFavors);
+    cityIntro.appendChild(cityTxt1);
+
+    // insert to the top of the section
+    if (section.firstChild) {
+      section.insertBefore(cityIntro, section.firstChild);
+    } else {
+      section.appendChild(cityIntro);
+    }
+
+    // add tips block to the second div of column p3
+    const tips = document.querySelector(".section.city .tips-wrapper");
+    document.querySelector(".columns.p3.block>div>div:nth-child(2)").appendChild(tips);
+  });
+
+
+  
   
   // Select all h3 elements
   // const cities = document.querySelectorAll(".city-wrapper h3");
