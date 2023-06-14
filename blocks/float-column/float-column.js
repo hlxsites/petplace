4bd2eb9cfdaf3c78959ee248f7a9a23509ac7aa8
FentPams <xinyifeng94@gmail.com>
2023-06-13 22:57:07 -0700

    modify styles

--- blocks/float-column/float-column.js
+++ blocks/float-column/float-column.js
@@ -10,22 +10,22 @@ export default function decorate(block) {
     float_cols.forEach((col) =>{
         // for each block, decorate image and text according to their positions
         let floatItem = col.querySelectorAll('div[data-valign="middle"]');
-        floatItem.forEach(function (value, i){
-            if(isPicture(value)){
-                const direction = (i % 2 === 0 )? "left":"right";
-                value.classList.add(`float-column-image-${direction}`);
-                decorateResponsiveImages(value);
+        floatItem.forEach(function (item, index){
+            if(isPicture(item)){
+                const direction = (index % 2 === 0 )? "left":"right";
+                item.classList.add(`float-column-image-${direction}`);
+                decorateResponsiveImages(item);
             } else {
-                value.classList.add("text");
+                item.classList.add("text");
             }
-            if(i % 2 === 1){
-                if(isPicture(value)){
-                    const parentElement = value.parentNode;
-                    if(isPicture(value)){
-                        parentElement.insertBefore(floatItem[i], floatItem[i-1])
+            if(index % 2 === 1){
+                if(isPicture(item)){
+                    const parentElement = item.parentNode;
+                    if(isPicture(item)){
+                        parentElement.insertBefore(floatItem[index], floatItem[index-1])
                     }
                 }
-                const parentElement = floatItem[i-1].parentNode;
+                const parentElement = floatItem[index-1].parentNode;
                 let newDiv = document.createElement("div");
                 newDiv.classList.add("separator");
                 parentElement.append(newDiv);
