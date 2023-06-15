b8f1f0b81e868a372173a30e53d98da49883fa8e
Mark Frisbey <5108740+mfrisbey@users.noreply.github.com>
2023-06-15 17:04:32 -0600

    feat: add a11y "skip to" links (#147)
    Fix #48 
    
    Test URLs:
    - Before: https://main--petplace--hlxsites.hlx.page/
    - After: https://issue-48--petplace--hlxsites.hlx.page/
    
    This PR adds "skip to" buttons to the site. The buttons are hidden by
    default, but are revealed when they're focused. They're the first
    focusable elements on the page, so pressing the tab button will reveal
    them. Interacting with the buttons will focus the corresponding location
    in the page:
    
    * "Skip to Content" will focus the `<main>` element.
    * "Skip to Menu" will focus the first `li`'s `<a>` element in the page's
    primary navigation.
    * "Skip to Footer" will focus the `<footer>` element.
    
    All of the "Skip to" buttons are in a containing `<nav>` element, which
    is fixed, covers the entire content of the window, has a transparent
    background, and a `z-index` of -1. When anything in the `nav` is
    focused, it's brought to the front with a transparent black background;
    this is what provides the slightly darker "overlay" of the entire page
    whenever a "skip to" button is visible.
    
    ---------
    
    Co-authored-by: Julien Ramboz <ramboz@users.noreply.github.com>

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -330,12 +330,34 @@ export function decorateScreenReaderOnly(container) {
     });
 }
 
+function createA11yQuickNav(links = []) {
+  const nav = document.createElement('nav');
+  nav.setAttribute('aria-label', 'Skip to specific locations on the page');
+  nav.classList.add('a11y-quicknav', 'sr-focusable');
+  links.forEach((l) => {
+    const button = document.createElement('button');
+    button.setAttribute('aria-label', l.label);
+    button.href = `#${l.id}`;
+    button.innerHTML = l.label;
+    button.addEventListener('click', (ev) => {
+      ev.preventDefault();
+      const el = document.getElementById(ev.currentTarget.href.split('#')[1]);
+      el.setAttribute('tabindex', 0);
+      el.focus();
+      el.addEventListener('focusout', () => { el.setAttribute('tabindex', -1); }, { once: true });
+    });
+    nav.append(button);
+  });
+  document.body.prepend(nav);
+}
+
 /**
  * Decorates the main element.
  * @param {Element} main The main element
  */
 // eslint-disable-next-line import/prefer-default-export
 export async function decorateMain(main) {
+  main.id = 'main';
   loadCategories(main);
   // hopefully forward compatible button decoration
   decorateButtons(main);
@@ -399,7 +421,19 @@ async function loadLazy(doc) {
 
   await loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   await loadHeader(doc.querySelector('header'));
-  loadFooter(doc.querySelector('footer'));
+  const footer = doc.querySelector('footer');
+  footer.id = 'footer';
+  loadFooter(footer);
+
+  // identify the first item in the menu
+  const firstMenu = document.querySelector('.nav-wrapper .nav-sections ul li a');
+  firstMenu.id = 'menu';
+
+  createA11yQuickNav([
+    { id: 'main', label: 'Skip to Content' },
+    { id: 'menu', label: 'Skip to Menu' },
+    { id: 'footer', label: 'Skip to Footer' },
+  ]);
 
   addFavIcon(`${window.hlx.codeBasePath}/styles/favicon.svg`);
   sampleRUM('lazy');
