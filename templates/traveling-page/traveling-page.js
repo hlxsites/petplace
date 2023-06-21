4ea8f86c8454561778b8f0060c14ba5b04c2d9b4
FentPams <xinyifeng94@gmail.com>
2023-06-21 01:25:34 -0700

    improve performance by using Metercall and appending arranged city section instead of replace

--- templates/traveling-page/traveling-page.js
+++ templates/traveling-page/traveling-page.js
@@ -1,10 +1,93 @@
+// async function updateSection(section) {
+//   // remove column wrapper
+//   ['city-header', 'city-middle', 'city-footer'].forEach((className) => {
+//     const column = section.querySelector(`.columns.${className}`);
+//     column.parentNode.replaceWith(column);
+//   });
+
+//   // get all essential elements
+//   const elements = [
+//     '.default-content-wrapper',
+//     '.icon-type-wrapper',
+//     '.link-box-wrapper',
+//     ['.columns.city-header.block>div>div:nth-child(1)', 'city-header-img'],
+//     ['.columns.city-header.block>div>div:nth-child(2)', 'city-header-txt'],
+//     ['.columns.city-middle.block>div>div:nth-child(1)', 'city-middle-txt'],
+//     ['.columns.city-middle.block>div>div:nth-child(2)', 'city-middle-img'],
+//     ['.columns.city-footer.block>div>div:nth-child(1)', 'city-footer-img'],
+//     ['.columns.city-footer.block>div>div:nth-child(2)', 'city-footer-txt'],
+//     '.section.city .tips-wrapper',
+//   ].map((e) => {
+//     if (Array.isArray(e)) {
+//       const el = section.querySelector(e[0]);
+//       el.classList.add(e[1]);
+//       return el;
+//     }
+//     return section.querySelector(e);
+//   });
+
+//   // rearrange the elements under section city
+//   const arrangedCitySection = document.createElement('div');
+//   arrangedCitySection.classList.add('section', 'city');
+
+//   elements.forEach((e) => arrangedCitySection.appendChild(e));
+
+//   section.replaceWith(arrangedCitySection);
+
+//   // pick the best grid area layout by image2 width/height
+//   const img2 = elements[6].querySelector('img');
+//   const width = img2.getAttribute('width');
+//   const height = img2.getAttribute('height');
+//   if (width / height > 1.9) {
+//     arrangedCitySection.classList.add('backup-layout');
+//   }
+// }
+
+// export function loadEager() {
+//   const links = document.querySelectorAll('a');
+//   links.forEach((link) => link.setAttribute('target', '_blank'));
+//   return false;
+// }
+
+// export function loadLazy() {
+//   const citySections = document.querySelectorAll('.section.city');
+//   citySections.forEach((section) => section.classList.add('hidden'));
+// }
+
+// export function loadDelayed() {
+//   const citySections = document.querySelectorAll('.section.city');
+//   const promises = Array.from(citySections).map(async (section) => {
+//     await updateSection(section);
+//   });
+//   Promise.all(promises);
+// }
+
+let interval;
+const queue = [];
+export async function meterCalls(fn, wait = 200, max = 5) {
+  return new Promise((res) => {
+    if (!interval) {
+      setTimeout(() => fn.call(null));
+      interval = window.setInterval(() => {
+        queue.splice(0, max).forEach((item) => window.requestAnimationFrame(() => item.call(null)));
+        if (!queue.length) {
+          res();
+          window.clearInterval(interval);
+          interval = null; // reset the interval
+        }
+      }, wait);
+    } else {
+      queue.push(fn);
+    }
+  });
+}
+
 async function updateSection(section) {
   // remove column wrapper
   ['city-header', 'city-middle', 'city-footer'].forEach((className) => {
     const column = section.querySelector(`.columns.${className}`);
     column.parentNode.replaceWith(column);
   });
-
   // get all essential elements
   const elements = [
     '.default-content-wrapper',
@@ -25,15 +108,16 @@ async function updateSection(section) {
     }
     return section.querySelector(e);
   });
-
   // rearrange the elements under section city
   const arrangedCitySection = document.createElement('div');
-  arrangedCitySection.classList.add('section', 'city');
-
+  arrangedCitySection.classList.add('arranged', 'section', 'city');
   elements.forEach((e) => arrangedCitySection.appendChild(e));
-
-  section.replaceWith(arrangedCitySection);
-
+  // clean reduntant childs
+  // append arragned city section under orignal city section
+  while (section.firstChild) {
+    section.removeChild(section.firstChild);
+  }
+  section.appendChild(arrangedCitySection);
   // pick the best grid area layout by image2 width/height
   const img2 = elements[6].querySelector('img');
   const width = img2.getAttribute('width');
@@ -46,18 +130,9 @@ async function updateSection(section) {
 export function loadEager() {
   const links = document.querySelectorAll('a');
   links.forEach((link) => link.setAttribute('target', '_blank'));
-  return false;
 }
 
 export function loadLazy() {
   const citySections = document.querySelectorAll('.section.city');
-  citySections.forEach((section) => section.classList.add('hidden'));
-}
-
-export function loadDelayed() {
-  const citySections = document.querySelectorAll('.section.city');
-  const promises = Array.from(citySections).map(async (section) => {
-    await updateSection(section);
-  });
-  Promise.all(promises);
+  citySections.forEach((section) => meterCalls(() => updateSection(section), 100, 1));
 }
