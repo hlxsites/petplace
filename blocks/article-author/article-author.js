0b1c93ef07bff5ca6716a827d1e987bdadcf33a8
FentPams <xinyifeng94@gmail.com>
2023-07-04 00:33:17 -0700

    fix: article author should link to author page (if it exists)

--- blocks/article-author/article-author.js
+++ blocks/article-author/article-author.js
@@ -1,11 +1,15 @@
+import ffetch from '../../scripts/ffetch.js';
 import { decorateIcons, getMetadata } from '../../scripts/lib-franklin.js';
 
-export default function decorate(block) {
+export default async function decorate(block) {
   const author = getMetadata('author');
+  const authorIndex = await ffetch('/authors/author-index.json').filter((item) => item.Name === author).first();
+  const path = authorIndex?.Path;
   const date = getMetadata('publication-date');
+  const titleithLinkIfExist = path ? `<a href="${path}">${author}</a>` : author;
   block.innerHTML = `
     <span class='icon icon-user'></span>
-    <p>${author}</p>
+    <p>${titleithLinkIfExist}</p>
     <time datetime="${date}">${date}</time>`;
   decorateIcons(block);
   setTimeout(() => {
