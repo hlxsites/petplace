f9d7d803d4647b338db119067f0aa1d35819513c
Julien Ramboz <ramboz@adobe.com>
2023-07-05 09:25:35 -0700

    style: fix ads placements

--- blocks/ad/ad.js
+++ blocks/ad/ad.js
@@ -46,8 +46,10 @@ const loadedObserver = new MutationObserver((entries) => {
     if (entry.attributeName !== 'data-google-query-id') {
       return;
     }
-    entry.target.classList.remove('skeleton');
-    entry.target.style.minHeight = 0;
+    setTimeout(() => {
+      entry.target.classList.remove('skeleton');
+      entry.target.style.minHeight = 0;
+    }, 60000);
   });
 });
 
