dc3fb88aa78401d26e6a985b439eabdda01f5c6c
FentPams <xinyifeng94@gmail.com>
2023-07-05 21:36:24 -0700

    fix:using fetch instead of ffetch, and add toLowerCase to compare strings

--- blocks/article-author/article-author.js
+++ blocks/article-author/article-author.js
@@ -1,15 +1,16 @@
-import ffetch from '../../scripts/ffetch.js';
 import { decorateIcons, getMetadata } from '../../scripts/lib-franklin.js';
 
 export default async function decorate(block) {
   const author = getMetadata('author');
-  const authorIndex = await ffetch('/authors/author-index.json').filter((item) => item.Name === author).first();
-  const path = authorIndex?.Path;
+  const path = await fetch('/authors/author-index.json')
+    .then((response) => response.json())
+    .then((table) => table.data.find((row) => row.Name.toLowerCase() === author.toLowerCase()))
+    .then((row) => row?.Path);
   const date = getMetadata('publication-date');
-  const titleithLinkIfExist = path ? `<a href="${path}">${author}</a>` : author;
+  const titleWithLinkIfExist = path ? `<a href="${path}">${author}</a>` : author;
   block.innerHTML = `
     <span class='icon icon-user'></span>
-    <p>${titleithLinkIfExist}</p>
+    <p>${titleWithLinkIfExist}</p>
     <time datetime="${date}">${date}</time>`;
   decorateIcons(block);
   setTimeout(() => {
