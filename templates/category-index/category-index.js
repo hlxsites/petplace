0d1ff5606c7abd1441f38d69b1a5d9b62a7f6d3f
FentPams <xinyifeng94@gmail.com>
2023-07-13 00:25:17 -0700

    Revert "fix: clean up defult category page"
    This reverts commit 9fdb97d13c61b9c3de09262ee8bdcc24d11d145d.

--- templates/category-index/category-index.js
+++ templates/category-index/category-index.js
@@ -119,12 +119,10 @@ async function updateMetadata() {
     throw new Error(404);
   }
   const { Category, Color, Image } = category;
-  document.title = `${Category} | ${document.title}`;
+  document.title = document.title.replace(/<Category>/, Category);
   document.head.querySelector('meta[property="og:title"]').content = document.title;
   document.head.querySelector('meta[name="twitter:title"]').content = document.title;
-  const h1 = document.querySelector('h1');
-  h1.textContent = Category;
-  h1.id = toClassName(Category);
+  document.querySelector('h1').textContent = Category;
   const heroColorDiv = document.querySelector('.category-index .hero > div');
   heroColorDiv?.style.setProperty('--bg-color', `var(--color-${Color}-transparent)`);
   if (Image && heroColorDiv) {
