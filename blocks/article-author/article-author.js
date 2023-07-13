e6510e144dc14380600472c200b3753b658ccd34
FentPams <xinyifeng94@gmail.com>
2023-07-13 00:04:44 -0700

    feat: adding author avatar in articles if exists

--- blocks/article-author/article-author.js
+++ blocks/article-author/article-author.js
@@ -1,15 +1,21 @@
-import { decorateIcons, getMetadata } from '../../scripts/lib-franklin.js';
+import { createOptimizedPicture, decorateIcons, getMetadata } from '../../scripts/lib-franklin.js';
 
 export default async function decorate(block) {
-  const author = getMetadata('author');
-  const path = await fetch('/authors/author-index.json')
+  const authorTitle = getMetadata('author');
+  const author = await fetch('/authors/query-index.json')
     .then((response) => response.json())
-    .then((table) => table.data.find((row) => row.Name.toLowerCase() === author.toLowerCase()))
-    .then((row) => row?.Path);
+    .then((data) => data.data.find(
+      (item) => item.title.toLowerCase().includes(authorTitle.toLowerCase()),
+    ));
+  const path = author?.path;
+  const avatar = author?.avatar;
   const date = getMetadata('publication-date');
-  const titleWithLinkIfExist = path ? `<a href="${path}">${author}</a>` : author;
+  const titleWithLinkIfExist = path ? `<a href="${path}">${authorTitle}</a>` : authorTitle;
+  const defaultAvatarIcon = document.createElement('span');
+  defaultAvatarIcon.classList.add('icon', 'icon-user');
+  const avatarIfExist = avatar ? createOptimizedPicture(avatar, authorTitle) : defaultAvatarIcon;
   block.innerHTML = `
-    <span class='icon icon-user'></span>
+    ${avatarIfExist.outerHTML}
     <p>${titleWithLinkIfExist}</p>
     <time datetime="${date}">${date}</time>`;
   decorateIcons(block);
