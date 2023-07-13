4555a7dbfb131042d79a49c4340902353764beb9
Mark Frisbey <5108740+mfrisbey@users.noreply.github.com>
2023-07-13 15:15:21 -0600

    add sr-only to heading text

--- templates/puppy-diaries-index/puppy-diaries-index.js
+++ templates/puppy-diaries-index/puppy-diaries-index.js
@@ -1,4 +1,5 @@
 // eslint-disable-next-line import/prefer-default-export
-export async function loadEager() {
-  // nothing here
+export async function loadLazy() {
+  const h1 = document.querySelector('.puppy-diaries-index .hero h1');
+  h1.classList.add('sr-only');
 }
