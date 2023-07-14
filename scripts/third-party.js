6db1cabdfe7975f1fcb6ce4214a04bbeac68fbfd
Julien Ramboz <ramboz@adobe.com>
2023-07-14 13:43:13 -0700

    feat: instrument MS clarity in martech

--- scripts/third-party.js
+++ scripts/third-party.js
@@ -2,12 +2,19 @@
 import { isMobile, loadScript } from './scripts.js';
 
 const GTM_ID = 'GTM-WP2SGNL';
+const CLARITY_ID = 'ekxr1n6fgy';
 
 const GTM_SCRIPT = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${GTM_ID}');`;
+
+const CLARITY_SCRIPT = `(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window,document,'clarity','script','${CLARITY_ID}');`;
 
 function createInlineScript(innerHTML, parent) {
   const script = document.createElement('script');
@@ -24,4 +31,5 @@ export default function integrateMartech() {
   }
 
   createInlineScript(GTM_SCRIPT, document.body);
+  createInlineScript(CLARITY_SCRIPT, document.body);
 }
