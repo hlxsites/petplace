fee79717a2c228924ba999ccad19778e1cda8354
Julien Ramboz <ramboz@adobe.com>
2023-07-14 11:34:40 -0700

    feat: add partytown and load GA4 using it

--- scripts/third-party.js
+++ scripts/third-party.js
@@ -3,18 +3,18 @@ import { isMobile, loadScript } from './scripts.js';
 
 const GTM_ID = 'GTM-WP2SGNL';
 
-//  Load Google Tag Manager
-const fireGTM = (w, d, s, l, i) => {
-  w[l] = w[l] || [];
-  w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-  const f = d.getElementsByTagName(s)[0];
-  const j = d.createElement(s);
-  j.type = 'text/partytown';
-  const dl = l !== 'dataLayer' ? `&l=${l}` : '';
-  j.async = true;
-  j.src = `https://www.googletagmanager.com/gtm.js?id=${i}${dl}`;
-  f.parentNode.insertBefore(j, f);
-};
+const GTM_SCRIPT = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`;
+
+function createInlineScript(innerHTML, parent) {
+  const script = document.createElement('script');
+  script.type = 'text/partytown';
+  script.innerHTML = innerHTML;
+  parent.appendChild(script);
+}
 
 export default function integrateMartech() {
   // Load ads early on desktop since the impact is minimal there and
@@ -23,5 +23,5 @@ export default function integrateMartech() {
     loadScript('https://securepubads.g.doubleclick.net/tag/js/gpt.js', () => {}, { async: '' });
   }
 
-  fireGTM(window, document, 'script', 'dataLayer', GTM_ID);
+  createInlineScript(GTM_SCRIPT, document.body);
 }
