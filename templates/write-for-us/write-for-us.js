bc42443619c7cdb93d60b7e87675f73d6372fd02
FentPams <xinyifeng94@gmail.com>
2023-07-23 12:27:43 -0700

    style form section and make svg clickable

--- templates/write-for-us/write-for-us.js
+++ templates/write-for-us/write-for-us.js
@@ -2,51 +2,53 @@ export async function loadEager() {
   return false;
 }
 
+function addSVGClickListener(main) {
+  const heroWrapper = main.querySelector('.hero-wrapper');
+  const formSection = main.querySelector('.form-container');
+
+  if (heroWrapper && formSection) {
+    heroWrapper.addEventListener('click', (event) => {
+    // Only trigger if the click was in the lower half of the element
+    // This is to approximate a click on the SVG
+      if (event.offsetY > heroWrapper.offsetHeight / 1.1) {
+        formSection.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  }
+}
+
 export async function loadLazy(main) {
-  // create the hero, imgDiv, and textDiv elements
   const hero = document.createElement('div');
   const imgDiv = document.createElement('div');
   const textDiv = document.createElement('div');
 
-  // select defaultContentWrapper and heroimg
   const defaultContentWrapper = main.querySelector('.default-content-wrapper');
   const heroimg = main.querySelector('.hero-wrapper');
 
-  // add hero-container class to defaultContentWrapper's parent
   defaultContentWrapper.parentElement.classList.add('hero-container');
 
-  // add classes to new elements
   hero.className = 'hero-wrapper';
   imgDiv.className = 'img-div';
   textDiv.className = 'text-div';
 
-  // append all pictures from heroimg to imgDiv
   [...heroimg.querySelectorAll('picture')].forEach((el) => {
     imgDiv.append(el.cloneNode(true));
   });
-
-  // append imgDiv to hero
   hero.append(imgDiv);
 
-  // append first h1 element to textDiv
   textDiv.append(main.querySelector('h1'));
 
-  // append all non-empty p elements from defaultContentWrapper to textDiv
   [...defaultContentWrapper.querySelectorAll('p')].forEach((el) => {
     if (el.innerText.trim() !== '') {
       textDiv.append(el.cloneNode(true));
     }
   });
-
-  // append textDiv to hero
   hero.append(textDiv);
-
-  // replace defaultContentWrapper with hero
   defaultContentWrapper.replaceWith(hero);
-
-  // remove old hero-container
   const heroContainer = main.querySelector('.hero-container');
   if (heroContainer) {
     heroContainer.remove();
   }
+
+  addSVGClickListener(main);
 }
