06dc6fd6c48b8cbfecf957dc42b39b9bcff60cd1
FentPams <xinyifeng94@gmail.com>
2023-07-26 23:09:53 -0700

    clean code style

--- templates/write-for-us/write-for-us.js
+++ templates/write-for-us/write-for-us.js
@@ -1,7 +1,3 @@
-export async function loadEager() {
-  return false;
-}
-
 function addSVGClickListener(main) {
   const heroWrapper = main.querySelector('.hero-wrapper');
   const formSection = main.querySelector('.form-container');
@@ -17,13 +13,16 @@ function addSVGClickListener(main) {
   }
 }
 
-export async function loadLazy(main) {
+// eslint-disable-next-line import/prefer-default-export
+export function loadLazy(main) {
+  // Select the h1 and the text from default content wrapper,
+  // wrap them as a new hero-container for stylying purposes
   const hero = document.createElement('div');
   const imgDiv = document.createElement('div');
   const textDiv = document.createElement('div');
 
   const defaultContentWrapper = main.querySelector('.default-content-wrapper');
-  const heroimg = main.querySelector('.hero-wrapper');
+  const heroImgWrapper = main.querySelector('.hero-wrapper');
 
   defaultContentWrapper.parentElement.classList.add('hero-container');
 
@@ -31,14 +30,13 @@ export async function loadLazy(main) {
   imgDiv.className = 'img-div';
   textDiv.className = 'text-div';
 
-  [...heroimg.querySelectorAll('picture')].forEach((el) => {
-    imgDiv.append(el.cloneNode(true));
-  });
+  const heroImg = heroImgWrapper.querySelector('picture');
+  imgDiv.append(heroImg.cloneNode(true));
   hero.append(imgDiv);
 
   textDiv.append(main.querySelector('h1'));
 
-  [...defaultContentWrapper.querySelectorAll('p')].forEach((el) => {
+  defaultContentWrapper.querySelectorAll('p').forEach((el) => {
     if (el.innerText.trim() !== '') {
       textDiv.append(el.cloneNode(true));
     }
