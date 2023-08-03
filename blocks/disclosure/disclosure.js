63ef9c76ee4d7f0dc37ed699db5e13eddb69f069
Mark Frisbey <5108740+mfrisbey@users.noreply.github.com>
2023-08-03 12:56:54 -0600

    extend disclosure block to allow static content

--- blocks/disclosure/disclosure.js
+++ blocks/disclosure/disclosure.js
@@ -1,8 +1,21 @@
 export default async function decorate(block) {
-  const fragment = block.textContent.trim();
-  const response = await fetch(fragment || '/fragments/disclosure.plain.html');
-  if (!response.ok) {
+  let content = block.textContent.trim();
+  if (!content) {
+    // if no content in the block, default to the disclosure fragment
+    content = '/fragments/disclosure.plain.html';
+  }
+  if (String(content).startsWith('/')) {
+    // if the content is a URL, retrieve it and use the response text
+    // as the block's content
+    const response = await fetch(content);
+    if (!response.ok) {
+      return;
+    }
+    block.innerHTML = await response.text();
     return;
   }
-  block.innerHTML = await response.text();
+
+  // if the block content isn't empty and isn't a URL, it will be used
+  // as-is
+  block.innerHTML = `<p>${content}</p>`;
 }
