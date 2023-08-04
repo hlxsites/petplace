6d2acde91d0c27310d80e6861eef08f736340029
Julien Ramboz <ramboz@adobe.com>
2023-08-04 09:06:45 -0700

    chore: try optimized font loading for smoother UX

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -647,13 +647,12 @@ async function loadLazy(doc) {
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
 
+  await loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
+  // loadFonts();
   await loadHeader(doc.querySelector('header'));
   const footer = doc.querySelector('footer');
   footer.id = 'footer';
   loadFooter(footer);
-
-  await loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
-  // loadFonts();
 
   // identify the first item in the menu
   const firstMenu = document.querySelector('.nav-wrapper .nav-sections ul li a');
