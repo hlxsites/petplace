155bbe3fbb868800480a7838516145681c36ef2c
Mark Frisbey <5108740+mfrisbey@users.noreply.github.com>
2023-08-08 15:46:02 -0600

    feat: implement get-pet-insurance-now page

--- blocks/pet-insurance-quote/pet-insurance-quote.js
+++ blocks/pet-insurance-quote/pet-insurance-quote.js
@@ -1,4 +1,10 @@
 export default function decorate(block) {
-  // Nothing here yet
-  block.innerHTML = '';
+  const a = document.createElement('a');
+  a.classList.add('button', 'primary');
+  a.href = 'https://www.petpartners.com/enroll?p=PPFB2020';
+
+  const buttonCell = block.children[0].children[1];
+  a.innerText = buttonCell.innerText;
+  buttonCell.remove();
+  block.children[0].append(a);
 }
