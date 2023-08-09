6c687aa2d936432c58682b15003b65c961e22c0c
Mark Frisbey <5108740+mfrisbey@users.noreply.github.com>
2023-08-09 09:40:56 -0600

    improve efficiency of text check

--- blocks/pet-insurance-quote/pet-insurance-quote.js
+++ blocks/pet-insurance-quote/pet-insurance-quote.js
@@ -1,7 +1,7 @@
 export default function decorate(block) {
   // for backward compatibility, there are some pet insurance blocks in existing
   // pages that don't have content. Continue showing nothing for those cases
-  if (!String(block.innerText).trim()) {
+  if (!block.textContent.trim()) {
     block.remove();
     return;
   }
