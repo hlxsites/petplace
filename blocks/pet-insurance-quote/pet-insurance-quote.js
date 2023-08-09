7d634c1554e768570ad3c2922ef79f521e4647fe
Mark Frisbey <5108740+mfrisbey@users.noreply.github.com>
2023-08-09 10:11:04 -0600

    feat: add "triple" variant of pet-insurance-quote block

--- blocks/pet-insurance-quote/pet-insurance-quote.js
+++ blocks/pet-insurance-quote/pet-insurance-quote.js
@@ -12,6 +12,13 @@ export default function decorate(block) {
 
   const buttonCell = block.children[0].children[1];
   a.innerText = buttonCell.innerText;
+  block.children[0].insertBefore(a, buttonCell);
   buttonCell.remove();
-  block.children[0].append(a);
+
+  block.children[0].children[0].classList.add('insurance-label');
+  block.children[0].children[1].classList.add('insurance-button');
+
+  if (block.children[0].children.length > 2) {
+    block.children[0].children[2].classList.add('insurance-description');
+  }
 }
