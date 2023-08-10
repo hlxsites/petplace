b1dbb6a6ee08dd4e4a5969eb95f55566da9d578d
Mark Frisbey <5108740+mfrisbey@users.noreply.github.com>
2023-08-10 10:21:58 -0600

    use single cell in pet insurance quote block

--- blocks/pet-insurance-quote/pet-insurance-quote.js
+++ blocks/pet-insurance-quote/pet-insurance-quote.js
@@ -3,22 +3,5 @@ export default function decorate(block) {
   // pages that don't have content. Continue showing nothing for those cases
   if (!block.textContent.trim()) {
     block.remove();
-    return;
-  }
-
-  const a = document.createElement('a');
-  a.classList.add('button', 'primary');
-  a.href = 'https://www.petpartners.com/enroll?p=PPFB2020';
-
-  const buttonCell = block.children[0].children[1];
-  a.innerText = buttonCell.innerText;
-  block.children[0].insertBefore(a, buttonCell);
-  buttonCell.remove();
-
-  block.children[0].children[0].classList.add('insurance-label');
-  block.children[0].children[1].classList.add('insurance-button');
-
-  if (block.children[0].children.length > 2) {
-    block.children[0].children[2].classList.add('insurance-description');
   }
 }
