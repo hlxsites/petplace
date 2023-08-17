74a24562e84dde121830bb0c90f06658039b9279
Julien Ramboz <ramboz@adobe.com>
2023-08-17 13:20:34 -0700

    Revert "first draft of lunr api for optimizing searching"
    This reverts commit e40bc47a1df70afd6cfc7d139207dbf0b48bf807.

--- templates/searchresults/searchresults.js
+++ templates/searchresults/searchresults.js
@@ -1,7 +1,6 @@
 import ffetch from '../../scripts/ffetch.js';
 import { buildBlock, sampleRUM } from '../../scripts/lib-franklin.js';
 import { decorateResponsiveImages, meterCalls } from '../../scripts/scripts.js';
-import { createAnIndex } from '../../scripts/lunr.js';
 
 function createArticleDiv(article) {
   const div = document.createElement('div');
@@ -36,7 +35,6 @@ async function renderArticles(articles) {
   let found = false;
   // eslint-disable-next-line no-restricted-syntax
   for await (const article of res) {
-    console.log(article);
     found = true;
     const div = createArticleDiv(article);
     promises.push(meterCalls(() => block.append(div)));
@@ -57,7 +55,6 @@ async function renderArticles(articles) {
 }
 
 async function getArticles() {
-  createAnIndex();
   const usp = new URLSearchParams(window.location.search);
   const limit = usp.get('limit') || 16;
   const query = usp.get('query');
