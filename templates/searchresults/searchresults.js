a1216daf1a96144df5774cdeb53afd1ed0562df4
Julien Ramboz <ramboz@adobe.com>
2023-08-28 16:19:31 -0700

    chore: also handle slow 2G in the search fallback

--- templates/searchresults/searchresults.js
+++ templates/searchresults/searchresults.js
@@ -4,7 +4,7 @@ import { decorateResponsiveImages } from '../../scripts/scripts.js';
 let searchWorker;
 // 2G connections are too slow to realistically load the elasticlunr search index
 // instead we'll do a poor man's search on the last 5K articles
-if (window.navigator.connection.effectiveType !== '2g') {
+if (!['slow-2g', '2g'].includes(window.navigator.connection.effectiveType)) {
   searchWorker = new Worker('/scripts/worker/search.js');
 }
 
