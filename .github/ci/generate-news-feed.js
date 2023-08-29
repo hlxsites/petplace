2f4e224c96def98bccc9e28aee9fa12cecfd8b0f
Julien Ramboz <ramboz@adobe.com>
2023-08-29 09:53:32 -0700

    feat: add RSS/Atom feed generation on article publishing

--- .github/ci/generate-news-feed.js
+++ .github/ci/generate-news-feed.js
@@ -6,10 +6,19 @@ const limit = 1000;
 
 function sanitize(str) {
   return str
-    .replaceAll('&amp;', '&')
-    .replaceAll('\x92', '\'')
-    .replaceAll('\x97', '—')
+    .replaceAll('&amp;', '&#x26;')  
+    .replaceAll('&', '&#x26;')
+    .replaceAll('<', '&#x3C;')
+    .replaceAll('\x91', '&#x27;') // single quote (opening)
+    .replaceAll('\x92', '&#x27;') // single quote (closing)
+    .replaceAll('\x93', '&#x22;') // double quote (opening)
+    .replaceAll('\x94', '&#x22;') // double quote (closing)
+    .replaceAll('\x96', '&#x2013;') // ndash
+    .replaceAll('\x97', '&#x2014;') // mdash
+    .replaceAll('\x98', '&#x7e;') // tilda symbole
+    .replaceAll('\x99', '&#x2122;'); // TM symbol
 }
+
 async function fetchArticles() {
   let offset = 0;
   const allPosts = [];
@@ -38,12 +47,16 @@ async function main() {
   const newestPost = Math.max(...articles.map((article) => article.date));
   const feed = new Feed({
     title: `PetPlace.com articles`,
-    id: 'petplace.com',
-    link: `https://www.petplace.com/rss.xml`,
+    id: 'https://www.petplace.com/',
+    link: 'https://www.petplace.com/atom.xml',
     updated: new Date(newestPost * 1000),
     generator: 'News feed generator (GitHub action)',
     language: 'en',
     copyright: `All rights reserved ${new Date().getFullYear()},  Independence America Holdings Corp.`,
+    feedLinks: {
+      atom: 'https://www.petplace.com/atom.xml',
+      rss: 'https://www.petplace.com/rss.xml'
+    },
   });
 
   articles.forEach((article) => {
@@ -52,12 +65,14 @@ async function main() {
       title: sanitize(article.title),
       description: sanitize(article.description),
       link: `https://www.petplace.com${article.path}`,
-      author: [{
-        name: article.author,
-      }],
+      author: article.author
+        ? [{ name: article.author }]
+        : '',
       date: article.date ? new Date(article.date * 1000) : '',
       image: `https://www.petplace.com${article.image.replace(/\?.*/, '')}`,
-      category: article['category name'] ? [{name: article['category name'] }] : ''
+      category: article['category name']
+        ? [{ name: sanitize(article['category name']), term: article.category }]
+        : ''
     })
   });
 
