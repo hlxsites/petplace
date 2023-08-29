2fc1e8faaefdb56cc36c1fa2bce7cc5ea83819b7
Julien Ramboz <ramboz@adobe.com>
2023-08-29 15:00:52 -0700

    feat: track UTM parameters in Franklin RUM

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -754,6 +754,12 @@ async function loadLazy(doc) {
   sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
   sampleRUM.observe(main.querySelectorAll('picture > img'));
 
+  const usp = new URLSearchParams(window.location.search);
+  const utmParams = [...usp.entries()].filter(([key]) => key.startsWith('utm_'));
+  utmParams.forEach(([key, value]) => {
+    sampleRUM('utm-campaign', { source: key, target: value });
+  });
+
   if (!isMartechDisabled) {
     integrateMartech();
     initPartytown();
