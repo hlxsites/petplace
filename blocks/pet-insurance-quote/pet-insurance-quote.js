8968ea5fcafeb1040bf89c3e8fade61b3f850009
Mark Frisbey <5108740+mfrisbey@users.noreply.github.com>
2023-08-31 13:29:13 -0600

    feat: implement give-the-gift-of-pet-insurance page (#296)

--- blocks/pet-insurance-quote/pet-insurance-quote.js
+++ blocks/pet-insurance-quote/pet-insurance-quote.js
@@ -5,19 +5,10 @@ export default function decorate(block) {
   // pages that don't have content. Continue showing nothing for those cases
   if (!block.textContent.trim()) {
     block.remove();
-    return;
   }
 
-  const a = document.createElement('a');
-  a.classList.add('button', 'primary');
-  a.href = 'https://www.petpartners.com/enroll?p=PPFB2020';
-
-  const buttonCell = block.children[0].children[1];
-  a.innerText = buttonCell.innerText;
-  buttonCell.remove();
-  block.children[0].append(a);
-
   if (sampleRUM.convert) {
+    const a = block.querySelector('a');
     a.addEventListener('click', () => {
       sampleRUM.convert('insurance-enroll', a.innerText, a, []);
     });
