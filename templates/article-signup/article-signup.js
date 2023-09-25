a107fd3594a422fe933d324432940c5bb8975077
Mark Frisbey <5108740+mfrisbey@users.noreply.github.com>
2023-09-25 11:02:42 -0600

    feat: improve newsletter signup on mobile

--- templates/article-signup/article-signup.js
+++ templates/article-signup/article-signup.js
@@ -3,18 +3,38 @@ import { loadBlock } from '../../scripts/lib-franklin.js';
 
 // eslint-disable-next-line import/prefer-default-export
 export async function loadLazy(main) {
-  const picture = main.querySelector('picture');
+  const templateContent = document.createElement('div');
+  templateContent.classList.add('article-signup-section');
   const defaultContent = main.querySelector('.default-content-wrapper');
-  if (picture && defaultContent) {
-    const pictureParent = picture.parentElement;
-    defaultContent.parentElement.insertBefore(picture, defaultContent);
-    if (pictureParent.children.length === 0) {
-      pictureParent.remove();
-    }
-    const newsletter = await createNewsletterAutoBlock('/fragments/newsletter-footer', (block) => {
-      defaultContent.append(block);
-    });
-    newsletter.classList.add('horizontal');
-    await loadBlock(newsletter);
+  if (!defaultContent) {
+    return;
   }
+
+  // rearrange element structure so that there are three items at
+  // the same level:
+  // 1. The first picture in the document
+  // 2. A div containing all the rest of the content from the
+  //    document.
+  // 3. The newsletter signup form
+  let pictureFound = false;
+  const contentDiv = document.createElement('div');
+  contentDiv.classList.add('article-signup-content');
+  [...defaultContent.children].forEach((child) => {
+    const picture = child.querySelector('picture');
+    if (!pictureFound && picture) {
+      pictureFound = true;
+      templateContent.append(picture);
+    } else {
+      contentDiv.append(child);
+    }
+  });
+  templateContent.append(contentDiv);
+
+  defaultContent.innerHTML = '';
+  defaultContent.append(templateContent);
+
+  const newsletter = await createNewsletterAutoBlock('/fragments/newsletter-footer', (block) => {
+    templateContent.append(block);
+  });
+  await loadBlock(newsletter);
 }
