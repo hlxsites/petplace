6816bfefe47669a7e9aa8cfecc9fc6a3c0371eb8
Julien Ramboz <ramboz@adobe.com>
2023-10-04 08:36:27 +0200

    feat: add GTM tag for ads tracking

--- scripts/third-party.js
+++ scripts/third-party.js
@@ -2,6 +2,7 @@
 import { isMobile, loadScript } from './scripts.js';
 
 const GTM_ID = 'GTM-WP2SGNL';
+const TAG_ID = 'AW-11334653569';
 
 const GTM_SCRIPT = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -9,7 +10,16 @@ const GTM_SCRIPT = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${GTM_ID}');`;
 
-function createInlineScript(innerHTML, parent) {
+function loadGTag(id) {
+  loadScript(`https://www.googletagmanager.com/gtag/js?id=${TAG_ID}`, () => {
+    function gtag(...args) {
+      window.dataLayer.push(...args);
+    }
+    gtag('js', new Date());
+    gtag('config', id);
+  }, { async: '' });
+}
+function loadScriptInWorker(innerHTML, parent) {
   const script = document.createElement('script');
   script.type = 'text/partytown';
   script.innerHTML = innerHTML;
@@ -23,5 +33,6 @@ export default function integrateMartech() {
     loadScript('https://securepubads.g.doubleclick.net/tag/js/gpt.js', () => {}, { async: '' });
   }
 
-  createInlineScript(GTM_SCRIPT, document.body);
+  loadScriptInWorker(GTM_SCRIPT, document.body);
+  loadGTag(TAG_ID);
 }
