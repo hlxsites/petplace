793c1be89745452f076f7241a2e715292fd6592f
ankyc-phi <ankit.curchorcar@pethealthinc.com>
2023-10-25 10:56:29 -0400

    added eslint rule for ++ in for loops

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     'import/extensions': ['error', {
       js: 'always',
     }],
+    'no-plusplus': 'off',
     'import/prefer-default-export': 'off',
     'no-return-await': 'off',
   },
