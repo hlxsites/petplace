c5702ede4124d67e4dc3f968edebadc2c04c3744
ankyc-phi <146023473+ankyc-phi@users.noreply.github.com>
2023-10-25 11:59:20 -0400

    Datalayer implementation for tracking article shares (#333)
    Branch Commits :
    
    - changes to href for consistency
    - added eslint rule for ++ in for loops
    - small changes to datalayer file
    - handle article share push to DL
    - added try catch block for console error
    - change to foreach instead of for loop
    
    Jira Issues :
    
    https://pethealthinc.atlassian.net/jira/software/c/projects/PPL/boards/35?selectedIssue=PPL-19
    
    Test URLs:
    - Before: https://main--petplace--hlxsites.hlx.page/
    - After:
      - https://datalayer-ppl-19--petplace--hlxsites.hlx.page/
      - https://datalayer-ppl-19--petplace--hlxsites.hlx.page/?martech=off

--- scripts/datalayer.js
+++ scripts/datalayer.js
@@ -6,15 +6,45 @@ export const pushToDataLayer = (layer) => {
 
 // GLOBAL VARIABLES
 const handleGlobalVariables = () => {
-  // TODO : if meta category not present, create user story (avinash)
+  // TODO: if meta category not present, create user story (avinash)
   const contentGroup = document.querySelector('meta[name="category"]');
   pushToDataLayer({
-    content_group: contentGroup ? contentGroup.content : '',
+    content_group: contentGroup
+      ? contentGroup.content
+      : 'N/A - Content Group Not Set',
   });
 };
 
-// ARTICLE SHARE -> to come in later
+// ARTICLE SHARE
+const handleArticleShare = () => {
+  try {
+    const aTags = document
+      .querySelector('.social-share')
+      .getElementsByTagName('a');
+
+    aTags.forEach((tag) => {
+      tag.addEventListener('click', () => {
+        const strSplit = tag.href.split('.com')[0];
+        const strValue = strSplit.split('.')[1] || 'Email';
+        const strCaps = strValue.charAt(0).toUpperCase() + strValue.slice(1);
+
+        pushToDataLayer({
+          event: 'article_share',
+          method: strCaps,
+        });
+      });
+    });
+  } catch (err) {
+    // TODO: figure out best way to handle no tags
+    console.warn('Warning - No Articles Found', err);
+  }
+};
+
+// ELEMENT CLICK -> to come later (#5)
+const handleElementClicks = () => {};
 
 export const handleDataLayerApproach = () => {
   handleGlobalVariables();
+  handleArticleShare();
+  handleElementClicks();
 };
