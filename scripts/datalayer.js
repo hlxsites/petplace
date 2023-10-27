395b4c6a5f7174efc80660d96756e2eb4a425418
ankyc-phi <146023473+ankyc-phi@users.noreply.github.com>
2023-10-27 14:50:55 -0400

    Datalayer implementation for tracking header clicks (#334)
    Commits:
    - change to article share to prevent warnings
    - helper function to help with pushing data
    - sending data for click events on headers
    - fixed linting (whitespace) issues
    
    Jira Issue:
    
    https://pethealthinc.atlassian.net/jira/software/c/projects/PPL/boards/35?selectedIssue=PPL-18
    
    https://pethealthinc.atlassian.net/jira/software/c/projects/PPL/boards/35?selectedIssue=PPL-43
    
    Bugs Fixed:
    ![screenshot_2023-10-26_at_8 06
    20___am_720](https://github.com/hlxsites/petplace/assets/146023473/5182a712-1372-41f0-9d05-fd469d0178d4)
    
    Test URLs:
    - Before: https://main--petplace--hlxsites.hlx.page/
    - After:
      - https://datalayer-ppl-18-43--petplace--hlxsites.hlx.page/
    - https://datalayer-ppl-18-43--petplace--hlxsites.hlx.page/?martech=off

--- scripts/datalayer.js
+++ scripts/datalayer.js
@@ -6,7 +6,6 @@ export const pushToDataLayer = (layer) => {
 
 // GLOBAL VARIABLES
 const handleGlobalVariables = () => {
-  // TODO: if meta category not present, create user story (avinash)
   const contentGroup = document.querySelector('meta[name="category"]');
   pushToDataLayer({
     content_group: contentGroup
@@ -17,31 +16,45 @@ const handleGlobalVariables = () => {
 
 // ARTICLE SHARE
 const handleArticleShare = () => {
-  try {
-    const aTags = document
-      .querySelector('.social-share')
-      .getElementsByTagName('a');
+  const aTags = document.querySelectorAll('.social-share a');
 
-    aTags.forEach((tag) => {
-      tag.addEventListener('click', () => {
-        const strSplit = tag.href.split('.com')[0];
-        const strValue = strSplit.split('.')[1] || 'Email';
-        const strCaps = strValue.charAt(0).toUpperCase() + strValue.slice(1);
+  aTags.forEach((tag) => {
+    tag.addEventListener('click', () => {
+      const strSplit = tag.href.split('.com')[0];
+      const strValue = strSplit.split('.')[1] || 'Email';
+      const strCaps = strValue.charAt(0).toUpperCase() + strValue.slice(1);
 
-        pushToDataLayer({
-          event: 'article_share',
-          method: strCaps,
-        });
+      pushToDataLayer({
+        event: 'article_share',
+        method: strCaps,
       });
     });
-  } catch (err) {
-    // TODO: figure out best way to handle no tags
-    console.warn('Warning - No Articles Found', err);
-  }
+  });
 };
 
-// ELEMENT CLICK -> to come later (#5)
-const handleElementClicks = () => {};
+// ELEMENT CLICK
+// Categories -> Outbound Link, Embedded Link, Header Link, Footer Link, CTA Button, Social Link
+
+const clickHelper = (category, text, type, url) => {
+  console.log('here', category, text, type, url);
+  pushToDataLayer({
+    event: 'element_click',
+    element_category: category,
+    element_text: text,
+    element_type: type,
+    element_url: url,
+  });
+};
+
+const handleElementClicks = () => {
+  // header link
+  const headerTracking = document.querySelectorAll('.nav-sections a');
+  headerTracking.forEach((tag) => {
+    tag.addEventListener('click', () => {
+      clickHelper('Header', tag.innerHTML, 'link', tag.href);
+    });
+  });
+};
 
 export const handleDataLayerApproach = () => {
   handleGlobalVariables();
