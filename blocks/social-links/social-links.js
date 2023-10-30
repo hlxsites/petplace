7e7dae48a845b2058bd0073ccd3d3606363acedd
ankyc-phi <ankit.curchorcar@pethealthinc.com>
2023-10-30 15:11:39 -0400

    changing for better naming and targeting

--- blocks/social-links/social-links.js
+++ blocks/social-links/social-links.js
@@ -6,7 +6,7 @@ export default function decorate(block) {
   block.innerHTML = `
     <ul>
       <li><a href="https://www.facebook.com/petplacefans" aria-label="Go to our Facebook page"><span class="icon icon-facebook"></span></a></li>
-      <li><a href="https://twitter.com/PetPlaceFans" aria-label="Go to our Twitter page"><span class="icon icon-twitter"></span></a></li>
+      <li><a href="https://www.twitter.com/PetPlaceFans" aria-label="Go to our Twitter page"><span class="icon icon-twitter"></span></a></li>
       <li><a href="https://www.pinterest.com/petplacefans/" aria-label="Go to our Pinterest page"><span class="icon icon-pinterest"></span></a></li>
       <li><a href="https://www.instagram.com/petplace/" aria-label="Go to our Instagram page"><span class="icon icon-instagram"></span></a></li>
       <li><a href="mailto:info@petplace.com?&subject=${pageTitle}&body=${url}" aria-label="Send us an email about this page"><span class="icon icon-email"></span></a></li>
