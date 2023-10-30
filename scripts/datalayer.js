dd8cea6da8d0912f1bc37fc91e8249f22c907b40
ankyc-phi <ankit.curchorcar@pethealthinc.com>
2023-10-30 15:14:49 -0400

    moved click helper func to utils file

--- scripts/datalayer.js
+++ scripts/datalayer.js
@@ -1,4 +1,5 @@
 import {
+  clickHelper,
   pushToDataLayer,
 } from './utils/helpers.js';
 
--- scripts/utils/helpers.js
+++ scripts/utils/helpers.js
@@ -3,3 +3,14 @@ window.dataLayer ||= [];
 export const pushToDataLayer = (layer) => {
   window.dataLayer.push(layer);
 };
+
+export const clickHelper = (category, text, type, url) => {
+  console.log('here', category, text, type, url); // TODO remove
+  pushToDataLayer({
+    event: 'element_click',
+    element_category: category,
+    element_text: text,
+    element_type: type,
+    element_url: url,
+  });
+};
