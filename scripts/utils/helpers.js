df88ea4ad48b76125aea0df27d1ff9891b3a83cd
ankyc-phi <ankit.curchorcar@pethealthinc.com>
2023-11-01 14:03:15 -0400

    added console logs for testing

--- scripts/utils/helpers.js
+++ scripts/utils/helpers.js
@@ -1,11 +1,12 @@
 window.dataLayer ||= [];
 
 export const pushToDataLayer = (layer) => {
+  console.log('layer', layer); // TODO: remove later
   window.dataLayer.push(layer);
 };
 
 export const clickHelper = (category, text, type, url) => {
-  console.log('here', category, text, type, url); // TODO remove
+  console.log('here', category, text, type, url); // TODO: remove
   pushToDataLayer({
     event: 'element_click',
     element_category: category,
@@ -22,6 +23,7 @@ export const getSocialName = (href) => {
   return strCaps;
 };
 
+// FOOTER HELPERS
 export const footerSocialHelper = () => {
   const socialTracking = document.querySelectorAll('.footer-social a');
   socialTracking.forEach((tag) => {
@@ -34,6 +36,16 @@ export const footerSocialHelper = () => {
 export const footerNavHelper = () => {
   const footerTracking = document.querySelectorAll('.footer-nav-links a');
   footerTracking.forEach((tag) => {
+    tag.addEventListener('click', () => {
+      clickHelper('Footer', tag.innerHTML, 'link', tag.href);
+    });
+  });
+};
+
+// TODO - revisit later (phase 2)
+export const footerLegalHelper = () => {
+  const legalTracking = document.querySelectorAll('.footer-legal a');
+  legalTracking.forEach((tag) => {
     tag.addEventListener('click', () => {
       clickHelper('Footer', tag.innerHTML, 'link', tag.href);
     });
