5f2ac3fdf5b9d05c802771fe03561797c7afb4e1
ankyc-phi <ankit.curchorcar@pethealthinc.com>
2023-11-09 16:59:25 -0500

    removed footer helpers

--- scripts/utils/helpers.js
+++ scripts/utils/helpers.js
@@ -1,18 +1,18 @@
 window.dataLayer ||= [];
 
 export const pushToDataLayer = (layer) => {
-  console.log('layer', layer); // TODO: remove later
+  console.log('layer:', layer); // TODO: remove later
   window.dataLayer.push(layer);
 };
 
-export const clickHelper = (category, text, type, url) => {
-  console.log('here', category, text, type, url); // TODO: remove
+export const clickHelper = (...args) => {
+  console.log('el_clicks:', args); // TODO: remove
   pushToDataLayer({
     event: 'element_click',
-    element_category: category,
-    element_text: text,
-    element_type: type,
-    element_url: url,
+    element_category: args[0],
+    element_text: args[1],
+    element_type: args[2],
+    element_url: args[3],
   });
 };
 
@@ -21,33 +21,4 @@ export const getSocialName = (href) => {
   const strValue = strSplit.split('.')[1] || 'Email';
   const strCaps = strValue.charAt(0).toUpperCase() + strValue.slice(1);
   return strCaps;
-};
-
-// FOOTER HELPERS
-export const footerSocialHelper = () => {
-  const socialTracking = document.querySelectorAll('.footer-social a');
-  socialTracking.forEach((tag) => {
-    tag.addEventListener('click', () => {
-      clickHelper('Footer Social', getSocialName(tag.href), 'link', tag.href);
-    });
-  });
-};
-
-export const footerNavHelper = () => {
-  const footerTracking = document.querySelectorAll('.footer-nav-links a');
-  footerTracking.forEach((tag) => {
-    tag.addEventListener('click', () => {
-      clickHelper('Footer Nav', tag.innerHTML, 'link', tag.href);
-    });
-  });
-};
-
-// TODO - revisit later (phase 2)
-export const footerLegalHelper = () => {
-  const legalTracking = document.querySelectorAll('.footer-legal a');
-  legalTracking.forEach((tag) => {
-    tag.addEventListener('click', () => {
-      clickHelper('Footer Legal', tag.innerHTML, 'link', tag.href);
-    });
-  });
 };
