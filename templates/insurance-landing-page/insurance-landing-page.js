94886ced88fc60e17394cf3ba05bdfa5e2a73fcd
ankyc-phi <ankit.curchorcar@pethealthinc.com>
2023-11-22 18:50:16 -0500

    datalayer tracking in lazy phase

--- templates/insurance-landing-page/insurance-landing-page.js
+++ templates/insurance-landing-page/insurance-landing-page.js
@@ -1,4 +1,14 @@
-// eslint-disable-next-line import/prefer-default-export
-export function loadEager() {
-  return false;
+import { pushToDataLayer } from '../../scripts/utils/helpers.js';
+
+export function loadLazy() {
+  document.querySelector('.insurance').addEventListener('click', (ev) => {
+    const btn = ev.target.closest('a');
+    if (!btn) return;
+
+    // TODO: check if correct event, and details
+    pushToDataLayer({
+      event: 'insurance',
+      url: btn.href,
+    });
+  });
 }
