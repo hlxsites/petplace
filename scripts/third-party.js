4281f8cdbcb5aaa147c4c4fe73cdc89b7453d05b
ankyc-phi <ankit.curchorcar@pethealthinc.com>
2023-12-20 12:04:23 -0500

    pushing changes to 3rd party script

--- scripts/third-party.js
+++ scripts/third-party.js
@@ -24,7 +24,25 @@ async function handleConfigs() {
   }
 }
 
-window.dataLayer = window.dataLayer || [];
+// TODO: leave it up for a week
+function gtmScript() {
+  /* eslint-disable */
+  (function (w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+    let f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != 'dataLayer' ? '&l=' + l : '';
+
+    console.log('script', f, j);
+
+    j.async = true;
+    j.type = 'text/javascript';
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, 'script', 'dataLayer', GTM_ID);
+  /* eslint-enable */
+}
 
 export async function loadLazy() {
   // Load ads early on desktop since the impact is minimal there and
@@ -34,24 +52,10 @@ export async function loadLazy() {
       async: '',
     });
   }
+
+  // calling gtm in lazy phase
+  gtmScript();
+
   // datalayer pushes for conversion tracking
   handleConfigs();
-}
-
-// TODO: leave it up for a week
-export function loadEager() {
-  /* eslint-disable */
-  (function (w, d, s, l, i) {
-    w[l] = w[l] || [];
-    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-    let f = d.getElementsByTagName(s)[0],
-      j = d.createElement(s),
-      dl = l != 'dataLayer' ? '&l=' + l : '';
-
-    j.async = true;
-    j.type = 'text/javascript';
-    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-    f.parentNode.insertBefore(j, f);
-  })(window, document, 'script', 'dataLayer', GTM_ID);
-  /* eslint-enable */
 }
