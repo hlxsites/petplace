056acdc7269cc34a14fbc033d4ceee6ba01359fa
ankyc-phi <146023473+ankyc-phi@users.noreply.github.com>
2024-01-04 09:11:11 -0500

    fix: added gtm noscript tag (#355)
    Branch Commits:
    - pushing gtag to generic datalayer
    - new functions for gtm and noscripts
    - function calls in delayed phase
    
    Related Jira Issues:
    https://pethealthinc.atlassian.net/browse/PM-19
    
    Related PR (closed):
    https://github.com/hlxsites/petplace/pull/354
    
    Test URLs:
    - Before: https://main--petplace--hlxsites.hlx.page/
    - After:
      - https://issue-gtm-noscript--petplace--hlxsites.hlx.live/
      - https://issue-gtm-noscript--petplace--hlxsites.hlx.live/?martech=off
    
    ---------
    
    Co-authored-by: Julien Ramboz <ramboz@users.noreply.github.com>

--- scripts/third-party.js
+++ scripts/third-party.js
@@ -1,47 +1,81 @@
 import { loadScript } from './lib-franklin.js';
 // eslint-disable-next-line import/no-cycle
 import { isMobile } from './scripts.js';
+import { pushToDataLayer } from './utils/helpers.js';
 
+const GTX_ID = 'GT-KD23TWW';
 const GTM_ID = 'GTM-WP2SGNL';
+const GA4_ID = 'G-V3CZKM4K6N';
 const TAG_ID = 'AW-11334653569';
 
-function gtag(...args) {
-  window.dataLayer.push(...args);
-}
+async function gtagLoad() {
+  await loadScript(`https://www.googletagmanager.com/gtag/js?id=${TAG_ID}`, {
+    async: '',
+  });
 
-async function loadGTag(id) {
-  await loadScript(`https://www.googletagmanager.com/gtag/js?id=${TAG_ID}`, { async: '' });
-  gtag('js', new Date());
-  gtag('config', id);
+  pushToDataLayer({ js: new Date() });
+  pushToDataLayer({ config: GTX_ID });
+  pushToDataLayer({ config: GA4_ID });
+  pushToDataLayer({ config: TAG_ID });
 
   const metaTemplate = document.querySelector('meta[name="template"]');
   if (
     metaTemplate.content === 'Home-page'
     && window.location.pathname === '/'
   ) {
-    gtag('event', 'conversion', {
+    pushToDataLayer('event', 'conversion', {
       send_to: `${TAG_ID}/iQbBCNzr2OoYEIGt5Jwq`,
     });
   }
 }
 
-window.dataLayer = window.dataLayer || [];
+function gtmScript() {
+  /* eslint-disable */
+  (function (w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+    let f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != 'dataLayer' ? '&l=' + l : '';
+
+    j.async = true;
+    j.type = 'text/javascript';
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, 'script', 'dataLayer', GTM_ID);
+  /* eslint-enable */
+}
+
+function noscriptBody() {
+  /* eslint-disable */
+  let b = document.body.firstChild,
+    ns = document.createElement('noscript'),
+    fr = document.createElement('iframe');
+
+  fr.src = `https://www.googletagmanager.com/ns.html?id=${GTM_ID}`;
+  fr.height = 0;
+  fr.width = 0;
+  fr.style = 'display:none;visibility:hidden;';
+
+  ns.appendChild(fr);
+  b.parentNode.insertBefore(ns, b);
+  /* eslint-enable */
+}
 
 export async function loadLazy() {
   // Load ads early on desktop since the impact is minimal there and
   // this helps reduce CLS and loading animation duration
   if (!isMobile() && document.querySelector('.block.ad')) {
-    loadScript('https://securepubads.g.doubleclick.net/tag/js/gpt.js', { async: '' });
+    loadScript('https://securepubads.g.doubleclick.net/tag/js/gpt.js', {
+      async: '',
+    });
   }
 }
 
 export function loadDelayed() {
-  /* eslint-disable */
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer',GTM_ID);
-  /* eslint-enable */
-  loadGTag(TAG_ID);
+  gtmScript();
+  noscriptBody();
+
+  // datalayer pushes for conversion tracking
+  gtagLoad();
 }
