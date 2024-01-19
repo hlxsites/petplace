cb53ba1a19433efa0408d5c49e710e4787fe50f0
PETCARE\Shiv.Alkondwar <shiv.alkondwar@pethealthinc.com>
2024-01-19 07:39:55 +0000

    PM #73 -  PPL: Aggregator- Insurance Landing Page

--- templates/insurance-landing-page/insurance-landing-page.js
+++ templates/insurance-landing-page/insurance-landing-page.js
@@ -1,14 +1,44 @@
 import { pushToDataLayer } from '../../scripts/utils/helpers.js';
+import { buildBlock } from '../../scripts/lib-franklin.js';
+
+function createSpanBlock(main) {
+  const searches = main.getElementsByClassName('search-box-wrapper');
+
+  [...searches].forEach((el) => {
+    el.removeAttribute('action');
+    const searchInput = el.querySelector('.search-input');
+
+    // For zipcode input validation.
+    // searchInput.addEventListener('input', (event) => {
+    //   event.target.value = event.target.value.replace(/[^0-9]/g, '');
+    // })
+    const searchButton = el.querySelector('.search-button');
+
+    // Eventlistener to redirect user to aggregator site.
+    searchButton.addEventListener('click', (ev) => {
+      const code = searchInput.value;
+      if(code) {
+        ev.preventDefault();
+        window.open(`https://polite-smoke-035c7bd10.4.azurestaticapps.net?zipCode=${code}`, '_blank');
+      }
+    });
+
+    const spanBlock = document.createElement('span');
+    spanBlock.className ='arrow-right';
+    searchButton.appendChild(spanBlock);
+  });
+}
 
 export function loadLazy() {
-  document.querySelector('.details-list-wrapper').addEventListener('click', (ev) => {
-    const btn = ev.target.closest('a');
-    if (!btn) return;
+  createSpanBlock(main);
+  // document.querySelector('.details-list-wrapper').addEventListener('click', (ev) => {
+  //   const btn = ev.target.closest('a');
+  //   if (!btn) return;
 
-    // TODO: check if correct event, and details
-    pushToDataLayer({
-      event: 'insurance',
-      url: btn.href,
-    });
-  });
+  //   // TODO: check if correct event, and details
+  //   pushToDataLayer({
+  //     event: 'insurance',
+  //     url: btn.href,
+  //   });
+  // });
 }
