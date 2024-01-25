d350a8043be71a7d13de8e889d8c8aebb224de89
ankyc-phi <ankit.curchorcar@pethealthinc.com>
2024-01-25 11:54:27 -0500

    removing aggregator code from branch

--- templates/insurance-landing-page/insurance-landing-page.js
+++ templates/insurance-landing-page/insurance-landing-page.js
@@ -1,42 +1,14 @@
-function createSpanBlock(main) {
-  const searches = main.getElementsByClassName('search-box-wrapper');
-
-  [...searches].forEach((el) => {
-    el.removeAttribute('action');
-    const searchInput = el.querySelector('.search-input');
-
-    // For zipcode input validation.
-    // searchInput.addEventListener('input', (event) => {
-    //   event.target.value = event.target.value.replace(/[^0-9]/g, '');
-    // })
-    const searchButton = el.querySelector('.search-button');
-
-    // Eventlistener to redirect user to aggregator site.
-    searchButton.addEventListener('click', (ev) => {
-      const code = searchInput.value;
-      if (code) {
-        ev.preventDefault();
-        window.open(`https://quote.petplace.com/questionnaire?zipCode=${code}`, '_blank');
-      }
-    });
-
-    const spanBlock = document.createElement('span');
-    spanBlock.className = 'arrow-right';
-    searchButton.appendChild(spanBlock);
-  });
-}
+import { pushToDataLayer } from '../../scripts/utils/helpers.js';
 
 export function loadLazy() {
-  const main = document.querySelector('#main');
-  createSpanBlock(main);
-  // document.querySelector('.details-list-wrapper').addEventListener('click', (ev) => {
-  //   const btn = ev.target.closest('a');
-  //   if (!btn) return;
+  document.querySelector('.details-list-wrapper').addEventListener('click', (ev) => {
+    const btn = ev.target.closest('a');
+    if (!btn) return;
 
-  //   // TODO: check if correct event, and details
-  //   pushToDataLayer({
-  //     event: 'insurance',
-  //     url: btn.href,
-  //   });
-  // });
+    // TODO: check if correct event, and details
+    pushToDataLayer({
+      event: 'insurance',
+      url: btn.href,
+    });
+  });
 }
