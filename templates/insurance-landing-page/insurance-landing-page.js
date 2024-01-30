9a6a7cdf1af799b98ccbba5b33a658766b3247ad
shiv-pethealth <72878246+shiv-pethealth@users.noreply.github.com>
2024-01-30 19:54:44 +0530

    PPL: Aggregator - Zip Code Component (#369)
    Please always provide the [GitHub issue(s)](../issues) your PR is for,
    as well as test URLs where your change can be observed (before and
    after):
    
    Jira Ticket: [PM-72](https://pethealthinc.atlassian.net/browse/PM-72)
    
    Test URLs:
    - Before: https://main--petplace--hlxsites.hlx.page/
    - After:
      - https://pm-72-zipcode-component--petplace--hlxsites.hlx.page/
    -
    https://pm-72-zipcode-component--petplace--hlxsites.hlx.page/?martech=off

--- templates/insurance-landing-page/insurance-landing-page.js
+++ templates/insurance-landing-page/insurance-landing-page.js
@@ -1,22 +1,45 @@
-function createSpanBlock(main) {
-  const searches = main.getElementsByClassName('search-box-wrapper');
-
-  [...searches].forEach((el) => {
-    el.removeAttribute('action');
+function removeAllErrorMessage(searchContainers) {
+  [...searchContainers].forEach((el) => {
+    const searchDiv = el.querySelector('.search-box-wrapper');
+    const errorMsg = el.querySelector('.find-useful-wrapper');
     const searchInput = el.querySelector('.search-input');
 
-    // For zipcode input validation.
-    // searchInput.addEventListener('input', (event) => {
-    //   event.target.value = event.target.value.replace(/[^0-9]/g, '');
-    // })
-    const searchButton = el.querySelector('.search-button');
+    errorMsg.style.display = 'none';
+    searchInput.classList.remove('error-state');
+    searchDiv.classList.remove('error-spacing');
+    searchInput.value = '';
+  });
+}
+
+function isValidZipcode(code) {
+  const regex = /^[0-9]{5}(?:-[0-9]{4})?$/;
+
+  return regex.test(code);
+}
+
+function createSpanBlock(main) {
+  const searchContainers = main.getElementsByClassName('search-container');
+
+  [...searchContainers].forEach((el) => {
+    const searchDiv = el.querySelector('.search-box-wrapper');
+    const errorMsg = el.querySelector('.find-useful-wrapper');
+
+    searchDiv.removeAttribute('action');
+    const searchInput = searchDiv.querySelector('.search-input');
+    const searchButton = searchDiv.querySelector('.search-button');
 
     // Eventlistener to redirect user to aggregator site.
     searchButton.addEventListener('click', (ev) => {
+      ev.preventDefault();
       const code = searchInput.value;
-      if (code) {
-        ev.preventDefault();
+      if (isValidZipcode(code)) {
+        removeAllErrorMessage(searchContainers);
         window.open(`https://quote.petplace.com/questionnaire?zipCode=${code}`, '_blank');
+      } else {
+        errorMsg.style.display = 'block';
+        searchInput.classList.add('error-state');
+        searchDiv.classList.add('error-spacing');
+        searchInput.value = '';
       }
     });
   });
