b56120b1975d9421cfd2a3f4558331195a528903
Julien Ramboz <ramboz@adobe.com>
2024-02-01 08:48:55 +0100

    chore: delay MS clarity instrumentation

--- scripts/third-party.js
+++ scripts/third-party.js
@@ -2,6 +2,19 @@ import { loadScript } from './lib-franklin.js';
 // eslint-disable-next-line import/no-cycle
 import { isMobile } from './scripts.js';
 
+function loadMSClarity() {
+  ((c, l, a, r, i, t, y) => {
+    c[a] = c[a] || ((...args) => { (c[a].q = c[a].q || []).push(args); });
+    // eslint-disable-next-line no-param-reassign
+    t = l.createElement(r);
+    t.async = 1;
+    t.src = `https://www.clarity.ms/tag/${i}?ref=gtm2`;
+    // eslint-disable-next-line no-param-reassign
+    [y] = l.getElementsByTagName(r);
+    y.parentNode.insertBefore(t, y);
+  })(window, document, 'clarity', 'script', 'hz6a0je2i3');
+}
+
 export async function loadLazy() {
   // Load ads early on desktop since the impact is minimal there and
   // this helps reduce CLS and loading animation duration
@@ -19,4 +32,5 @@ export async function loadLazy() {
 }
 
 export function loadDelayed() {
+  loadMSClarity();
 }
