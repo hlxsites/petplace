917f16c339ae7da47acb5c80370c15c0a674b8f7
ankyc-phi <146023473+ankyc-phi@users.noreply.github.com>
2024-02-08 10:46:38 -0500

    hotfix: change gpt script load condition (#392)
    Commit: remove condition for block ads checker
    Note: need to publish `index.docx` as well on sharepoint
    
    Related: #391
    
    Test URLs:
    - Before: https://main--petplace--hlxsites.hlx.page/
    - After:
      - https://hotfix-homepage-ads-issue--petplace--hlxsites.hlx.page/
    -
    https://hotfix-homepage-ads-issue--petplace--hlxsites.hlx.page/?martech=off

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -42,7 +42,7 @@ function pushly(...args) {
 
 if (window.hlx.plugins.get('martech')) {
   if (
-    (document.querySelector('.block.ad')
+    (window.location.pathname === '/'
     || window.location.pathname.includes('tags')
     || window.location.pathname.includes('article')
     || window.location.pathname.includes('category'))
