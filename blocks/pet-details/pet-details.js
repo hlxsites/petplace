2e8276aec5a7f25e8e0ff1fc2f20977f8a747783
Jane Li <jane.li@herodigital.com>
2024-02-19 12:09:01 -0500

    PPAD-23: update js and styles

--- blocks/pet-details/pet-details.js
+++ blocks/pet-details/pet-details.js
@@ -1,5 +1,5 @@
 /* eslint-disable indent */
-import { fetchPlaceholders, createOptimizedPicture } from '../../scripts/lib-franklin.js';
+import { fetchPlaceholders, getMetadata } from '../../scripts/lib-franklin.js';
 import { ImageCarousel } from './image-carousel.js';
 
 async function getParametersFromUrl() {
@@ -48,6 +48,7 @@ async function fetchSimilarPets(zip, animalType) {
         });
         if(resp.ok) {
             const json = await resp.json();
+            console.log(json.animal)
             return formatSimilarPetData(json.animal)
         } else {
             return [];
@@ -152,10 +153,10 @@ async function createCarouselSection(petName, images){
         imageSectionContainer.className = 'image-section';
         const imageDiv = document.createElement('div');
         imageDiv.className = 'image-div';
-        console.log('pet name', petName)
-        imageDiv.innerHTML = `<div><img src=${imageArr.length === 0 ? "/images/pet_profile_placeholder.png" : imageArr[0]} alt=${ imageArr.length === 0 ? "no image available" : petName || '' } /></div>`;
+        imageDiv.innerHTML = '<div></div>';
+        imageDiv.firstElementChild.append(createImageObject(imageArr[0] ||'', getMetadata('carousel-image-fall-back'), null, 700, 575));
         imageSectionContainer.append(imageDiv);
-        return imageSectionContainer
+        return imageSectionContainer;
     } else {
         const carouselSectionContainer = document.createElement('div');
         carouselSectionContainer.className = imageArr.length === 2 ? 'carousel-section two-slides' : 'carousel-section';
@@ -211,8 +212,6 @@ async function createCarouselSection(petName, images){
         carouselSectionContainer.append(carouselDiv);
         return carouselSectionContainer;
     }
-
-
 }
 async function createAboutPetSection(aboutPet){
     const {petName, animalId, primaryBreed, secondaryBreed, city, state, age, gender, size, email, description, locatedAt, ageDescription, moreInfo, dataUpdated} = aboutPet
@@ -323,7 +322,7 @@ async function createSimilarPetsSection(sectionTitle, petArr){
         const gallery = document.createElement('div');
         gallery.className = 'similar-pets-gallery';
         petArr.forEach(pet => {
-            gallery.append(createPetCard(pet));
+            gallery.append(createPetCard(pet, getMetadata('animal-card-image-fall-back')));
         })
         galleryWrapper.appendChild(gallery)
         similarPetsContainer.append(galleryWrapper);
@@ -372,21 +371,18 @@ function createChecklistItem(index, label, text) {
     }
     return checklistItemContainer;
 }
-function createPetCard(petData) {
-    const {Name: petName, ['Animal type']: animalType, Gender: gender, Breed: breed, City: city, State: state, coverImagePath: image, animalId, clientId} = petData
+function createPetCard(petData, fallBackImage) {
+    const {Name: petName, ['Animal type']: animalType, Gender: gender, Breed: breed, City: city, State: state, coverImagePath, animalId, clientId} = petData
     const petDetailPageUrl = `/pet-adoption/${animalType}/${animalId}/${clientId}`
     const petCard = document.createElement('div');
     petCard.className = 'pet-card';
     const pictureContainer = document.createElement('div');
     pictureContainer.className = 'pet-card-image';
-    pictureContainer.append(createOptimizedPicture(image, petName, false, [
-        { media: '(min-width: 1024px)', width: 300 },
-        { width: 250 },
-    ]));
+    pictureContainer.append(createImageObject(coverImagePath, fallBackImage, null, 300, 246))
     const cardBody = document.createElement('div');
     cardBody.className = 'pet-card-body';
     cardBody.innerHTML = `
-        <h3 class="pet-card-name"><a href=${petDetailPageUrl} class="stretched-link">${petName}</a></h3>
+        <h3 class="pet-card-name"><a href=${petDetailPageUrl} class="stretched-link">${petName ? extractName(petName) : animalId}</a></h3>
         <div class="pet-card-info">
             ${gender ? `<span class="pet-card-gender">${gender}</span>` : ''}
             ${gender && breed ? `<span class="pet-card-dot"></span>`: ''}
@@ -397,20 +393,16 @@ function createPetCard(petData) {
         </div>
     `;
     const buttonContainer = document.createElement('div');
-    buttonContainer.className = 'pet-card-button-conainer';
+    buttonContainer.className = 'like-button-conainer';
     buttonContainer.innerHTML = `
-    <button class="pet-card-favorite">
-        <span class="pet-card-favorite-icon"></span>
-        <span class="sr-only">Favorite</span>
+    <button class="like-button" aria-label="Like">
+        <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M23.3 4.55114C22.8314 3.63452 22.154 2.84083 21.3223 2.2341C20.4906 1.62736 19.528 1.22455 18.5121 1.05815C17.4962 0.891748 16.4554 0.966411 15.4736 1.27612C14.4918 1.58583 13.5966 2.12191 12.86 2.84114L12 3.62114L11.17 2.86114C10.4344 2.13135 9.53545 1.58739 8.54766 1.27436C7.55987 0.961323 6.51169 0.888233 5.49002 1.06114C4.47076 1.21761 3.50388 1.61617 2.67044 2.22341C1.837 2.83066 1.16131 3.62888 0.700017 4.55114C0.0805959 5.76129 -0.13608 7.13766 0.0815777 8.47958C0.299236 9.82151 0.939848 11.0588 1.91002 12.0111L11.28 21.6711C11.3733 21.7679 11.4851 21.8449 11.6088 21.8975C11.7326 21.9501 11.8656 21.9772 12 21.9772C12.1344 21.9772 12.2675 21.9501 12.3912 21.8975C12.5149 21.8449 12.6267 21.7679 12.72 21.6711L22.08 12.0311C23.0535 11.0768 23.6968 9.8366 23.9163 8.49115C24.1357 7.1457 23.9198 5.76533 23.3 4.55114ZM20.66 10.6211L12.36 19.1711C12.2634 19.2651 12.1347 19.3187 12 19.3211C11.8657 19.3168 11.7377 19.2634 11.64 19.1711L3.33002 10.6111C2.66352 9.95404 2.22228 9.10271 2.06957 8.1793C1.91687 7.25589 2.06055 6.30784 2.48002 5.47114C2.80009 4.82822 3.26939 4.27124 3.84871 3.84675C4.42803 3.42227 5.10053 3.14261 5.81002 3.03114C6.53132 2.91042 7.27107 2.96456 7.96711 3.18903C8.66315 3.41351 9.29515 3.80175 9.81002 4.32114L11.34 5.71114C11.5238 5.87698 11.7625 5.96878 12.01 5.96878C12.2575 5.96878 12.4963 5.87698 12.68 5.71114L14.24 4.29114C14.7573 3.77638 15.3905 3.39328 16.0866 3.17402C16.7826 2.95477 17.5211 2.90577 18.24 3.03114C18.9406 3.15001 19.603 3.43308 20.1731 3.85719C20.7432 4.28129 21.2048 4.83436 21.52 5.47114C21.9391 6.31036 22.0815 7.26072 21.927 8.18592C21.7725 9.11112 21.329 9.96362 20.66 10.6211Z" fill="#464646"/>
+        </svg>
     </button>
     `
     petCard.append(pictureContainer, cardBody, buttonContainer);
     return petCard;
-}
-function htmlToString(html) {
-    const tempDivElement = document.createElement("div");
-    tempDivElement.innerHTML = html;
-    return tempDivElement.textContent || tempDivElement.innerText || "";
 }
 function formatPhoneNumber(phoneNumberString) {
     var cleaned = ('' + phoneNumberString).replace(/\D/g, '');
@@ -422,6 +414,22 @@ function formatPhoneNumber(phoneNumberString) {
 }
 function extractName(nameString) {
     return nameString.replace(/\s*\([^)]*\)/, '').trim();
+}
+function createImageObject(imagePath, fallBackSrc, fallBackAlt, width, height) {
+    const img = document.createElement('object');
+    img.data = imagePath;
+    img.type = "image/jpg";
+    if(width) {
+        img.width = width;
+    }
+    if(height) {
+        img.height = height;
+    }
+    const fallback = document.createElement('img');
+    fallback.src = fallBackSrc;
+    fallback.alt = fallBackAlt || '';
+    img.append(fallback);
+    return img;
 }
 export default async function decorate(block) {
     const {clientId, animalId} = await getParametersFromUrl();
