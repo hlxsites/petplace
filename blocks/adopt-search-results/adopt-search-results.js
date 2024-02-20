3e112748c62338900482b1fcad572a75cf54de4a
Ray Sanford <raymond.sanford@herodigital.com>
2024-02-20 17:15:23 -0500

    adding mobile layout for filtering

--- blocks/adopt-search-results/adopt-search-results.js
+++ blocks/adopt-search-results/adopt-search-results.js
@@ -21,7 +21,11 @@ const {
     genderLabel,
     sizeLabel,
     ageLabel,
-    clearLabel
+    clearLabel,
+    sortedLabel,
+    applyFiltersLabel,
+    filterCta,
+    createSearchAlert
 } = placeholders;
 // console.log('placeholders', petTypeLabel, petTypeValues, breedLabel, breedPlaceholder, zipLabel, zipPlaceholder, zipErrorMessage, searchButtonText);
 // console.log(placeholders);
@@ -167,7 +171,22 @@ function clearFilters() {
     for(var i=0;i<radioButtons.length;i++) {
         radioButtons[i].checked = false;
     }
-    
+    callAnimalList().then((data) => {
+        buildResultsContainer(data);
+    })
+}
+
+function openModal() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.add('show');
+    const overlay = document.querySelector('.overlay');
+    overlay.classList.add('show');
+}
+function closeModal() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.remove('show');
+    const overlay = document.querySelector('.overlay');
+    overlay.classList.remove('show');
 }
 
 function calculatePagination(page) {
@@ -192,7 +211,6 @@ function calculatePagination(page) {
             
             const button = document.createElement('button');
             if (i === current_page) {
-                console.log(current_page, i)
                 button.className = 'active';
             }
             button.addEventListener('click', calculatePagination);
@@ -236,11 +254,53 @@ window.onload = callBreedList("null").then((data) => {
     // When the page loads, check if there are any query parameters in the URL
     let params = new URLSearchParams(window.location.search);
 
-    // If there are, select the corresponding filters
-    let filters = {};
+    // If there are, select the corresponding filters - Top filters first
     if (params.has('filterZip')) {
-        console.log(params.get('filterZip'))
-        document.getElementById('zip').value = params.get('filterZip');
+        const petZip = document.getElementById('zip');
+        petZip.value = params.get('filterZip');
+        const petType = document.getElementById('pet-type');
+        const petTypes = petType.options;
+        for(var i = 0; i < petTypes.length; i++){
+            if (petTypes[i].value === params.get('filterType')) {
+                petType.selectedIndex = i;
+            }
+        }
+        const petBreed = document.getElementById('breed');
+        const petBreeds = petBreed.options;
+        for(var i = 0; i < petBreeds.length; i++){
+            if (petBreeds[i].value === params.get('filterBreed')) {
+                petBreed.selectedIndex = i;
+            }
+        }
+        callAnimalList().then((data) => {
+            buildResultsContainer(data);
+            // Populate Sidebar filters
+            const petRadius = document.getElementById('radius');
+            const petRadiusOptions = petRadius.options;
+            for(var i = 0; i < petRadiusOptions.length; i++){
+                if (petRadiusOptions[i].value === params.get('filterRadius')) {
+                    petRadius.selectedIndex = i;
+                }
+            }
+            const genderRadios = document.querySelectorAll('input[name="gender"]');
+            for(var i = 0; i < genderRadios.length; i++){
+                if (genderRadios[i].value === params.get('filterGender')) {
+                    genderRadios[i].checked = true;
+                }
+            }
+            const ageRadios = document.querySelectorAll('input[name="age"]');
+            for(var i = 0; i < ageRadios.length; i++){
+                if (ageRadios[i].value === params.get('filterAge')) {
+                    ageRadios[i].checked = true;
+                }
+            }
+            const sizeRadios = document.querySelectorAll('input[name="size"]');
+            for(var i = 0; i < sizeRadios.length; i++){
+                if (sizeRadios[i].value === params.get('filterSize')) {
+                    sizeRadios[i].checked = true;
+                }
+            }
+        });
     }
     
 });
@@ -282,7 +342,7 @@ function buildResultsList(animalList) {
     });
     setTimeout(() => {
         tempResultsBlock.style.removeProperty('height');
-      }, "400");
+    }, "400");
     
 }
 
@@ -291,6 +351,13 @@ function buildFilterSidebar(sidebar)  {
     filterLabel.className = 'sidebar-label';
     filterLabel.innerHTML = filtersLabel;
     sidebar.append(filterLabel);
+
+    // create mobile close button
+    const closeButton = document.createElement('button');
+    closeButton.className = 'modal-close';
+    closeButton.innerHTML = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.5 19.5L19.5 4.5" stroke="#09090D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M4.5 4.5L19.5 19.5" stroke="#09090D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>'
+    closeButton.addEventListener('click', closeModal);
+    sidebar.append(closeButton);
 
     // create clear button
     const clearButton = document.createElement('button');
@@ -387,7 +454,6 @@ function buildFilterSidebar(sidebar)  {
             });
         });
         let formattedAge = age[0].toLowerCase() + age.slice(1);
-        console.log(formattedAge?.replace(/\s+/g, '')?.replace(/\+/g, '')?.replace(/\//g, ''), placeholders)
         ageRadio.value = placeholders[formattedAge?.replace(/\s+/g, '')?.replace(/\+/g, '')?.replace(/\//g, '')];
         ageLabel.append(ageRadio);
         const checkmark = document.createElement('span');
@@ -398,7 +464,7 @@ function buildFilterSidebar(sidebar)  {
 
     // create size radio buttons
     const sizeBlock = document.createElement('div');
-    sizeBlock.className = 'radio-block';
+    sizeBlock.className = 'radio-block radio-size';
     const sizeLabelElement = document.createElement('div');
     sizeLabelElement.className = 'sidebar-header';
     sizeLabelElement.innerText = sizeLabel;
@@ -415,7 +481,10 @@ function buildFilterSidebar(sidebar)  {
         sizeRadio.type = 'radio';
         sizeRadio.name = 'size';
         sizeRadio.id = size;
-        sizeRadio.value = placeholders[size.toLowerCase()];
+        sizeRadio.value = placeholders[size.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
+            if (+match === 0) return "";
+            return index === 0 ? match.toLowerCase() : match.toUpperCase();
+        })];
         sizeRadio.addEventListener('click', () => {
             callAnimalList().then((data) => {
                 buildResultsContainer(data);
@@ -427,6 +496,27 @@ function buildFilterSidebar(sidebar)  {
         sizeLabel.append(checkmark);
         sizeBlock.append(sizeLabel);
     });
+
+    // create mobile filter buttons
+    const mobileContainer = document.createElement('div');
+    mobileContainer.className = 'mobile-button-container';
+
+    // create clear button
+    const clearMobileButton = document.createElement('button');
+    clearMobileButton.className = 'sidebar-clear-mobile';
+    clearMobileButton.innerHTML = clearLabel;
+    clearMobileButton.addEventListener('click', clearFilters);
+    mobileContainer.append(clearMobileButton);
+
+    // create filter button
+    const mobileFilterButton = document.createElement('button');
+    mobileFilterButton.className = 'filter-mobile';
+    mobileFilterButton.innerHTML = applyFiltersLabel;
+    mobileFilterButton.addEventListener('click', closeModal);
+    mobileContainer.append(mobileFilterButton);
+
+    sidebar.append(mobileContainer);
+
 }
  // Get filters
 function getFilters() {
@@ -451,7 +541,6 @@ function getFilters() {
 // Apply filters
     const applyFilters = function() {
     const filters = getFilters();
-    console.log(filters)
     // Update the URL with the selected filters as query parameters
     let params = new URLSearchParams(filters);
     window.history.replaceState({}, '', '?' + params.toString());
@@ -465,6 +554,7 @@ function buildResultsContainer(data) {
     if (resultsContainer) {
         resultsContainer.innerHTML = '';
     }
+    
     // show pagination
     const pagination = document.querySelector('.pagination.hidden');
     pagination?.classList.remove('hidden');
@@ -493,7 +583,29 @@ function buildResultsContainer(data) {
         const sidebar = document.createElement('div');
         sidebar.classList.add('sidebar');
         buildFilterSidebar(sidebar);
+        const overlay = document.createElement('div');
+        overlay.className = 'overlay';
+        tempResultsContainer.prepend(overlay);
         tempResultsContainer.prepend(sidebar);
+    }
+
+    // add sorted label
+    let sortLabel = document.querySelector('.sorted-label');
+    if (!sortLabel) {
+        const label = document.createElement('span');
+        label.className = 'sorted-label';
+        label.innerHTML = sortedLabel;
+        tempResultsContainer.append(label);
+    }
+
+    // add mobile filter buttton
+    const filterToggle = document.querySelector('.filter-button');
+    if (!filterToggle) {
+        const filterButton = document.createElement('button');
+        filterButton.className = 'filter-button';
+        filterButton.innerHTML = '<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_1990_3857)"><path d="M5.83008 12.4711H2.01074" stroke="#09090D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M23.4883 12.4711H10.7559" stroke="#09090D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M14.7441 20.5883H2.01074" stroke="#09090D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M23.4883 20.5883H19.6699" stroke="#09090D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M14.7441 4.49451H2.01074" stroke="#09090D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M23.4883 4.49451H19.6699" stroke="#09090D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M5.83008 12.4731C5.83008 12.7965 5.89379 13.1168 6.01756 13.4156C6.14134 13.7144 6.32276 13.986 6.55147 14.2147C6.78018 14.4434 7.0517 14.6248 7.35053 14.7486C7.64935 14.8724 7.96963 14.9361 8.29308 14.9361C8.61652 14.9361 8.9368 14.8724 9.23563 14.7486C9.53445 14.6248 9.80597 14.4434 10.0347 14.2147C10.2634 13.986 10.4448 13.7144 10.5686 13.4156C10.6924 13.1168 10.7561 12.7965 10.7561 12.4731C10.7561 12.1496 10.6924 11.8293 10.5686 11.5305C10.4448 11.2317 10.2634 10.9602 10.0347 10.7315C9.80597 10.5028 9.53445 10.3213 9.23563 10.1976C8.9368 10.0738 8.61652 10.0101 8.29308 10.0101C7.96963 10.0101 7.64935 10.0738 7.35053 10.1976C7.0517 10.3213 6.78018 10.5028 6.55147 10.7315C6.32276 10.9602 6.14134 11.2317 6.01756 11.5305C5.89379 11.8293 5.83008 12.1496 5.83008 12.4731Z" stroke="#09090D" stroke-width="1.5"/><path d="M14.7439 20.5901C14.7439 21.2433 15.0034 21.8698 15.4653 22.3317C15.9272 22.7936 16.5537 23.0531 17.2069 23.0531C17.8601 23.0531 18.4866 22.7936 18.9485 22.3317C19.4104 21.8698 19.6699 21.2433 19.6699 20.5901C19.6699 19.9368 19.4104 19.3104 18.9485 18.8485C18.4866 18.3866 17.8601 18.1271 17.2069 18.1271C16.5537 18.1271 15.9272 18.3866 15.4653 18.8485C15.0034 19.3104 14.7439 19.9368 14.7439 20.5901Z" stroke="#09090D" stroke-width="1.5"/><path d="M14.7439 4.35607C14.7439 5.00929 15.0034 5.63577 15.4653 6.09767C15.9272 6.55957 16.5537 6.81907 17.2069 6.81907C17.8601 6.81907 18.4866 6.55957 18.9485 6.09767C19.4104 5.63577 19.6699 5.00929 19.6699 4.35607C19.6699 3.70284 19.4104 3.07636 18.9485 2.61446C18.4866 2.15256 17.8601 1.89307 17.2069 1.89307C16.5537 1.89307 15.9272 2.15256 15.4653 2.61446C15.0034 3.07636 14.7439 3.70284 14.7439 4.35607Z" stroke="#09090D" stroke-width="1.5"/></g><defs><clipPath id="clip0_1990_3857"><rect width="24" height="24" fill="white" transform="translate(0.75 0.471069)"/></clipPath></defs></svg>' + ' ' + filterCta;
+        filterButton.addEventListener('click', openModal)
+        tempResultsContainer.append(filterButton)
     }
 
     current_page = 1;
@@ -540,8 +652,15 @@ export default async function decorate(block) {
     petTypeContainer.append(petTypeLabelElement);
     petTypeContainer.append(petTypeSelect);
     petTypeSelect.addEventListener('change', () => {
+        clearFilters();
         callBreedList(petTypeSelect.value.toLowerCase()).then((data) => {
             breedList = data;
+            const radioSize = document.querySelector('.radio-size');
+            if (petTypeSelect.value.toLowerCase() === "cat") {
+                radioSize?.classList.add('hidden');
+            } else {
+                radioSize?.classList.remove('hidden')
+            }
             updateBreedListSelect();
         });
     })
@@ -576,9 +695,17 @@ export default async function decorate(block) {
     zipInput.type = 'text';
     zipInput.name = 'zipPostal';
     zipInput.id = 'zip';
-    zipInput.pattern = `^\\d{5}(?:[-\\s]\\d{4})?$`;
+    zipInput.pattern = "[0-9]{5}";
     zipInput.required = true;
     zipInput.placeholder = zipPlaceholder;
+    zipInput.addEventListener('blur', () => {
+        var isValidZip = /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(zipInput.value);
+        if (isValidZip) {
+            callAnimalList().then((data) => {
+                buildResultsContainer(data);
+            });
+        }
+    })
     zipContainer.append(zipLabelElem);
     zipContainer.append(zipInput);
 
@@ -617,12 +744,17 @@ export default async function decorate(block) {
     button.className = 'adopt-search-button';
     button.textContent = searchAlertText;
 
+    const saveButton = document.createElement('button');
+    saveButton.className = 'adopt-save-search-button';
+    saveButton.innerHTML = '<svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_1997_2586)"><path d="M10 22.221C10.127 22.6537 10.3908 23.0336 10.7518 23.3039C11.1127 23.5741 11.5516 23.7202 12.0025 23.7202C12.4534 23.7202 12.8923 23.5741 13.2532 23.3039C13.6142 23.0336 13.878 22.6537 14.005 22.221" stroke="#09090D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M12 3.47104V1.22104" stroke="#09090D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M12 3.47104C13.9891 3.47104 15.8968 4.26122 17.3033 5.66774C18.7098 7.07426 19.5 8.98192 19.5 10.971C19.5 18.017 21 19.221 21 19.221H3C3 19.221 4.5 17.305 4.5 10.971C4.5 8.98192 5.29018 7.07426 6.6967 5.66774C8.10322 4.26122 10.0109 3.47104 12 3.47104Z" stroke="#09090D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></g><defs><clipPath id="clip0_1997_2586"><rect width="24" height="24" fill="white" transform="translate(0 0.471039)"/></clipPath></defs></svg>' + ' ' + createSearchAlert;
+    console.log(placeholders)
     form.append(petTypeContainer);
 
     form.append(breedContainer);
 
     form.append(zipContainer);
     form.append(button);
+    form.append(saveButton)
 
 
     const heroContainer = document.querySelector('.columns.hero');
