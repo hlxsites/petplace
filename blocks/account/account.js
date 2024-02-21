015ef399b3fc50be9b1b86f1b49e01ec95b3e67a
Jane Li <jane.li@herodigital.com>
2024-02-21 17:06:27 -0500

    PPAD-42: account details panel html/css

--- blocks/account/account.js
+++ blocks/account/account.js
@@ -7,7 +7,7 @@ async function createAccountDetailsPanel() {
     panelDiv.innerHTML = `
         <div class="tab-pannel-inner">
             <h3>Personal Information</h3>
-            <div class="account-form-container">
+            <div class="account-layout-container">
                 <form class="account-form account-form--personal" id="personal-info-form">
                     <div class="form-control form-control--text half-width">
                         <label for="fname">First Name</label>
@@ -30,10 +30,87 @@ async function createAccountDetailsPanel() {
                         <input type="text" id="zip" name="zip">
                     </div>
                     <div class="form-control form-control--submit">
-                        <input type="submit" value="Save Changes">
+                        <input type="submit" class="account-button" value="Save Changes">
                     </div>
                 </form>
             </div>
+            <h3>Change Password</h3>
+            <div class="account-layout-container">
+                <div class="account-layout-row account-layout-row--change-pwd">
+                    <p>Create a new account password</p>
+                    <a class="change-password-link account-button" href="">Change Password</a>
+                </div>
+            </div>
+            <h3>Communication Preferences</h3>
+            <div class="account-layout-container">
+                <form class="account-form account-form--preferences" id="perferences-form">
+                    <h3>Offers and Resources</h3>
+                    <div class="account-layout-row">
+                        <div class="account-layout-column">
+                            <h4>Newsletter</h4>
+                            <p>Receive our PetPlace Newsletter</p>
+                        </div>
+                        <div class="account-layout-column account-layout-column--form-controls">
+                            <div class="form-control form-control--checkbox">
+                                <label class="checkbox-container">Dog
+                                    <input type="checkbox" checked="checked" id="news-dog">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </div>
+                            <div class="form-control form-control--checkbox">
+                                <label class="checkbox-container">Cat
+                                    <input type="checkbox" id="news-cat">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="account-layout-row">
+                        <div class="account-layout-column">
+                            <h4>PetPlace Offers</h4>
+                            <p>Get updates on the latest PetPlace happenings</p>
+                        </div>
+                        <div class="account-layout-column">
+                        </div>
+                    </div>
+                    <div class="account-layout-row">
+                        <div class="account-layout-column">
+                            <h4>Partner Offers</h4>
+                            <p>Receive updates from PetPlace and our trusted partners</p>
+                        </div>
+                        <div class="account-layout-column">
+                            <div class="form-control form-control--switch">
+                                <label class="switch-container">
+                                    <input type="checkbox" checked="checked" id="alert-sms">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </div>
+                        </div>                
+                    </div>
+                    <h3>Notifications</h3>
+                    <div class="account-layout-row">
+                        <div class="account-layout-column">
+                            <h4>PetPlace Adopt Alerts</h4>
+                            <p>Receive adoption related updates</p>
+                        </div>
+                        <div class="account-layout-column account-layout-column--form-controls">
+                            <div class="form-control form-control--checkbox">
+                                <label class="checkbox-container">SMS
+                                    <input type="checkbox" checked="checked" id="alert-sms">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </div>
+                            <div class="form-control form-control--checkbox">
+                                <label class="checkbox-container">Email
+                                    <input type="checkbox" id="alert-email">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
         </div>
     
     `;
