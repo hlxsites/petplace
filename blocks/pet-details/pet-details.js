d7ff4d451f405d7764d13dc8cd1a5b66aa769f59
Jane Li <jane.li@herodigital.com>
2024-02-22 19:33:45 -0500

    PPAD-23: fix linting error

--- blocks/pet-details/pet-details.js
+++ blocks/pet-details/pet-details.js
@@ -130,14 +130,7 @@ function formatSimilarPetData(apiData) {
 }
 
 async function createCarouselSection(petName, images){
-    //const imageArr = images;
-        // example images for testing carousel
-    const imageArr = [
-        'https://www.petplace.com/article/breed/media_18690a7f17637edc779b59ac94cd3303b3c46d597.jpeg',
-        'https://www.petplace.com/article/dogs/just-for-fun/media_12c574158c76b42b855fdb1b3c983a546ccf22637.jpeg',
-        'https://www.petplace.com/article/dogs/pet-care/media_1d7035030f35833989f5b2f765eeb04c3c3539c07.jpeg',
-        'https://www.petplace.com/article/dogs/pet-care/media_13cb8037aa8ff514c96d9a08ced9d7773409c2947.jpeg'
-    ]
+    const imageArr = images;
     if(imageArr.length < 2 ) {
         const imageSectionContainer = document.createElement('div');
         imageSectionContainer.className = 'image-section';
