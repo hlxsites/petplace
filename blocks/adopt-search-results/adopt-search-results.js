60d44e71868617ed64f991c8b97354fdb272d02e
Ray Sanford <raymond.sanford@herodigital.com>
2024-02-23 11:16:50 -0500

    fixing build issue

--- blocks/adopt-search-results/adopt-search-results.js
+++ blocks/adopt-search-results/adopt-search-results.js
@@ -69,6 +69,7 @@ async function callAnimalList() {
     if (petType !== 'null') {
         animalType = petType;
     }
+    
     const breedType = document.getElementById('breed')?.value;
     let breed = null;
     if (breedType !== '') {
