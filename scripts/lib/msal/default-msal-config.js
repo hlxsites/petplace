a973da70dc6d6f8ac41e2dba81f952026e896671
Eric VanGeem <eric.vangeem@avionos.com>
2024-02-23 08:38:00 -0600

    initial azure b2c authentication.

---
+++ scripts/lib/msal/default-msal-config.js
@@ -0,0 +1,72 @@
+/**
+ * Configuration object to be passed to MSAL instance on creation. 
+ * For a full list of MSAL.js configuration parameters, visit:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
+ * For more details on using MSAL.js with Azure AD B2C, visit:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/working-with-b2c.md 
+ */
+
+import { b2cPolicies } from "./policies.js";
+
+export const msalConfig = {
+    auth: {
+      clientId: "680fcf6c-4c38-4d11-8963-d65de9aec0d9", // This is the ONLY mandatory field; everything else is optional.
+      authority: b2cPolicies.authorities.signUpSignIn.authority, // Choose sign-up/sign-in user-flow as your default.
+      knownAuthorities: [b2cPolicies.authorityDomain], // You must identify your tenant's domain as a known authority.
+
+      //TODO this should be a dynamic redirectUri based on the environment
+      redirectUri: "http://localhost:3000" // You must register this URI on Azure Portal/App Registration. Defaults to "window.location.href".
+    },
+    cache: {
+      cacheLocation: "localStorage", // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
+      storeAuthStateInCookie: false, // If you wish to store cache items in cookies as well as browser cache, set this to "true".
+    },
+    system: {
+      loggerOptions: {
+        loggerCallback: (level, message, containsPii) => {
+          if (containsPii) {
+            return;
+          }
+          switch (level) {
+            case msal.LogLevel.Error:
+              console.error(message);
+              return;
+            case msal.LogLevel.Info:
+              console.info(message);
+              return;
+            case msal.LogLevel.Verbose:
+              console.debug(message);
+              return;
+            case msal.LogLevel.Warning:
+              console.warn(message);
+              return;
+          }
+        }
+      }
+    }
+  };
+  
+/**
+ * Scopes you add here will be prompted for user consent during sign-in.
+ * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
+ * For more information about OIDC scopes, visit: 
+ * https://docs.microsoft.com/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
+ */
+export const loginRequest = {
+  scopes: ["https://petplacepoc.onmicrosoft.com/api/adopt.all", "openid"],
+};
+
+export const logoutRequest = {
+  postLogoutRedirectUri: window.location.href,
+  mainWindowRedirectUri: window.location.href
+};
+
+/**
+ * Scopes you add here will be used to request a token from Azure AD B2C to be used for accessing a protected resource.
+ * To learn more about how to work with scopes and resources, see: 
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
+ */
+export const tokenRequest = {
+  scopes: ["https://petplacepoc.onmicrosoft.com/api/adopt.all", "openid"], 
+  forceRefresh: false // Set this to "true" to skip a cached token and go to the server to get a new token
+};
---
+++ scripts/lib/msal/msal-authentication.js
@@ -0,0 +1,159 @@
+import { createDefaultMsalInstance } from './msal-instance.js';
+import { loginRequest, logoutRequest, tokenRequest } from './default-msal-config.js';
+import endPoints from '../../../variables/endpoints.js';   
+
+const msalInstance = createDefaultMsalInstance();
+
+// register a custom callback function (e.g. handleResponse()) once the user has successfully logged in and was redirected back to the site
+msalInstance.initialize().then(() => {
+    // Register Callbacks for Redirect flow
+    msalInstance.handleRedirectPromise().then(handleResponse).catch((error) => {
+        console.log(error);
+    });
+});
+
+/**
+ * 
+ * @param {*} callback individual features can pass their own custom callback function to be invoked after the user has successfully logged in, e.g. invoke the Favorite API for a pet.
+ * @param {*} featureName name of the feature that is invoking the login function, e.g. "Favorite". Used for logging purposes when a user signs up an account for the first time.
+ */
+export function login(callback, featureName) {
+    sessionStorage.setItem('originUrl', window.location.href);
+
+    // msalInstance.loginPopup(loginRequest)
+    msalInstance.loginRedirect(loginRequest)
+        .then((response) => handleResponse(response, callback, featureName))
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+export function logout() {
+    /**
+     * You can pass a custom request object below. This will override the initial configuration. For more information, visit:
+     * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/request-response-object.md#request
+     */
+
+    msalInstance.logoutPopup(logoutRequest);
+}
+
+export function acquireToken(customCallback) {
+    const accounts = msalInstance.getAllAccounts();
+
+    return new Promise((resolve, reject) => {
+        if (accounts.length === 1) {
+            msalInstance.acquireTokenSilent({ account: accounts[0], scopes: tokenRequest.scopes })
+                .then(function (accessTokenResponse) {
+                    // Acquire token silent success
+                    let accessToken = accessTokenResponse.accessToken;
+                    resolve(accessToken);
+                })
+                .catch(function (error) {
+                    //Acquire token silent failure, and send an interactive request
+                    if (error instanceof InteractionRequiredAuthError) {
+                        msalInstance
+                            .acquireTokenPopup(tokenRequest)
+                            .then(function (accessTokenResponse) {
+                                // Acquire token interactive success
+                                let accessToken = accessTokenResponse.accessToken;
+                                resolve(accessToken);
+                            })
+                            .catch(function (error) {
+                                // Acquire token interactive failure
+                                console.error(error);
+                                reject(error);
+                            });
+                    } else {
+                        console.log(error);
+                        reject(error);
+                    }
+                });
+        } else {
+            // prompt login if no token exists
+            if (customCallback) {
+                login(customCallback);
+            } else {
+                login();
+            }
+        }
+    });
+}
+
+function selectAccount() {
+    /**
+     * See here for more info on account retrieval: 
+     * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-common/docs/Accounts.md
+     */
+
+    const currentAccounts = msalInstance.getAllAccounts();
+
+    if (currentAccounts.length < 1) {
+        console.log('azure user not logged in');
+        return;
+    } else if (currentAccounts.length > 1) {
+
+        /**
+         * Due to the way MSAL caches account objects, the auth response from initiating a user-flow
+         * is cached as a new account, which results in more than one account in the cache. Here we make
+         * sure we are selecting the account with homeAccountId that contains the sign-up/sign-in user-flow, 
+         * as this is the default flow the user initially signed-in with.
+         */
+        const accounts = currentAccounts.filter(account =>
+            account.homeAccountId.toUpperCase().includes(b2cPolicies.names.signUpSignIn.toUpperCase())
+            &&
+            account.idTokenClaims.iss.toUpperCase().includes(b2cPolicies.authorityDomain.toUpperCase())
+            &&
+            account.idTokenClaims.aud === msalConfig.auth.clientId 
+            );
+
+        if (accounts.length > 1) {
+            // localAccountId identifies the entity for which the token asserts information.
+            if (accounts.every(account => account.localAccountId === accounts[0].localAccountId)) {
+                // All accounts belong to the same user
+                // setAccount(accounts[0]);
+            } else {
+                // Multiple users detected. Logout all to be safe.
+                logout();
+            };
+        } else if (accounts.length === 1) {
+            // setAccount(accounts[0]);
+        }
+
+    } else if (currentAccounts.length === 1) {
+        // setAccount(currentAccounts[0]);
+    }
+}
+
+function handleResponse(response, customCallback, featureName = 'PetPlace (Generic)') {
+    /**
+     * To see the full list of response object properties, visit:
+     * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/request-response-object.md#response
+     */
+
+    if (response !== null) {
+        // the 'newUser' flag is present for newly registered users that are logging in for the very first time.
+        if (response.account.idTokenClaims.newUser) {
+            // New user detected. Send POST request to create user in the database
+            fetch(`${endPoints.apiUrl}/adopt/api/User`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + response.accessToken,
+                },
+                body: featureName ? "\"" + featureName + "\"" : null
+            })
+            .catch((error) => {
+                console.error('/adopt/api/User Error:', error);
+            });
+        }
+
+        sessionStorage.removeItem('originUrl');
+        
+        // invoke custom callback if one was provided
+        if (customCallback) {
+          customCallback(response);
+        }
+    } else {
+        selectAccount();
+    }
+}
