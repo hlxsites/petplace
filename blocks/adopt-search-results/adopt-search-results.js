421c50b0ac06ce5312a5ad11504a04557635b9b1
Ray Sanford <raymond.sanford@herodigital.com>
2024-02-23 15:08:30 -0500

    adding state to pet cards

--- blocks/adopt-search-results/adopt-search-results.js
+++ blocks/adopt-search-results/adopt-search-results.js
@@ -199,7 +199,7 @@ function buildResultsList(animalList) {
         p.innerText = `${animal.Gender} • ${animal.Breed}`;
         const animalLocation = document.createElement('p');
         animalLocation.className = 'location';
-        animalLocation.innerHTML = `${animal.City}`;
+        animalLocation.innerHTML = `${animal.City}, ${animal.State}`;
         div.append(anchor);
         div.append(animalName);
         div.append(p);
