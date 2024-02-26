7558a2d7e8eae9d02cf724a1a88b5ee77f5cd112
Jane Li <jane.li@herodigital.com>
2024-02-26 15:15:15 -0500

    PPAD-42: add tabs

--- blocks/account/account.js
+++ blocks/account/account.js
@@ -1,140 +1,210 @@
+import TabsManual from "./tabs-manual.js";
 
-import { CheckboxSwitch } from './checkbox-switch.js';
-
-function createSwitch(inputId, label, ariaLabelledBy) {
+function createSwitch(inputId, ariaLabelledBy) {
     const switchDiv = document.createElement('div');
     switchDiv.className = 'switch-container';
-    switchDiv.innerHTML = `
-        ${label ? `<label><span class='switch-label'>${label}</span>` : ''}
-        <input id=${inputId || ''} type='checkbox' role='switch'${!label && ariaLabelledBy ? ` aria-labelledby = ${ariaLabelledBy}`: ''}>
-        <span class='switch-state'>
-            <span class='on' aria-hidden='true'>On</span>
-            <span class='off' aria-hidden='true'>Off</span>
-            <span class='switch-slider'>
-                <span class='switch-slider-position'></span>
-            </span>
-        </span>
-        ${label ? `</label>` : ''}
+    const inputEl = document.createElement('input');
+    inputEl.id = inputId;
+    inputEl.setAttribute('role', 'switch');
+    inputEl.setAttribute('type', 'checkbox');
+    inputEl.setAttribute('aria-labelledby', ariaLabelledBy);
+    switchDiv.append(inputEl);
+    const stateSpan = document.createElement('span');
+    stateSpan.className = 'switch-state';
+    stateSpan.innerHTML = `
+    <span class='on' aria-hidden='true'>On</span>
+    <span class='off' aria-hidden='true'>Off</span>
+    <span class='switch-slider'>
+        <span class='switch-slider-position'></span>
+    </span>
     `;
-    new CheckboxSwitch(switchDiv);
+    switchDiv.append(stateSpan);
+    const checkboxSwitch = switchDiv.querySelector('input[role=\'switch\']');
+    if (checkboxSwitch) {
+        checkboxSwitch.addEventListener('focus', (event) => {
+            event.currentTarget.parentNode.classList.add('focus');
+        });
+        checkboxSwitch.addEventListener('blur', (event) => {
+            event.currentTarget.parentNode.classList.remove('focus');
+        });
+    }
     return switchDiv;
 }
 
 async function createAccountDetailsPanel() {
-
     const panelDiv = document.createElement('div');
-    panelDiv.className = 'tab-panel';
+    panelDiv.className = 'tab-pannel-inner';
     panelDiv.innerHTML = `
-        <div class='tab-pannel-inner'>
-            <h3>Personal Information</h3>
-            <div class='account-layout-container'>
-                <form class='account-form account-form--personal' id='personal-info-form'>
-                    <div class='form-control form-control--text half-width'>
-                        <label for='fname'>First Name</label>
-                        <input type='text' id='fname' name='fname'>
-                    </div>
-                    <div class='form-control form-control--text half-width'>
-                        <label for='lname'>Last Name</label>
-                        <input type='text' id='lname' name='lname'>
-                    </div>
-                    <div class='form-control form-control--text'>
-                        <label for='email'>Email</label>
-                        <input type='text' id='email' name='email'>
-                    </div>
-                    <div class='form-control form-control--text half-width'>
-                        <label for='phone'>Phone Number</label>
-                        <input type='text' id='phone' name='phone'>
-                    </div>
-                    <div class='form-control form-control--text half-width'>
-                        <label for='zip'>Zip/Postal Code</label>
-                        <input type='text' id='zip' name='zip'>
-                    </div>
-                    <div class='form-control form-control--submit'>
-                        <input type='submit' class='account-button' value='Save Changes'>
-                    </div>
-                </form>
-            </div>
-            <h3>Change Password</h3>
-            <div class='account-layout-container'>
-                <div class='account-layout-row account-layout-row--change-pwd'>
-                    <p>Create a new account password</p>
-                    <a class='change-password-link account-button' href=''>Change Password</a>
+        <h3>Personal Information</h3>
+        <div class='account-layout-container'>
+            <form class='account-form account-form--personal' id='personal-info-form'>
+                <div class='form-control form-control--text half-width'>
+                    <label for='fname'>First Name</label>
+                    <input type='text' id='fname' name='fname'>
                 </div>
+                <div class='form-control form-control--text half-width'>
+                    <label for='lname'>Last Name</label>
+                    <input type='text' id='lname' name='lname'>
+                </div>
+                <div class='form-control form-control--text'>
+                    <label for='email'>Email</label>
+                    <input type='text' id='email' name='email'>
+                </div>
+                <div class='form-control form-control--text half-width'>
+                    <label for='phone'>Phone Number</label>
+                    <input type='text' id='phone' name='phone'>
+                </div>
+                <div class='form-control form-control--text half-width'>
+                    <label for='zip'>Zip/Postal Code</label>
+                    <input type='text' id='zip' name='zip'>
+                </div>
+                <div class='form-control form-control--submit'>
+                    <input type='submit' class='account-button' value='Save Changes'>
+                </div>
+            </form>
+        </div>
+        <h3>Change Password</h3>
+        <div class='account-layout-container'>
+            <div class='account-layout-row account-layout-row--change-pwd'>
+                <p>Create a new account password</p>
+                <a class='change-password-link account-button' href=''>Change Password</a>
             </div>
-            <h3>Communication Preferences</h3>
-            <div class='account-layout-container'>
-                <form class='account-form account-form--preferences' id='perferences-form'>
-                    <h3>Offers and Resources</h3>
-                    <div class='account-layout-row'>
-                        <div class='account-layout-column'>
-                            <h4>Newsletter</h4>
-                            <p>Receive our PetPlace Newsletter</p>
+        </div>
+        <h3>Communication Preferences</h3>
+        <div class='account-layout-container'>
+            <form class='account-form account-form--preferences' id='perferences-form'>
+                <h3>Offers and Resources</h3>
+                <div class='account-layout-row'>
+                    <div class='account-layout-column'>
+                        <h4>Newsletter</h4>
+                        <p>Receive our PetPlace Newsletter</p>
+                    </div>
+                    <div class='account-layout-column account-layout-column--form-controls'>
+                        <div class='form-control form-control--checkbox'>
+                            <label class='checkbox-container'>Dog
+                                <input type='checkbox' checked='checked' id='news-dog'>
+                                <span class='checkmark'></span>
+                            </label>
                         </div>
-                        <div class='account-layout-column account-layout-column--form-controls'>
-                            <div class='form-control form-control--checkbox'>
-                                <label class='checkbox-container'>Dog
-                                    <input type='checkbox' checked='checked' id='news-dog'>
-                                    <span class='checkmark'></span>
-                                </label>
-                            </div>
-                            <div class='form-control form-control--checkbox'>
-                                <label class='checkbox-container'>Cat
-                                    <input type='checkbox' id='news-cat'>
-                                    <span class='checkmark'></span>
-                                </label>
-                            </div>
+                        <div class='form-control form-control--checkbox'>
+                            <label class='checkbox-container'>Cat
+                                <input type='checkbox' id='news-cat'>
+                                <span class='checkmark'></span>
+                            </label>
                         </div>
                     </div>
-                    <div class='account-layout-row'>
-                        <div class='account-layout-column'>
-                            <h4>PetPlace Offers</h4>
-                            <p>Get updates on the latest PetPlace happenings</p>
+                </div>
+                <div class='account-layout-row'>
+                    <div class='account-layout-column'>
+                        <h4 id='petplace-offers-label'>PetPlace Offers</h4>
+                        <p>Get updates on the latest PetPlace happenings</p>
+                    </div>
+                    <div class='account-layout-column'>
+                        ${createSwitch('petplace-offers-switch', 'petplace-offers-label').outerHTML}
+                    </div>
+                </div>
+                <div class='account-layout-row'>
+                    <div class='account-layout-column'>
+                        <h4 id='partner-offers-label'>Partner Offers</h4>
+                        <p>Receive updates from PetPlace and our trusted partners</p>
+                    </div>
+                    <div class='account-layout-column'>
+                        <div class='form-control form-control--switch'>
+                            ${createSwitch('partner-offers-switch', 'partner-offers-label').outerHTML}
                         </div>
-                        <div class='account-layout-column'>
+                    </div>                
+                </div>
+                <h3>Notifications</h3>
+                <div class='account-layout-row'>
+                    <div class='account-layout-column'>
+                        <h4>PetPlace Adopt Alerts</h4>
+                        <p>Receive adoption related updates</p>
+                    </div>
+                    <div class='account-layout-column account-layout-column--form-controls'>
+                        <div class='form-control form-control--checkbox'>
+                            <label class='checkbox-container'>SMS
+                                <input type='checkbox' checked='checked' id='alert-sms'>
+                                <span class='checkmark'></span>
+                            </label>
+                        </div>
+                        <div class='form-control form-control--checkbox'>
+                            <label class='checkbox-container'>Email
+                                <input type='checkbox' id='alert-email'>
+                                <span class='checkmark'></span>
+                            </label>
                         </div>
                     </div>
-                    <div class='account-layout-row'>
-                        <div class='account-layout-column'>
-                            <h4 id='partner-offers-label'>Partner Offers</h4>
-                            <p>Receive updates from PetPlace and our trusted partners</p>
-                        </div>
-                        <div class='account-layout-column'>
-                            <div class='form-control form-control--switch'>
-                                ${createSwitch('partner-offers-switch', null, 'partner-offers-label').outerHTML}
-                            </div>
-                        </div>                
-                    </div>
-                    <h3>Notifications</h3>
-                    <div class='account-layout-row'>
-                        <div class='account-layout-column'>
-                            <h4>PetPlace Adopt Alerts</h4>
-                            <p>Receive adoption related updates</p>
-                        </div>
-                        <div class='account-layout-column account-layout-column--form-controls'>
-                            <div class='form-control form-control--checkbox'>
-                                <label class='checkbox-container'>SMS
-                                    <input type='checkbox' checked='checked' id='alert-sms'>
-                                    <span class='checkmark'></span>
-                                </label>
-                            </div>
-                            <div class='form-control form-control--checkbox'>
-                                <label class='checkbox-container'>Email
-                                    <input type='checkbox' id='alert-email'>
-                                    <span class='checkmark'></span>
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
+                </div>
+            </form>
         </div>
     `;
     return panelDiv;
 }
 
+async function createSearchAlertsPanel() {
+    const panelDiv = document.createElement('div');
+    panelDiv.className = 'account-tabpanel-inner';
+    return panelDiv;
+}
 
+async function createFavoritesPanel() {
+    const panelDiv = document.createElement('div');
+    panelDiv.className = 'account-tabpanel-inner';
+    return panelDiv;
+}
+async function createSurveyPanel() {
+    const panelDiv = document.createElement('div');
+    panelDiv.className = 'account-tabpanel-inner';
+    return panelDiv;
+}
+
+async function createTabComponent() {
+    const tabTitles = ['Account Details', 'Search Alerts', 'Favorites', 'Pet Match Survey'];
+    const tabPanels = [await createAccountDetailsPanel(), await createSearchAlertsPanel(), await createFavoritesPanel(), await createSurveyPanel()];
+    const tabs = document.createElement('div');
+    tabs.className = 'account-tabs';
+    const title = document.createElement('h2');
+    title.className = 'account-tabs-title';
+    title.textContent = 'My Account';
+    tabs.append(title);
+    const tablist =  document.createElement('div');
+    tablist.className = 'account-tablist';
+    tablist.setAttribute('role', 'tablist');
+    tabTitles.forEach((tab, index) => {
+        const btn  = document.createElement('button');
+        btn.className = `account-tab account-tab--${tab.toLowerCase().split(' ').join('-')}`;
+        btn.setAttribute('type', 'button');
+        btn.setAttribute('role', 'tab');
+        btn.setAttribute('id', `account-tab-${index}`);
+        btn.setAttribute('aria-selected', `${index === 0 ? 'true' : 'false'}`);
+        btn.setAttribute('aria-controls', `account-tabpanel-${index}`);
+        const iconEl = document.createElement('span');
+        iconEl.className = 'account-tab-icon';
+        const textEl = document.createElement('span');
+        textEl.className = 'account-tab-text';
+        textEl.textContent = tab;
+        btn.append(iconEl, textEl);
+        tablist.append(btn);
+    });
+    tabs.append(tablist);
+    tabPanels.forEach((panel, index) => {
+        const panelWrapper = document.createElement('div');
+        panelWrapper.className = `account-tabpanel account-tabpanel--${tabTitles[index].toLowerCase().split(' ').join('-')}${index === 0 ? '' : ' is-hidden'}`
+        panelWrapper.setAttribute('id', `account-tabpanel-${index}`);
+        panelWrapper.setAttribute('role', 'tabpanel');
+        panelWrapper.setAttribute('aria-labelledby', `account-tab-${index}`);
+        panelWrapper.append(panel);
+        tabs.append(panelWrapper);
+    });
+    const tabListEl = tabs.querySelector('[role=\'tablist\']');
+    const tabPanelEls = tabs.querySelectorAll('[role=\'tabpanel\']')
+    new TabsManual(tabListEl, tabPanelEls);
+    return tabs;
+
+
+}
 export default async function decorate(block) {
     block.innerHTML = '';
-    block.append(await createAccountDetailsPanel());
+    block.append(await createTabComponent());
 
 }
---
+++ blocks/account/tabs-manual.js
@@ -0,0 +1,105 @@
+export default class TabsManual {
+    constructor(groupNode, panelNodes) {
+      this.tablistNode = groupNode;
+      this.tabs = [];
+      this.firstTab = null;
+      this.lastTab = null;
+      this.tabs = Array.from(this.tablistNode.querySelectorAll('[role=tab]'));
+      this.tabpanels = Array.from(panelNodes);
+  
+      for (let i = 0; i < this.tabs.length; i += 1) {
+        const tab = this.tabs[i];
+  
+        tab.tabIndex = -1;
+        tab.setAttribute('aria-selected', 'false');
+  
+        tab.addEventListener('keydown', this.onKeydown.bind(this));
+        tab.addEventListener('click', this.onClick.bind(this));
+  
+        if (!this.firstTab) {
+          this.firstTab = tab;
+        }
+        this.lastTab = tab;
+      }
+  
+      this.setSelectedTab(this.firstTab);
+    }
+  
+    setSelectedTab(currentTab) {
+      for (let i = 0; i < this.tabs.length; i += 1) {
+        const tab = this.tabs[i];
+        if (currentTab === tab) {
+          tab.setAttribute('aria-selected', 'true');
+          tab.removeAttribute('tabindex');
+          this.tabpanels[i].classList.remove('is-hidden');
+        } else {
+          tab.setAttribute('aria-selected', 'false');
+          tab.tabIndex = -1;
+          this.tabpanels[i].classList.add('is-hidden');
+        }
+      }
+    }
+  
+    moveFocusToTab(currentTab) {
+      currentTab.focus();
+    }
+  
+    moveFocusToPreviousTab(currentTab) {
+      var index;
+  
+      if (currentTab === this.firstTab) {
+        this.moveFocusToTab(this.lastTab);
+      } else {
+        index = this.tabs.indexOf(currentTab);
+        this.moveFocusToTab(this.tabs[index - 1]);
+      }
+    }
+  
+    moveFocusToNextTab(currentTab) {
+      let index; 
+      if (currentTab === this.lastTab) {
+        this.moveFocusToTab(this.firstTab);
+      } else {
+        index = this.tabs.indexOf(currentTab);
+        this.moveFocusToTab(this.tabs[index + 1]);
+      }
+    }
+  
+    /* EVENT HANDLERS */
+    onKeydown(event) {
+      const tgt = event.currentTarget;
+      let flag = false;
+  
+      switch (event.key) {
+        case 'ArrowLeft':
+          this.moveFocusToPreviousTab(tgt);
+          flag = true;
+          break;
+        case 'ArrowRight':
+          this.moveFocusToNextTab(tgt);
+          flag = true;
+          break;
+        case 'Home':
+          this.moveFocusToTab(this.firstTab);
+          flag = true;
+          break;
+        case 'End':
+          this.moveFocusToTab(this.lastTab);
+          flag = true;
+          break;
+        default:
+          break;
+      }
+  
+      if (flag) {
+        event.stopPropagation();
+        event.preventDefault();
+      }
+    }
+  
+    // Since this example uses buttons for the tabs, the click onr also is activated
+    // with the space and enter keys
+    onClick(event) {
+      this.setSelectedTab(event.currentTarget);
+    }
+}
