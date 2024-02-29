41b4e8bc64844294475bc1ca9297d8b069a98391
Jane Li <jane.li@herodigital.com>
2024-02-29 18:42:43 -0500

    PPAD-42: add user api calls

--- blocks/account/account.js
+++ blocks/account/account.js
@@ -1,13 +1,58 @@
 import TabsManual from "./tabs-manual.js";
+import endPoints from '../../variables/endpoints.js';
+import { acquireToken } from '../../scripts/lib/msal/msal-authentication.js';
 
-function createSwitch(inputId, ariaLabelledBy) {
+function serialize(data) {
+	let obj = {};
+	for (let [key, value] of data) {
+		if (obj[key] !== undefined) {
+			if (!Array.isArray(obj[key])) {
+				obj[key] = [obj[key]];
+			}
+			obj[key].push(value);
+		} else {
+			obj[key] = value;
+		}
+	}
+	return obj;
+}
+function refactorPreferenceForm(formObj) {
+    const formatted = {
+        EmailOptIn: false,
+        SmsOptIn: false,
+        PetPlaceOffer: false,
+        PartnerOffer: false,
+        CatNewsletterOptIn: false,
+        DogNewsletterOptIn: false,
+    }
+    for (const property in formObj){
+        if (formObj[property] === 'on') {
+            formatted[property] = true;
+        }
+    }
+    return formatted;
+}
+function toggleButtonStates(buttons, disabled = true) {
+    buttons.forEach(button => {
+        if (disabled) {
+            button.setAttribute('disabled', 'disabled');
+        } else {
+            button.removeAttribute('disabled');
+        }
+    });
+}
+function createSwitch(inputId, accessibilityLabel, isOn = false) {
     const switchDiv = document.createElement('div');
     switchDiv.className = 'switch-container';
     const inputEl = document.createElement('input');
     inputEl.id = inputId;
+    inputEl.name = inputId;
     inputEl.setAttribute('role', 'switch');
     inputEl.setAttribute('type', 'checkbox');
-    inputEl.setAttribute('aria-labelledby', ariaLabelledBy);
+    inputEl.setAttribute('aria-label', accessibilityLabel);
+    if (isOn) {
+        inputEl.setAttribute('checked', 'checked');
+    }
     switchDiv.append(inputEl);
     const stateSpan = document.createElement('span');
     stateSpan.className = 'switch-state';
@@ -31,7 +76,20 @@ function createSwitch(inputId, ariaLabelledBy) {
     return switchDiv;
 }
 
-async function createAccountDetailsPanel() {
+async function createAccountDetailsPanel(userData) {
+    const { 
+        FirstName,
+        LastName,
+        Email,
+        ZipCode,
+        PhoneNumber,
+        CatNewsletterOptIn,
+        DogNewsletterOptIn,
+        EmailOptIn,
+        PartnerOffer,
+        PetPlaceOffer,
+        SmsOptIn,
+    } = userData;
     const panelDiv = document.createElement('div');
     panelDiv.className = 'tab-pannel-inner';
     panelDiv.innerHTML = `
@@ -40,26 +98,26 @@ async function createAccountDetailsPanel() {
             <form class='account-form account-form--personal' id='personal-info-form'>
                 <div class='form-control form-control--text half-width'>
                     <label for='fname'>First Name</label>
-                    <input type='text' id='fname' name='fname'>
+                    <input type='text' id='FirstName' name='FirstName' value=${FirstName}>
                 </div>
                 <div class='form-control form-control--text half-width'>
                     <label for='lname'>Last Name</label>
-                    <input type='text' id='lname' name='lname'>
+                    <input type='text' id='LastName' name='LastName' value=${LastName}>
                 </div>
                 <div class='form-control form-control--text'>
                     <label for='email'>Email</label>
-                    <input type='text' id='email' name='email'>
+                    <input type='text' id='Email' name='email' value=${Email} disabled>
                 </div>
                 <div class='form-control form-control--text half-width'>
                     <label for='phone'>Phone Number</label>
-                    <input type='text' id='phone' name='phone'>
+                    <input type='text' id='PhoneNumber' name='PhoneNumber' placeholder="Enter your phone number" value=${PhoneNumber || ''}>
                 </div>
                 <div class='form-control form-control--text half-width'>
                     <label for='zip'>Zip/Postal Code</label>
-                    <input type='text' id='zip' name='zip'>
+                    <input type='text' id='ZipCode' name='ZipCode' value=${ZipCode}>
                 </div>
                 <div class='form-control form-control--submit'>
-                    <input type='submit' class='account-button' value='Save Changes'>
+                    <button type='submit' class='account-button' value='Save Changes' disabled>Save Changes</button>
                 </div>
             </form>
         </div>
@@ -72,23 +130,23 @@ async function createAccountDetailsPanel() {
         </div>
         <h3>Communication Preferences</h3>
         <div class='account-layout-container'>
-            <form class='account-form account-form--preferences' id='perferences-form'>
+            <form class='account-form account-form--preferences' id='preferences-form'>
                 <h3>Offers and Resources</h3>
                 <div class='account-layout-row'>
                     <div class='account-layout-column'>
                         <h4>Newsletter</h4>
-                        <p>Receive our PetPlace Newsletter</p>
+                        <p class="text-supporting">Receive our PetPlace Newsletter</p>
                     </div>
                     <div class='account-layout-column account-layout-column--form-controls'>
                         <div class='form-control form-control--checkbox'>
                             <label class='checkbox-container'>Dog
-                                <input type='checkbox' checked='checked' id='news-dog'>
+                                <input type='checkbox' id='DogNewsletterOptIn' name='DogNewsletterOptIn' ${DogNewsletterOptIn ? 'checked' :''}>
                                 <span class='checkmark'></span>
                             </label>
                         </div>
                         <div class='form-control form-control--checkbox'>
                             <label class='checkbox-container'>Cat
-                                <input type='checkbox' id='news-cat'>
+                                <input type='checkbox' id='CatNewsletterOptIn' name='CatNewsletterOptIn' ${CatNewsletterOptIn ? 'checked' :''}>
                                 <span class='checkmark'></span>
                             </label>
                         </div>
@@ -96,21 +154,21 @@ async function createAccountDetailsPanel() {
                 </div>
                 <div class='account-layout-row'>
                     <div class='account-layout-column'>
-                        <h4 id='petplace-offers-label'>PetPlace Offers</h4>
-                        <p>Get updates on the latest PetPlace happenings</p>
+                        <h4>PetPlace Offers</h4>
+                        <p class="text-supporting">Get updates on the latest PetPlace happenings</p>
                     </div>
                     <div class='account-layout-column'>
-                        ${createSwitch('petplace-offers-switch', 'petplace-offers-label').outerHTML}
+                        ${createSwitch('PetPlaceOffer', 'PetPlace Offers', PetPlaceOffer).outerHTML}
                     </div>
                 </div>
                 <div class='account-layout-row'>
                     <div class='account-layout-column'>
                         <h4 id='partner-offers-label'>Partner Offers</h4>
-                        <p>Receive updates from PetPlace and our trusted partners</p>
+                        <p class="text-supporting">Receive updates from PetPlace and our trusted partners</p>
                     </div>
                     <div class='account-layout-column'>
                         <div class='form-control form-control--switch'>
-                            ${createSwitch('partner-offers-switch', 'partner-offers-label').outerHTML}
+                            ${createSwitch('PartnerOffer', 'Partner Offers', PartnerOffer).outerHTML}
                         </div>
                     </div>                
                 </div>
@@ -118,18 +176,18 @@ async function createAccountDetailsPanel() {
                 <div class='account-layout-row'>
                     <div class='account-layout-column'>
                         <h4>PetPlace Adopt Alerts</h4>
-                        <p>Receive adoption related updates</p>
+                        <p class="text-supporting">Receive adoption related updates</p>
                     </div>
                     <div class='account-layout-column account-layout-column--form-controls'>
                         <div class='form-control form-control--checkbox'>
                             <label class='checkbox-container'>SMS
-                                <input type='checkbox' checked='checked' id='alert-sms'>
+                                <input type='checkbox' id='SmsOptIn' name='SmsOptIn' ${SmsOptIn ? 'checked' : ''}>
                                 <span class='checkmark'></span>
                             </label>
                         </div>
                         <div class='form-control form-control--checkbox'>
                             <label class='checkbox-container'>Email
-                                <input type='checkbox' id='alert-email'>
+                                <input type='checkbox' id='EmailOptIn' name='EmailOptIn' ${EmailOptIn ? 'checked' : ''}>
                                 <span class='checkmark'></span>
                             </label>
                         </div>
@@ -137,7 +195,7 @@ async function createAccountDetailsPanel() {
                 </div>
                 <div class='account-layout-row'>
                     <div class='form-control form-control--submit'>
-                        <input type='submit' class='account-button' value='Save Changes'>
+                        <button type='submit' class='account-button' value='Save Changes' disabled>Save Changes</button>
                     </div>
                 </div>
             </form>
@@ -163,14 +221,14 @@ async function createSurveyPanel() {
     return panelDiv;
 }
 
-async function createTabComponent() {
+async function createTabComponent(accountDetails) {
     const tabArray = [
         {title: 'Account Details', hash: 'details'},
         {title: 'Search Alerts', hash: 'searchalerts'},
         {title: 'Favorites', hash: 'favorites'},
         {title: 'Pet Match Survey', hash: 'survey'},
     ];
-    const tabPanels = [await createAccountDetailsPanel(), await createSearchAlertsPanel(), await createFavoritesPanel(), await createSurveyPanel()];
+    const tabPanels = [await createAccountDetailsPanel(accountDetails), await createSearchAlertsPanel(), await createFavoritesPanel(), await createSurveyPanel()];
     const tabs = document.createElement('div');
     tabs.className = 'account-tabs';
     const title = document.createElement('h2');
@@ -238,8 +296,66 @@ async function createTabComponent() {
 
 
 }
+async function callUserApi(token, apiUrl, method = 'GET', payload = null) {
+    let result = null;
+    const config = {
+        method: method,
+        headers: {Authorization: `Bearer ${token}`},
+    };
+    if (method !== 'GET') {
+        config.body = JSON.stringify(payload);
+    }
+    try {
+        const resp = await fetch(apiUrl, config);
+        if (resp.ok) {
+            result = await resp.json();
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+    return result;
+}
+
 export default async function decorate(block) {
     block.innerHTML = '';
-    block.append(await createTabComponent());
+    let initialUserData = {};
+    const token = await acquireToken();
+    const baseUrl = endPoints.apiUrl;
+    const userApi = `${baseUrl}/adopt/api/User`;
+    if (token) {
+        initialUserData = await callUserApi(token, userApi);
+        console.log('initialUserData:', initialUserData)
+        block.append(await createTabComponent(initialUserData));
+        const personalInfoForm = block.querySelector('#personal-info-form');
+        const preferencesForm = block.querySelector('#preferences-form');
+        const textInputs = block.querySelectorAll('input[type=\'text\']:not(:disabled)');
+        const checkboxes = block.querySelectorAll('input[type=\'checkbox\']');
+        const submitButtons = block.querySelectorAll('button[type=\'submit\']');
+        textInputs.forEach(input => {
+            input.addEventListener('keypress', (event) => {
+                if (event.key === "Enter") {
+                    event.preventDefault();
+                } 
+            });
+            input.addEventListener('input', (event) => {
+                if (event.target.value !== initialUserData[event.target.name]) {
+                    submitButtons.forEach(button => {button.removeAttribute('disabled')});
+                } else {
+                    submitButtons.forEach(button => {button.setAttribute('disabled', 'disabled')});
+                }
+            });
+        });
+        submitButtons.forEach(button => {
+            button.addEventListener('click', async (event) => {
+                event.preventDefault();
+                const payLoad = {...serialize(new FormData(personalInfoForm)), ...refactorPreferenceForm(serialize(new FormData(preferencesForm)))};
+                const response = await callUserApi(token, userApi, 'PUT', payLoad);
+            });
+        })
+
+    }
+
+
+
 
 }
