eb450d743cabf62a33c301638c0b1a6a12b79bca
carolinesampaio-herodigital <161368342+carolinesampaio-herodigital@users.noreply.github.com>
2024-03-07 17:06:08 -0300

    [PPAD-63] Account - Favorite Animals Page (#423)
    Fix #PPAD-63
    
    Test URLs:
     - http://localhost:3000/pet-adoption/account > click on favorites panel
    
    ![Screenshot 2024-03-07 at 15 54
    30](https://github.com/hlxsites/petplace/assets/161368342/27c01ebd-8a9c-4d56-a185-27ef2eb2260d)
    ![Screenshot 2024-03-07 at 15 54
    53](https://github.com/hlxsites/petplace/assets/161368342/49e0b35f-a846-4b0f-8d2d-f7f46cd3f008)
    ![Screenshot 2024-03-07 at 15 55
    04](https://github.com/hlxsites/petplace/assets/161368342/0fc26d78-b87f-4137-a7f3-36d6991d5dce)
    
    ![Screenshot 2024-03-07 at 16 48
    04](https://github.com/hlxsites/petplace/assets/161368342/7ff5d1d4-103a-42ab-b380-ff08be92c2dc)

--- blocks/account/account.js
+++ blocks/account/account.js
@@ -3,6 +3,7 @@ import TabsManual from './tabs-manual.js';
 import endPoints from '../../variables/endpoints.js';
 import { acquireToken } from '../../scripts/lib/msal/msal-authentication.js';
 import { bindAccountDetailsEvents, createAccountDetailsPanel } from './account-details.js';
+import { createAccountFavoritesPanel } from './account-favorites.js';
 
 async function createTabComponent() {
     const tabArray = [
@@ -109,6 +110,7 @@ export default async function decorate(block) {
         initialUserData = await callUserApi(token);
         block.append(await createTabComponent());
         block.querySelector('#details').append(await createAccountDetailsPanel(initialUserData));
+        block.querySelector('#favorites').append(await createAccountFavoritesPanel(token));
         await bindAccountDetailsEvents(block, token, initialUserData);
     }
 }
