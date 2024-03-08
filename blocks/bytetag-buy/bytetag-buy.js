51ec5bf37bea3b63a30dad4f953283e3ae586dee
Tom Scott <tom.scott@herodigital.com>
2024-03-08 09:40:35 -0800

    Fixing ByteTag shopify script on iOS

--- blocks/bytetag-buy/bytetag-buy.js
+++ blocks/bytetag-buy/bytetag-buy.js
@@ -2,9 +2,12 @@ import {loadShopifyBuy} from '../../scripts/third-party.js';
 
 export default async function decorate(block) {
   const container = block.parentElement.parentElement;
-  const bytetagId = container.dataset.bytetagId;
+  let bytetagId = container.dataset.bytetagId;
   const bytetagToken = container.dataset.bytetagToken;
   if(bytetagId && bytetagToken) {
+    if(bytetagId.indexOf('tel:') !== -1){
+      bytetagId = bytetagId.replace('tel:', '');
+    }
     loadShopifyBuy(bytetagId, block, bytetagToken)
   }
 }
