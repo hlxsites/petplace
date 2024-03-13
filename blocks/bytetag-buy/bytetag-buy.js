4533d28972e32d97917f5aa6fc708126b8026927
Tom Scott <tom.scott@herodigital.com>
2024-03-13 12:38:44 -0700

    Fixing more linting errors

--- blocks/bytetag-buy/bytetag-buy.js
+++ blocks/bytetag-buy/bytetag-buy.js
@@ -1,13 +1,13 @@
-import { loadShopifyBuy } from "../../scripts/third-party.js";
+import { loadShopifyBuy } from '../../scripts/third-party.js';
 
 export default async function decorate(block) {
   const container = block.parentElement.parentElement;
-  let {
+  const {
     dataset: { bytetagId, bytetagToken },
   } = container;
   if (bytetagId && bytetagToken) {
-    if (bytetagId.indexOf("tel:") !== -1) {
-      bytetagId = bytetagId.replace("tel:", "");
+    if (bytetagId.indexOf('tel:') !== -1) {
+      bytetagId = bytetagId.replace('tel:', '');
     }
     loadShopifyBuy(bytetagId, block, bytetagToken);
   }
--- scripts/third-party.js
+++ scripts/third-party.js
@@ -36,26 +36,8 @@ export function loadDelayed() {
 }
 
 export function loadShopifyBuy(id, node, accessToken) {
-  const scriptURL = "https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js";
-  if (window.ShopifyBuy) {
-    if (window.ShopifyBuy.UI) {
-      ShopifyBuyInit();
-    } else {
-      loadScript();
-    }
-  } else {
-    loadScript();
-  }
-  function loadScript() {
-    var script = document.createElement("script");
-    script.async = true;
-    script.src = scriptURL;
-    (
-      document.getElementsByTagName("head")[0] ||
-      document.getElementsByTagName("body")[0]
-    ).appendChild(script);
-    script.onload = ShopifyBuyInit;
-  }
+  const scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+
   function ShopifyBuyInit() {
     var client = ShopifyBuy.buildClient({
       domain: "bytetag.myshopify.com",
@@ -314,4 +296,24 @@ export function loadShopifyBuy(id, node, accessToken) {
       });
     });
   }
+  function loadShopifyScript() {
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = scriptURL;
+    (
+      document.getElementsByTagName("head")[0] ||
+      document.getElementsByTagName("body")[0]
+    ).appendChild(script);
+    script.onload = ShopifyBuyInit;
+  }
+  if (window.ShopifyBuy) {
+    if (window.ShopifyBuy.UI) {
+      ShopifyBuyInit();
+    } else {
+      loadShopifyScript();
+    }
+  } else {
+    loadShopifyScript();
+  }
+ 
 }
