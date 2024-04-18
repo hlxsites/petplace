6e5b8c878456846f7dbc114136b741646df8c415
ankyc-phi <146023473+ankyc-phi@users.noreply.github.com>
2024-04-18 10:31:06 -0400

    feat: deployment for sprint8 (#484)
    Includes:
    PM-144: updating favicon (#477) 
    feat: adopt landing page changes (#483)
    
    Jira Issues:
    https://pethealthinc.atlassian.net/browse/PM-256
    https://pethealthinc.atlassian.net/browse/PM-144
    
    Test URLs:
    - Before: https://main--petplace--hlxsites.hlx.page/
    - After:
      - https://sprint8-deploy--petplace--hlxsites.hlx.live/
      - https://sprint8-deploy--petplace--hlxsites.hlx.live/?martech=off
    
    ---------
    
    Co-authored-by: Diego-Gomez-code <69477459+Diego-Gomez-code@users.noreply.github.com>

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -968,7 +968,6 @@ async function loadLazy(doc) {
     { id: 'footer', label: getPlaceholder('skipFooter') },
   ]);
 
-  addFavIcon(`${window.hlx.codeBasePath}/styles/favicon.svg`);
   sampleRUM('lazy');
   sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
   sampleRUM.observe(main.querySelectorAll('picture > img'));
