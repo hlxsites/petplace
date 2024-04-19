75015558565a1e8b560c69b16627b69cebbf68be
carolinesampaio-herodigital <caroline.sampaio@herodigital.com>
2024-04-18 21:39:16 -0300

    PPAD-243: Created logic to show modal with specific message when user tries to create a duplicated search.

--- scripts/adoption/saveSearch.js
+++ scripts/adoption/saveSearch.js
@@ -12,6 +12,10 @@ const {
   continueSearch,
   searchAlertFail,
   searchAlertFailDetail,
+  duplicatedSearchModalTitle,
+  duplicatedSearchModalDescription,
+  duplicatedSearchModalBackButton,
+  duplicatedSearchModalManageButton,
 } = placeholders;
 
 function openModal() {
@@ -23,6 +27,20 @@ function openModal() {
 
 function closeModal() {
   const modal = document.querySelector('.save-search-modal');
+  modal.classList.remove('show');
+  const overlay = document.querySelector('.overlay');
+  overlay.classList.remove('show');
+}
+
+function openDuplicatedSearchModal() {
+  const modal = document.querySelector('#duplicated-search-modal');
+  modal.classList.add('show');
+  const overlay = document.querySelector('.overlay');
+  overlay.classList.add('show');
+}
+
+function closeDuplicatedSearchModal() {
+  const modal = document.querySelector('#duplicated-search-modal');
   modal.classList.remove('show');
   const overlay = document.querySelector('.overlay');
   overlay.classList.remove('show');
@@ -85,6 +103,23 @@ export async function saveSearch(token) {
     });
   }
 
+  const arrSavedSearchesIds = [];
+  let currentSearchId = 0;
+  let isDuplicatedSearch = false;
+  let maxAlertReached = false;
+
+  await fetch(`${endPoints.apiUrl}/adopt/api/UserSearch`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((response1) => response1.json()).then(async (data) => {
+    data.forEach((item) => {
+      arrSavedSearchesIds.push(item.Id);
+    });
+  });
+
   const response = await fetch(`${endPoints.apiUrl}/adopt/api/UserSearch`, {
     method: 'POST',
     headers: {
@@ -110,8 +145,21 @@ export async function saveSearch(token) {
         },
       },
     }),
+  }).then((searchId) => searchId.json()).then((data) => {
+    if (typeof data === 'number') {
+      currentSearchId = data;
+      if (arrSavedSearchesIds.includes(parseInt(currentSearchId, 10))) {
+        isDuplicatedSearch = true;
+      }
+    } else {
+      maxAlertReached = true;
+    }
   });
-  if (response.status === 204 || response.status === 400 || response.status === 500) {
+
+  if (
+    maxAlertReached
+    || (response && (response.status === 204 || response.status === 400 || response.status === 500))
+  ) {
     // build and show error modal
     let modal = document.getElementById('saved-search-modal');
     const resultsContainer = document.getElementById('results-container');
@@ -155,81 +203,132 @@ export async function saveSearch(token) {
     resultsContainer.append(modal);
     openModal();
   } else {
-    // build and show success modal
-    let modal = document.getElementById('saved-search-modal');
-    const resultsContainer = document.getElementById('results-container');
-    if (!modal) {
-      modal = document.createElement('div');
-      modal.id = 'saved-search-modal';
-      modal.className = 'save-search-modal';
-    } else {
-      modal.innerHTML = '';
-    }
+    // eslint-disable-next-line no-lonely-if
+    if (isDuplicatedSearch) {
+      // build and show search duplicated modal
+      const savedSearchModal = document.getElementById('saved-search-modal');
+      if (savedSearchModal) {
+        savedSearchModal.innerHTML = '';
+        savedSearchModal.classList.remove('show');
+      }
+      let modal = document.getElementById('duplicated-search-modal');
+      const resultsContainer = document.getElementById('results-container');
+      if (!modal) {
+        modal = document.createElement('div');
+        modal.id = 'duplicated-search-modal';
+        modal.className = 'duplicate-search-modal';
+      } else {
+        modal.innerHTML = '';
+      }
 
-    // create success icon
-    const successIcon = document.createElement('div');
-    successIcon.className = 'success-icon';
-    successIcon.innerHTML = `<svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0 32C0 14.3269 14.3269 0 32 0C49.6731 0 64 14.3269 64 32C64 49.6731 49.6731 64 32 64C14.3269 64 0 49.6731 0 32Z" fill="#EDFCF2"/>
-          <path d="M47 16.9987L26.8773 45.744C26.6118 46.1255 26.2595 46.4384 25.8493 46.6571C25.4391 46.8757 24.9829 46.9937 24.5181 47.0014C24.0534 47.009 23.5935 46.9062 23.1763 46.7012C22.7591 46.4963 22.3966 46.1952 22.1187 45.8227L17 38.9987" stroke="#1A6132" stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round"/>
-          </svg>`;
+      const modalTitle = document.createElement('h3');
+      modalTitle.innerText = duplicatedSearchModalTitle;
+      modalTitle.className = 'modal-title';
 
-    const modalTitle = document.createElement('h3');
-    modalTitle.innerText = searchAlertCreated;
-    modalTitle.className = 'modal-title';
+      const modalSubtitle = document.createElement('div');
+      modalSubtitle.innerText = duplicatedSearchModalDescription;
+      modalSubtitle.className = 'modal-subtitle';
 
-    const modalSubtitle = document.createElement('div');
-    modalSubtitle.innerText = searchFilters;
-    modalSubtitle.className = 'modal-subtitle';
+      const buttonContainer = document.createElement('div');
+      buttonContainer.className = 'button-container';
 
-    // build filter string
-
-    const filterArray = [];
-    breedSelectedOptions.forEach((filter) => {
-      filterArray.push(filter.value);
-    });
-    const ageFilters = document.querySelectorAll('.sidebar input:checked');
-    ageFilters.forEach((filter) => {
-      filterArray.push(filter.parentElement.innerText);
-    });
-    let filterString = '';
-    if (filterArray.length > 0) {
-      filterString += ' including ';
-      filterArray.forEach((filter) => {
-        filterString += `${filter}, `;
+      const manageButton = document.createElement('button');
+      manageButton.className = 'manage-button';
+      manageButton.innerText = duplicatedSearchModalManageButton;
+      manageButton.addEventListener('click', () => {
+        window.location.href = '/pet-adoption/account#searchalerts';
       });
-      filterString = filterString.slice(0, -2);
+
+      const continueButton = document.createElement('button');
+      continueButton.className = 'continue-button';
+      continueButton.innerText = duplicatedSearchModalBackButton;
+      continueButton.addEventListener('click', closeDuplicatedSearchModal);
+
+      buttonContainer.append(manageButton);
+      buttonContainer.append(continueButton);
+
+      modal.append(modalTitle);
+      modal.append(modalSubtitle);
+      modal.append(buttonContainer);
+      resultsContainer.append(modal);
+      openDuplicatedSearchModal();
+    } else {
+      // build and show success modal
+      let modal = document.getElementById('saved-search-modal');
+      const resultsContainer = document.getElementById('results-container');
+      if (!modal) {
+        modal = document.createElement('div');
+        modal.id = 'saved-search-modal';
+        modal.className = 'save-search-modal';
+      } else {
+        modal.innerHTML = '';
+      }
+
+      // create success icon
+      const successIcon = document.createElement('div');
+      successIcon.className = 'success-icon';
+      successIcon.innerHTML = `<svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 32C0 14.3269 14.3269 0 32 0C49.6731 0 64 14.3269 64 32C64 49.6731 49.6731 64 32 64C14.3269 64 0 49.6731 0 32Z" fill="#EDFCF2"/>
+            <path d="M47 16.9987L26.8773 45.744C26.6118 46.1255 26.2595 46.4384 25.8493 46.6571C25.4391 46.8757 24.9829 46.9937 24.5181 47.0014C24.0534 47.009 23.5935 46.9062 23.1763 46.7012C22.7591 46.4963 22.3966 46.1952 22.1187 45.8227L17 38.9987" stroke="#1A6132" stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>`;
+
+      const modalTitle = document.createElement('h3');
+      modalTitle.innerText = searchAlertCreated;
+      modalTitle.className = 'modal-title';
+
+      const modalSubtitle = document.createElement('div');
+      modalSubtitle.innerText = searchFilters;
+      modalSubtitle.className = 'modal-subtitle';
+
+      // build filter string
+
+      const filterArray = [];
+      breedSelectedOptions.forEach((filter) => {
+        filterArray.push(filter.value);
+      });
+      const ageFilters = document.querySelectorAll('.sidebar input:checked');
+      ageFilters.forEach((filter) => {
+        filterArray.push(filter.parentElement.innerText);
+      });
+      let filterString = '';
+      if (filterArray.length > 0) {
+        filterString += ' including ';
+        filterArray.forEach((filter) => {
+          filterString += `${filter}, `;
+        });
+        filterString = filterString.slice(0, -2);
+      }
+
+      const filters = document.createElement('div');
+      filters.className = 'saved-filters';
+      filters.innerText = `${petType !== 'null' ? `${petType}s` : 'Pets'} near ${zip} ${filterString}`;
+
+      const buttonContainer = document.createElement('div');
+      buttonContainer.className = 'button-container';
+
+      const manageButton = document.createElement('button');
+      manageButton.className = 'manage-button';
+      manageButton.innerText = manageSearchAlerts;
+      manageButton.addEventListener('click', () => {
+        window.location.href = '/pet-adoption/account#searchalerts';
+      });
+
+      const continueButton = document.createElement('button');
+      continueButton.className = 'continue-button';
+      continueButton.innerText = continueSearch;
+      continueButton.addEventListener('click', closeModal);
+
+      buttonContainer.append(manageButton);
+      buttonContainer.append(continueButton);
+
+      modal.append(successIcon);
+      modal.append(modalTitle);
+      modal.append(modalSubtitle);
+      modal.append(filters);
+      modal.append(buttonContainer);
+      resultsContainer.append(modal);
+      openModal();
     }
-
-    const filters = document.createElement('div');
-    filters.className = 'saved-filters';
-    filters.innerText = `${petType !== 'null' ? `${petType}s` : 'Pets'} near ${zip} ${filterString}`;
-
-    const buttonContainer = document.createElement('div');
-    buttonContainer.className = 'button-container';
-
-    const manageButton = document.createElement('button');
-    manageButton.className = 'manage-button';
-    manageButton.innerText = manageSearchAlerts;
-    manageButton.addEventListener('click', () => {
-      window.location.href = '/pet-adoption/account#searchalerts';
-    });
-
-    const continueButton = document.createElement('button');
-    continueButton.className = 'continue-button';
-    continueButton.innerText = continueSearch;
-    continueButton.addEventListener('click', closeModal);
-
-    buttonContainer.append(manageButton);
-    buttonContainer.append(continueButton);
-
-    modal.append(successIcon);
-    modal.append(modalTitle);
-    modal.append(modalSubtitle);
-    modal.append(filters);
-    modal.append(buttonContainer);
-    resultsContainer.append(modal);
-    openModal();
   }
 }
 
