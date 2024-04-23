c6cf9de7d5a372400ffd4a6c01bf402b8a64c531
Julien Ramboz <ramboz@adobe.com>
2024-04-23 16:32:00 -0700

    test: add unit test boilerplate with basic martech checks

--- tests/martech.spec.js
+++ tests/martech.spec.js
@@ -1,12 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { test } from '@playwright/test';
 
-const [org, repo] = process.env.GITHUB_REPOSITORY?.split('/') || [];
-const ref = process.env.GITHUB_HEAD_REF;
-const baseURL = process.env.CI ? `https://${ref}--${repo}--${org}.hlx.live/` : 'http://127.0.0.1:3000/';
-
 test('Google Tag Manager is properly loaded', async ({ page }) => {
-  await page.goto(baseURL);
+  await page.goto('/');
   await Promise.all([
     page.waitForRequest('https://www.googletagmanager.com/gtm.js?id=GTM-WP2SGNL', { timeout: 60000 }),
     page.waitForRequest('https://www.googletagmanager.com/gtag/js?id=AW-11334653569', { timeout: 60000 }),
@@ -15,6 +11,6 @@ test('Google Tag Manager is properly loaded', async ({ page }) => {
 });
 
 test('Events are sent to Google Analytics backend', async ({ page }) => {
-  await page.goto(baseURL);
+  await page.goto('/');
   await page.waitForRequest(/https:\/\/analytics.google.com\/g\/collect\?/, { timeout: 60000 });
 });
