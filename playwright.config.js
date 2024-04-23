57ef6a79ff1894e620ff94c6cbaa03711a153251
Julien Ramboz <ramboz@adobe.com>
2024-04-23 16:42:15 -0700

    test: add unit test boilerplate with basic martech checks

--- playwright.config.js
+++ playwright.config.js
@@ -24,8 +24,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: /* process.env.CI ? 'dot' : */ 'list',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: process.env.CI ? 'dot' : 'list',
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.CI
@@ -36,7 +35,7 @@ export default defineConfig({
     trace: 'on-first-retry',
   },
 
-  /* Configure projects for major browsers */
+  /* Configure the tests for the top 3 browsers that access the site */
   projects: [
     {
       name: 'Mobile Safari',
