77c09e1f3154fb7d2b8c815c6d3150408fec1907
Cristiano Amici <cristiano.amici@herodigital.com>
2024-05-29 14:14:36 -0500

    feat(PM-298): Address PR feedback
    Simplify logic for Partner Offer block

--- blocks/partner-offer/partner-offer.js
+++ blocks/partner-offer/partner-offer.js
@@ -4,12 +4,10 @@ export default async function decorate(block) {
     return;
   }
 
-  block.children[0].classList.add('partner-featured-title');
-  block.children[1].classList.add('partner-logo');
-  block.children[2].classList.add('partner-name');
-  block.children[3].classList.add('partner-data');
-  block.children[3].querySelector('p:first-child').classList.add('partner-data-title');
-  block.children[4].classList.add('partner-cta');
+  ['featured-title', 'logo', 'name', 'data', 'cta'].forEach((name, index) => {
+    block.children[index]?.classList.add(`partner-${name}`);
+  });
+  block.querySelector('.partner-data p:first-child')?.classList.add('partner-data-title');
 
   const featuredTitle = block.children[0].innerText;
   block.setAttribute('data-featured-title', featuredTitle);
