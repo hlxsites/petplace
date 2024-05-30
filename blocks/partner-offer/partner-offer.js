6d44319fc4c0d20fe5fd215532caef9372606104
Cristiano Amici <amicicristiano@gmail.com>
2024-05-30 15:23:33 -0500

    feat: insurance page template (#503)
    Please always provide the [GitHub issue(s)](../issues) your PR is for,
    as well as test URLs where your change can be observed (before and
    after):
    
    This PR adds support for the new Insurance Page template, as well as
    adding new blocks, and a minor adjustment to the Table block to make
    properly render as a table element (based on the latest AEM Block
    Collection code:
    [https://github.com/adobe/aem-block-collection/tree/main/blocks/table](https://github.com/adobe/aem-block-collection/tree/main/blocks/table)).
    
    Fix: PM-298
    
    Test URLs:
    - Before:
    https://feat-insurance-page-template--petplace--hlxsites.hlx.page/new-pet-insurance/best-pet-insurance-california
    - After:
    -
    https://feat-insurance-page-template--petplace--hlxsites.hlx.page/new-pet-insurance/best-pet-insurance-california
    -
    https://feat-insurance-page-template--petplace--hlxsites.hlx.page/new-pet-insurance/best-pet-insurance-california?martech=off
    
    Screenshots
    ![Screenshot 2024-05-27 at 11 39
    39 AM](https://github.com/hlxsites/petplace/assets/4684558/6f93377d-c5cd-4220-8286-b769c9176d61)
    ![Screenshot 2024-05-27 at 11 39
    44 AM](https://github.com/hlxsites/petplace/assets/4684558/596d1f8f-0b63-44bf-87f2-792c9426a992)
    ![Screenshot 2024-05-27 at 11 39
    49 AM](https://github.com/hlxsites/petplace/assets/4684558/7e3cc501-c2e1-4804-aa52-e052d2d22caf)
    ![Screenshot 2024-05-27 at 11 40
    30 AM](https://github.com/hlxsites/petplace/assets/4684558/3872c455-5da1-4e4f-9375-2abb9e26a443)
    ![Screenshot 2024-05-27 at 11 40
    40 AM](https://github.com/hlxsites/petplace/assets/4684558/7059ceb6-a35d-4d65-8cbb-c6acfdca4fd1)
    
    ---------
    
    Co-authored-by: ankyc-phi <146023473+ankyc-phi@users.noreply.github.com>

---
+++ blocks/partner-offer/partner-offer.js
@@ -0,0 +1,14 @@
+export default async function decorate(block) {
+  if (block.children.length < 5) {
+    console.warn('Invalid structure for partner-offer block');
+    return;
+  }
+
+  ['featured-title', 'logo', 'name', 'data', 'cta'].forEach((name, index) => {
+    block.children[index]?.classList.add(`partner-${name}`);
+  });
+  block.querySelector('.partner-data p:first-child')?.classList.add('partner-data-title');
+
+  const featuredTitle = block.children[0].innerText;
+  block.setAttribute('data-featured-title', featuredTitle);
+}
