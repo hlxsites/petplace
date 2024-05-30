373c4272eb4aa0b9ab0aee03a47e9e050753906e
ankyc-phi <ankit.curchorcar@pethealthinc.com>
2024-05-30 03:01:02 -0400

    template files for new homepage

---
+++ templates/homepage-new/homepage-new.js
@@ -0,0 +1,16 @@
+import { decorateResponsiveImages } from '../../scripts/scripts.js';
+
+export default function loadLazy(document) {
+  const imgDiv = document.querySelector('.home-banner').querySelector('div')
+    .children[1];
+  decorateResponsiveImages(imgDiv);
+
+  const main = document.querySelector('main');
+  console.log(main);
+  const bgWaveDiv = document.createElement('div');
+  bgWaveDiv.append(main.children[0]);
+  // add this in for when "explore" is live
+  // bgWaveDiv.append(main.children[0]);
+  bgWaveDiv.className = 'home-banner-bg';
+  main.prepend(bgWaveDiv);
+}
