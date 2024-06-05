36864a4ed2dbef59ee9f7d18f0bc39841249082d
ankyc-phi <ankit.curchorcar@pethealthinc.com>
2024-06-05 15:57:24 -0400

    fixed linting issue for line endings

--- blocks/home-banner/home-banner.js
+++ blocks/home-banner/home-banner.js
@@ -1,9 +1,11 @@
 export default function decorate(block) {
+  const ctaContainer = block.querySelector('.button-container');
+  const ctaLink = ctaContainer.querySelector('a');
+
   // Make entire block clickable.
   block.onclick = () => {
-    const link = ctaLink.querySelector('a');
-    if (link) {
-      link.click();
+    if (ctaLink) {
+      ctaLink.click();
     }
   };
 }
