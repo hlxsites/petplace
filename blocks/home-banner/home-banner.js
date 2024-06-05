b8531052c2e09325291d71374858d545765e6f8d
sagar-pethealth <107197116+sagar-pethealth@users.noreply.github.com>
2024-06-06 01:23:19 +0530

    PM#97 - Homepage - Image Clickability (#518)
    Please always provide the [GitHub issue(s)](../issues) your PR is for,
    as well as test URLs where your change can be observed (before and
    after):
    
    Fix PM#97
    
    Test URLs:
    - Before: https://main--petplace--hlxsites.hlx.page/
    - After:
      - https://pm-97-image-clickability--petplace--hlxsites.hlx.live/
    -
    https://pm-97-image-clickability--petplace--hlxsites.hlx.live/?martech=off
    
    ---------
    
    Co-authored-by: ankyc-phi <ankit.curchorcar@pethealthinc.com>
    Co-authored-by: ankyc-phi <146023473+ankyc-phi@users.noreply.github.com>

--- blocks/home-banner/home-banner.js
+++ blocks/home-banner/home-banner.js
@@ -2,4 +2,12 @@ export default function decorate(block) {
   const ctaLink = block.querySelector('.button-container');
   const arrow = document.createElement('p');
   ctaLink.append(arrow);
+
+  // Make entire block clickable.
+  block.onclick = () => {
+    const link = ctaLink.querySelector('a');
+    if (link) {
+      link.click();
+    }
+  };
 }
