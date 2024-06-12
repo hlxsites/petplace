7d4215bbb96ae05765df6e49addeb6ea88dbe3ab
Darlan Tódero ten Caten <darlantc@me.com>
2024-06-12 20:48:22 -0300

    feat: fixed react redirect route for all children paths

--- react-client/src/routes/AppRoute.tsx
+++ react-client/src/routes/AppRoute.tsx
@@ -1,42 +1,49 @@
 import { useEffect } from "react";
-import { createBrowserRouter, Outlet, RouterProvider, useNavigate, useSearchParams } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  useNavigate,
+  useSearchParams,
+} from "react-router-dom";
 
 const Home = () => {
-    const [searchParams] = useSearchParams();
-    const navigate = useNavigate()
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const redirectFrom = searchParams.get('redirectFrom');
-      if (redirectFrom){
-        navigate(redirectFrom);
-      }
-    }, [searchParams, navigate])
+  useEffect(() => {
+    const redirectFrom = searchParams.get("redirectFrom");
+    if (redirectFrom) {
+      navigate(redirectFrom);
+    }
+  }, [searchParams, navigate]);
 
-    return (
-        <div>Hello world!
-            <Outlet />
-        </div>
-    )
-}
+  return (
+    <div>
+      Hello world!
+      <Outlet />
+    </div>
+  );
+};
 
 const routes = [
-    {
-        path: "/",
-        element: <Home />,
-                    errorElement: <p>Not found</p>,
-        children: [
-            {
-                path: "account",
-                element: <p>Account</p>,
-              },
-            ]
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <p>Not found</p>,
+    children: [
+      {
+        path: "account",
+        element: <p>Account</p>,
       },
+    ],
+  },
+];
 
+const router = createBrowserRouter(routes, {
+  basename: "/react-test",
+});
 
-]
-
-const router = createBrowserRouter(routes);
-
-  export const AppRouter = (): JSX.Element => {
-    return <RouterProvider router={router} />;
-  };
+export const AppRouter = (): JSX.Element => {
+  return <RouterProvider router={router} />;
+};
