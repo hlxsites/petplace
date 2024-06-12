5b9a01a999bcb91f5846fb704efaff232c066867
Cristiano Amici <cristiano.amici@herodigital.com>
2024-06-12 10:26:34 -0500

    feat(PM-277): Implement Popular Tags dropdown dialog

--- blocks/popular-tags/popular-tags.js
+++ blocks/popular-tags/popular-tags.js
@@ -1,13 +1,23 @@
 import { decorateIcons } from '../../scripts/lib-franklin.js';
 import { getPlaceholder } from '../../scripts/scripts.js';
 
+/**
+ * Determine the maximum amount of tags to be displayed,
+ * based on the data attribute provided and current viewport.
+ * The remaining tags will be opened via the Show More dialog.
+ *
+ * @param {Element} block The popular tags block
+ * @returns {number | undefined} The maximum amount of tags to be displayed, if any.
+ */
 function getMaxTags(block) {
+  // List of supported viewports
   const mediaObj = {
     mobile: 0,
     tablet: 768,
     desktop: 1024,
   };
 
+  // Check the block's data attributes to determine the max tags amount to be applied.
   const { viewport } = Object.entries(mediaObj).reduce((acc, [val, minWidth]) => {
     if (minWidth > acc.minWidth && document.documentElement.clientWidth >= minWidth) {
       return { viewport: val, minWidth };
@@ -19,14 +29,23 @@ function getMaxTags(block) {
   return maxTags;
 }
 
-function updateTagDisplay(block) {
+/**
+ * Hide tags based on the authored data attributes and viewport,
+ * and move them to the dropdown dialog.
+ * @param {Element} block The popular tags block
+ * @param {Element} dropdown The dialog container's element
+ */
+function updateTagDisplay(block, dropdown) {
   const maxTags = getMaxTags(block);
   const tags = block.querySelectorAll('li');
+  const dropdownTags = dropdown.querySelectorAll('li');
   tags.forEach((tag, idx) => {
     if (maxTags && idx >= maxTags) {
       tag.setAttribute('hidden', true);
+      dropdownTags[idx].removeAttribute('hidden');
     } else {
       tag.removeAttribute('hidden');
+      dropdownTags[idx].setAttribute('hidden', true);
     }
   });
 
@@ -35,6 +54,26 @@ function updateTagDisplay(block) {
     button.setAttribute('hidden', true);
   } else {
     button.removeAttribute('hidden');
+  }
+}
+
+/**
+ * Display/hide the dropdown dialog and update the button status.
+ * @param {boolean} status Whether to show or hide the dialog
+ * @param {Element} buttonEl The button element
+ * @param {Element} containerEl The dropdown container element
+ */
+function setDropdownStatus(status, buttonEl, containerEl) {
+  if (status) {
+    buttonEl.classList.add('active');
+    containerEl.removeAttribute('hidden');
+    containerEl.style.top = `${buttonEl.getBoundingClientRect().top + buttonEl.clientHeight + 8}px`;
+    containerEl.style.right = `${document.documentElement.clientWidth - buttonEl.getBoundingClientRect().right}px`;
+  } else {
+    buttonEl.classList.remove('active');
+    containerEl.setAttribute('hidden', true);
+    containerEl.style.top = null;
+    containerEl.style.right = null;
   }
 }
 
@@ -53,6 +92,44 @@ export default async function decorate(block) {
   block.querySelector('ul').append(moreTagsTrigger);
   decorateIcons(block);
 
-  updateTagDisplay(block);
-  window.addEventListener('resize', () => updateTagDisplay(block));
+  // Create the dropdown dialog and copy the tags list.
+  const dropdownContainer = document.createElement('div');
+  dropdownContainer.classList.add('popular-tags-dropdown');
+  dropdownContainer.setAttribute('hidden', true);
+  const dropdownList = document.createElement('ul');
+  dropdownContainer.append(dropdownList);
+  block.querySelectorAll('li').forEach((tag) => {
+    const dropdownTag = tag.cloneNode(true);
+    dropdownList.append(dropdownTag);
+  });
+  document.querySelector('body').append(dropdownContainer);
+
+  // Hide the tags depending on the current viewport.
+  updateTagDisplay(block, dropdownContainer);
+
+  // Toggle the dialog status on clicking the Show More button.
+  moreTagsTrigger.addEventListener('click', () => {
+    const isActive = moreTagsTrigger.classList.contains('active');
+    setDropdownStatus(!isActive, moreTagsTrigger, dropdownContainer);
+  });
+
+  // Close the dialog if the user clicks outside.
+  window.addEventListener('click', (e) => {
+    const dropdownEls = [moreTagsTrigger, dropdownContainer];
+    if (!dropdownEls.find((el) => el.contains(e.target))) {
+      setDropdownStatus(false, moreTagsTrigger, dropdownContainer);
+    }
+  });
+
+  // Update the tags displayed on resize (due to viewport changes)
+  window.addEventListener('resize', () => {
+    updateTagDisplay(block, dropdownContainer);
+
+    // If the dialog is open, update the dialog location to keep it
+    // consistent with newest viewport width.
+    const isActive = moreTagsTrigger.classList.contains('active');
+    if (isActive) {
+      setDropdownStatus(true, moreTagsTrigger, dropdownContainer);
+    }
+  });
 }
