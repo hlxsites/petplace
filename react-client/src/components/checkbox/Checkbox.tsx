c81cf437eaa482ced1a8bcb3a453b1b5bdd75064
Mateus Pereira <mateusp.s@outlook.com>
2024-06-25 10:56:21 -0300

    refactor: styling from css to tailwind

--- react-client/src/components/checkbox/Checkbox.tsx
+++ react-client/src/components/checkbox/Checkbox.tsx
@@ -1,6 +1,6 @@
 import classNames from "classnames";
 import * as RadixCheckbox from "@radix-ui/react-checkbox";
-import "./styles.css";
+import { Icon } from "../icon/Icon";
 
 export type CheckboxProps = {
   asChild?: boolean;
@@ -15,40 +15,19 @@ export type CheckboxProps = {
   value: string;
 };
 
-const Checkbox = ({ className, ...props }: CheckboxProps) => (
-  <RadixCheckbox.Root
-    className={classNames("CheckboxRoot", className)}
-    {...props}
-  >
-    <RadixCheckbox.Indicator className="CheckboxIndicator">
-      <CheckSvg />
-    </RadixCheckbox.Indicator>
-  </RadixCheckbox.Root>
-);
-
-const CheckSvg = () => {
+const Checkbox = ({ className, ...props }: CheckboxProps) => {
   return (
-    <svg
-      width="20"
-      height="20"
-      viewBox="0 0 20 20"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+    <RadixCheckbox.Root
+      className={classNames(
+        "p-0 flex h-5 w-5 items-center justify-center rounded-md border-solid border-[#6E6D73] bg-white focus:outline-none",
+        className
+      )}
+      {...props}
     >
-      <g id="Checkbox">
-        <rect id="Rectangle 2" width="20" height="20" rx="6" fill="#C74D2F" />
-        <g id="Check">
-          <path
-            id="Vector"
-            d="M14.6875 5.31213L8.39917 14.2951C8.3162 14.4143 8.20609 14.5121 8.07791 14.5804C7.94973 14.6487 7.80715 14.6856 7.66192 14.688C7.51669 14.6904 7.37297 14.6582 7.2426 14.5942C7.11223 14.5302 6.99895 14.436 6.91208 14.3196L5.3125 12.1871"
-            stroke="white"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </g>
-      </g>
-    </svg>
+      <RadixCheckbox.Indicator className="absolute flex h-5 w-5 items-center justify-center rounded-md bg-[#C74D3F] text-white">
+        <Icon display="check" />
+      </RadixCheckbox.Indicator>
+    </RadixCheckbox.Root>
   );
 };
 
