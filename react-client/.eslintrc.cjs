c495d52f7e16a7c871d6c99e6324ede5bb5e9708
Mateus Pereira <mateusp.s@outlook.com>
2024-06-26 10:39:44 -0300

    80517 tailwind setup (#541)
    Test URLs:
    - Before: https://main--petplace--hlxsites.hlx.page/
    - After:
      - https://80517-tailwind-setup--petplace--hlxsites.hlx.live/
    - https://80517-tailwind-setup--petplace--hlxsites.hlx.live/?martech=off
    
    ---------
    
    Co-authored-by: Darlan Tódero ten Caten <darlantc@me.com>

--- react-client/.eslintrc.cjs
+++ react-client/.eslintrc.cjs
@@ -2,24 +2,35 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react-hooks/recommended",
     "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
     tsconfigRootDir: __dirname,
   },
-}
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+    },
+    {
+      files: ["*.js", "*.jsx"],
+      parserOptions: {
+        project: null,
+      },
+    },
+  ],
+};
--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,10 +1,11 @@
-import "./App.css";
+import "./index.css";
 import { AppRouter } from "./routes/AppRoute";
 
 function App() {
   return (
     <>
-      <h1>PetPlace React project</h1>
+      <h1 className="text-primary-color">PetPlace React project</h1>
+      <p className="text-primary-color">Teste</p>
       <AppRouter />
     </>
   );
