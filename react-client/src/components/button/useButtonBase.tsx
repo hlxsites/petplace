b72ec2b89b2c4c2ec51a72abd98adcc03d59a40c
Darlan Tódero ten Caten <darlantc@me.com>
2024-06-27 10:48:08 -0300

    fix: Button base style

--- react-client/src/components/button/useButtonBase.tsx
+++ react-client/src/components/button/useButtonBase.tsx
@@ -20,7 +20,7 @@ function useButtonBase({
   const isError = variant === "error";
 
   const className = classNames(
-    "rounded-full min-w-[175px] h-[48px] py-3.5 px-large outline outline-transparent focus:outline-orange-300-contrast outline-2 outline-offset-2",
+    "rounded-full font-semibold h-[48px] py-3.5 px-large outline outline-transparent focus:outline-orange-300-contrast outline-2 outline-offset-2 items-center flex justify-center",
     {
       "bg-orange-300-contrast text-white hover:bg-orange-500": isPrimary,
       "bg-white border-1 border-neutral-700 text-neutral-700": isSecondary,
