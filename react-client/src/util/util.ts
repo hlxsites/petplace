670b3055f30a277b9a0c6721c6331ec7a0a0a413
Darlan Tódero ten Caten <darlantc@me.com>
2024-06-27 12:13:32 -0300

    Disabled strange estlint-error

--- react-client/src/util/util.ts
+++ react-client/src/util/util.ts
@@ -2,5 +2,6 @@ import clsx, { type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function classNames(...inputs: ClassValue[]): string {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
   return twMerge(clsx(inputs));
 }
