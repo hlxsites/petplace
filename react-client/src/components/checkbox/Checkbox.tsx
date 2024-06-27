97dde681f4433112e75930ade62cd3a2e2e9c9d4
Darlan Tódero ten Caten <darlantc@me.com>
2024-06-27 20:20:18 -0300

    feat: improved Checkbox component API

--- react-client/src/components/checkbox/Checkbox.tsx
+++ react-client/src/components/checkbox/Checkbox.tsx
@@ -1,34 +1,39 @@
 import * as RadixCheckbox from "@radix-ui/react-checkbox";
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
 import { classNames } from "../../util/styleUtil";
+import { CommonInputProps } from "../design-system/types/FormTypes";
 import { Icon } from "../icon/Icon";
 
-export type CheckboxProps = {
-  asChild?: boolean;
-  checked?: boolean | "indeterminate";
-  className?: string;
-  defaultChecked?: boolean | "indeterminate";
-  disabled?: boolean;
-  id: string;
-  name: string;
-  onCheckedChange: () => void;
-  required?: boolean;
-  value: string;
-};
+type CheckboxProps = Omit<
+  ComponentPropsWithoutRef<typeof RadixCheckbox.Root>,
+  "children"
+> &
+  CommonInputProps;
 
-const Checkbox = ({ className, ...props }: CheckboxProps) => {
+export const Checkbox = forwardRef<
+  ElementRef<typeof RadixCheckbox.Root>,
+  CheckboxProps
+>(({ className, hideLabel, label, ...props }, ref) => {
   return (
-    <RadixCheckbox.Root
-      className={classNames(
-        "p-0 flex h-5 w-5 items-center justify-center rounded-md border-solid border-[#6E6D73] bg-white focus:outline-none",
-        className
+    <div className="flex items-center gap-2">
+      <RadixCheckbox.Root
+        aria-label={hideLabel ? label : undefined}
+        className={classNames(
+          "p-0 flex h-5 w-5 items-center justify-center rounded-md border-solid border-[#6E6D73] bg-white focus:outline-none",
+          className
+        )}
+        ref={ref}
+        {...props}
+      >
+        <RadixCheckbox.Indicator className="absolute flex h-5 w-5 items-center justify-center rounded-md bg-[#C74D3F] text-white">
+          <Icon display="check" />
+        </RadixCheckbox.Indicator>
+      </RadixCheckbox.Root>
+      {!hideLabel && (
+        <label className="Label" htmlFor={props.id}>
+          {label}
+        </label>
       )}
-      {...props}
-    >
-      <RadixCheckbox.Indicator className="absolute flex h-5 w-5 items-center justify-center rounded-md bg-[#C74D3F] text-white">
-        <Icon display="check" />
-      </RadixCheckbox.Indicator>
-    </RadixCheckbox.Root>
+    </div>
   );
-};
-
-export default Checkbox;
+});
