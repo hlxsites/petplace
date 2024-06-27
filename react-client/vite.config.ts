8ad38526d337d6c527e634d300732cd0b522252a
Darlan Tódero ten Caten <darlantc@me.com>
2024-06-27 12:07:16 -0300

    chore: added vite-plugin-svgr dependency

--- react-client/vite.config.ts
+++ react-client/vite.config.ts
@@ -1,17 +1,29 @@
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
+
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-test/',
+  base: "/react-test/",
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: 'assets/react-[name]-[hash].js',
-        chunkFileNames: 'assets/react-[name]-[hash].js',
-        assetFileNames: 'assets/react.[ext]',
-      }
-    }
+        entryFileNames: "assets/react-[name]-[hash].js",
+        chunkFileNames: "assets/react-[name]-[hash].js",
+        assetFileNames: "assets/react.[ext]",
+      },
+    },
   },
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        replaceAttrValues: {
+          white: "currentColor",
+        },
+      },
+      include: "**/*.svg",
+    }),
+  ],
+});
