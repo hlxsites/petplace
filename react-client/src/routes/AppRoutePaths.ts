d853d141ab3f343d53de0a6023684e2093072777
Darlan Tódero ten Caten <darlantc@me.com>
2024-07-02 12:06:57 -0300

    feat: added route utilities

--- react-client/src/routes/AppRoutePaths.ts
+++ react-client/src/routes/AppRoutePaths.ts
@@ -2,4 +2,7 @@ export const AppRoutePaths = {
   root: "/",
   account: "account",
   myPets: "my-pets",
-};
+  myPetsIndex: undefined,
+} as const;
+
+export type AppRoutePathIds = keyof typeof AppRoutePaths;
