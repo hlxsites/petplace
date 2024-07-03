cc4f8600fd96d320987a2018ba0ddbd35c0b0c75
Mateus Pereira <mateusp.s@outlook.com>
2024-07-03 09:50:54 -0300

    feat: my-pets grid

--- react-client/src/routes/my-pets/MyPetsIndex.tsx
+++ react-client/src/routes/my-pets/MyPetsIndex.tsx
@@ -1,9 +1,19 @@
+import PetCard from "~/components/card/PetCard";
 import { Title } from "~/components/design-system";
 
-export const MyPetsIndex = () => {
+export const MyPetsIndex = ({ pets }: { pets: any[] }) => {
   return (
     <>
       <Title>My Pets</Title>
+      <div className="mt-base grid w-full grid-flow-row grid-cols-1 justify-center gap-6 px-base tablet:grid-cols-2 laptop:grid-cols-3 desktop:grid-cols-4">
+        {pets.map((pet) => {
+          return (
+            <div key={pet.name} className="flex w-full justify-center">
+              <PetCard {...pet} />
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
