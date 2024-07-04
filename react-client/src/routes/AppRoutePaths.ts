c26efb755725a809459b84c7b6fac3b371c43fca
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-07-04 09:07:54 -0300

    feat: Create a dynamic route to render a specific pet profile base on the id

--- react-client/src/routes/AppRoutePaths.ts
+++ react-client/src/routes/AppRoutePaths.ts
@@ -1,8 +1,12 @@
+const myPets = "my-pets"
+
 export const AppRoutePaths = {
   root: "/",
   account: "account",
-  myPets: "my-pets",
+  myPets,
   myPetsIndex: undefined,
+  petProfile: `${myPets}/:petId`,
+  petProfileIndex: undefined
 } as const;
 
 export type AppRoutePathIds = keyof typeof AppRoutePaths;
