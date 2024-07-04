bce3a549b4d474069e69c83ec215bd25ce5824cf
Mateus Pereira <mateusp.s@outlook.com>
2024-07-04 12:03:42 -0300

    80846 my pets animal grid (#559)
    https://github.com/hlxsites/petplace/assets/78119745/1398e4b8-5430-4dfe-8723-d1704fef4776
    
    
    
    Test URLs:
    - Before: https://main--petplace--hlxsites.hlx.page/
    - After:
      - https://80846-my-pets-animal-grid--petplace--hlxsites.hlx.live/
    -
    https://80846-my-pets-animal-grid--petplace--hlxsites.hlx.live/?martech=off
    
    ---------
    
    Co-authored-by: Darlan Tódero ten Caten <darlantc@me.com>

--- react-client/src/components/design-system/icon/Icon.tsx
+++ react-client/src/components/design-system/icon/Icon.tsx
@@ -1,8 +1,12 @@
 import { classNames } from "~/util/styleUtil";
 import CheckIcon from "./assets/check-icon.svg";
+import ShieldGood from "./assets/shield-good-icon.svg";
+import ShieldOff from "./assets/shield-off-icon.svg";
 
 const IconMap = Object.freeze({
   check: <CheckIcon />,
+  shieldGood: <ShieldGood />,
+  shieldOff: <ShieldOff />,
 });
 
 export type IconKeys = keyof typeof IconMap;
--- react-client/src/routes/AppRoutePaths.ts
+++ react-client/src/routes/AppRoutePaths.ts
@@ -2,7 +2,8 @@ export const AppRoutePaths = {
   root: "/",
   account: "account",
   myPets: "my-pets",
-  myPetsIndex: undefined,
 } as const;
 
-export type AppRoutePathIds = keyof typeof AppRoutePaths;
+export const AppRoutePathsIndexes = {
+  myPetsIndex: undefined,
+} as const;
