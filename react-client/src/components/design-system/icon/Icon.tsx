5f8420e52f550f1f71c1fcb084ce207ffefdcd38
Mateus Pereira <mateusp.s@outlook.com>
2024-07-05 19:15:49 -0300

    chore: add new icons

--- react-client/src/components/design-system/icon/Icon.tsx
+++ react-client/src/components/design-system/icon/Icon.tsx
@@ -1,13 +1,19 @@
 import { classNames } from "~/util/styleUtil";
 import AddIcon from "./assets/add-icon.svg";
+import AlertIcon from "./assets/alert-icon.svg";
 import CheckIcon from "./assets/check-icon.svg";
+import InformationIcon from "./assets/information-icon.svg";
+import SearchIcon from "./assets/search-icon.svg";
 import ShieldGoodIcon from "./assets/shield-good-icon.svg";
 import ShieldOffIcon from "./assets/shield-off-icon.svg";
 import WarningTriangleIcon from "./assets/warning-triangle-icon.svg";
 
 const IconMap = Object.freeze({
   add: <AddIcon />,
+  alert: <AlertIcon />,
   check: <CheckIcon />,
+  search: <SearchIcon />,
+  information: <InformationIcon />,
   shieldGood: <ShieldGoodIcon />,
   shieldOff: <ShieldOffIcon />,
   warningTriangle: <WarningTriangleIcon />,
