b136180748bf1ec5d1a372727146d15983f25050
Ju <89787597+yuliaribeiro@users.noreply.github.com>
2024-07-05 11:27:05 -0300

    80918 Create link icon button (#568)
    This PR is responsible to create the LinkIconButton and its unit tests;
    
    Test URLs:
    - Before: https://main--petplace--hlxsites.hlx.page/
    - After:
      - https://80918-create-link-icon-button--petplace--hlxsites.hlx.live/
    -
    https://80918-create-link-icon-button--petplace--hlxsites.hlx.live/?martech=off

---
+++ react-client/src/components/design-system/button/LinkIconButton.test.tsx
@@ -0,0 +1,60 @@
+import { render, screen } from "@testing-library/react";
+import { LinkIconButton } from "./LinkIconButton";
+import { ComponentProps } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const { getByRole } = screen;
+
+describe("LinkIconButton", () => {
+  it("should pass linkProps to Link component", () => {
+    const target = "_blank";
+    const to = "/my-pets";
+
+    getRenderer({
+      target,
+      to,
+    });
+
+    const linkElement = getByRole("link");
+    expect(linkElement).toHaveAttribute("href", to);
+    expect(linkElement).toHaveAttribute("target", target);
+  });
+
+  it("should pass buttonProps to IconButton component", () => {
+    const label = "Shield off";
+
+    getRenderer({
+      buttonProps: {
+        icon: "shieldOff",
+        label,
+      },
+    });
+
+    expect(document.querySelector("svg")).toHaveAttribute(
+      "data-file-name",
+      "SvgShieldOffIcon"
+    );
+    expect(getByRole("button")).toHaveAttribute("aria-label", label);
+  });
+
+  it("should match snapshot to assure that the component is being rendered correctly", () => {
+    const { container } = getRenderer();
+
+    expect(container).toMatchSnapshot();
+  });
+});
+
+function getRenderer({
+  buttonProps = {
+    icon: "check",
+    label: "Check button",
+  },
+  to = "/test",
+  ...rest
+}: Partial<ComponentProps<typeof LinkIconButton>> = {}) {
+  return render(
+    <Router>
+      <LinkIconButton buttonProps={buttonProps} to={to} {...rest} />
+    </Router>
+  );
+}
