b9f6bd031c66a0efe963a4afccdb2ebbfa3114aa
Mateus Pereira <mateusp.s@outlook.com>
2024-07-04 23:49:49 -0300

    feat: add responsiveness to petcard

--- react-client/src/components/design-system/card/PetCard.tsx
+++ react-client/src/components/design-system/card/PetCard.tsx
@@ -14,9 +14,9 @@ interface IPetCardProps {
 const PetCard = ({ name, img, isProtected, ...props }: IPetCardProps) => {
   return (
     <Card {...props} hasShadow={true} radius="sm">
-      <div className="h-[312x] w-[306px] max-w-[312px] rounded-xl bg-neutral-400">
-        <div className={`z-10 flex h-[246px] justify-end`}>
-          <div className="relative h-full w-full">
+      <div className="tablet:h-[306px] h-[251px] w-[327px] tablet:w-[312px] max-w-[312px] rounded-xl bg-neutral-400">
+        <div className={`z-10 flex h-[191px] tablet:h-[246px] justify-end`}>
+          <div className="relative w-full">
             <div className="z-10 relative flex justify-end p-base">
               <div
                 className={classNames(
@@ -32,12 +32,12 @@ const PetCard = ({ name, img, isProtected, ...props }: IPetCardProps) => {
             <img
               src={img}
               alt={name}
-              className="absolute inset-0 h-full w-full rounded-t-xl object-cover z-0"
+              className="absolute inset-0 h-[191px] tablet:h-[246px] w-full rounded-t-xl object-cover z-0"
             />
           </div>
         </div>
         <div className="z-0 rounded-b-xl bg-neutral-white p-base">
-          <span className="h-[28px] text-2xl font-semibold text-black">
+          <span className="text-xl font-semibold text-black">
             {name}
           </span>
         </div>
