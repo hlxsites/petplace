f791e85f5b464e514ae8db16d2fa2a5f9f873f0f
Mateus Pereira <mateusp.s@outlook.com>
2024-07-04 23:50:35 -0300

    feat: add flexibility to title component

--- react-client/src/components/design-system/text/Title.tsx
+++ react-client/src/components/design-system/text/Title.tsx
@@ -1,12 +1,19 @@
+import { classNames } from "~/util/styleUtil";
+
 type TitleProps = {
   children: string;
+  className?: string;
   level?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 };
 
-export const Title = ({ children, level = "h1" }: TitleProps) => {
+export const Title = ({ children, level = "h1", className }: TitleProps) => {
   const Comp = level;
 
   return (
-    <Comp className="text-neutral-950 text-3xl font-bold">{children}</Comp>
+    <Comp
+      className={classNames("text-3xl font-bold text-neutral-950", className)}
+    >
+      {children}
+    </Comp>
   );
 };
