27545f93dc3e6d3e03e188359c14a4fa70feb3c3
Darlan Tódero ten Caten <darlantc@me.com>
2024-07-06 17:52:05 -0300

    feat: improved PetCard styling

--- react-client/src/components/Pet/PetCard.tsx
+++ react-client/src/components/Pet/PetCard.tsx
@@ -10,28 +10,30 @@ type PetCardProps = {
 export const PetCard = ({ name, img, isProtected, ...props }: PetCardProps) => {
   return (
     <Card {...props} hasShadow={true} radius="sm">
-      <div className="h-[251px] w-[327px] max-w-[312px] rounded-xl bg-neutral-400 tablet:h-[306px] tablet:w-[312px]">
-        <div className={`z-10 flex h-[191px] justify-end tablet:h-[246px]`}>
-          <div className="relative w-full">
-            <div className="relative z-10 flex justify-end p-base">
-              <div
-                className={classNames(
-                  "flex h-8 w-8 items-center justify-center rounded-full bg-white p-base",
-                  isProtected ? "text-green-500" : "text-red-500"
-                )}
-              >
-                <Icon display={isProtected ? "shieldGood" : "shieldOff"} />
-              </div>
-            </div>
-            <img
-              src={img}
-              alt={name}
-              className="absolute inset-0 z-0 h-[191px] w-full rounded-t-xl object-cover tablet:h-[246px]"
+      <div className="h-[251px] w-[327px] max-w-[312px] tablet:h-[306px] tablet:w-[312px]">
+        <div className="relative flex h-[191px] justify-end tablet:h-[246px]">
+          <img
+            src={img}
+            alt={name}
+            className="inset-0 h-[191px] w-full rounded-t-xl object-cover tablet:h-[246px]"
+          />
+          <div
+            className={classNames(
+              "absolute right-4 top-4 flex h-8 w-8 items-center justify-center rounded-full",
+              {
+                "bg-success-background text-success-contrast": isProtected,
+                "bg-error-background text-error-contrast": !isProtected,
+              }
+            )}
+          >
+            <Icon
+              display={isProtected ? "shieldGood" : "shieldOff"}
+              size={16}
             />
           </div>
         </div>
-        <div className="z-0 rounded-b-xl bg-neutral-white p-base">
-          <span className="text-xl font-semibold text-black">{name}</span>
+        <div className="p-base text-2xl font-bold leading-none text-black">
+          {name}
         </div>
       </div>
     </Card>
