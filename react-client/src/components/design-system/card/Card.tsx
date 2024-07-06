d6ce4580a0bd83a178ae82e9b8b4f503a9b08dcf
Darlan Tódero ten Caten <darlantc@me.com>
2024-07-06 17:14:29 -0300

    feat: added overflow hidden style to card

--- react-client/src/components/design-system/card/Card.tsx
+++ react-client/src/components/design-system/card/Card.tsx
@@ -11,7 +11,7 @@ export const Card = ({ children, hasShadow, radius = "base" }: CardProps) => {
   return (
     <div
       className={classNames(
-        "w-fit border border-solid border-[#d0d0d6] bg-neutral-white",
+        "w-fit overflow-hidden border border-solid border-[#d0d0d6] bg-neutral-white",
         {
           "rounded-2xl": radius === "base",
           "rounded-xl": radius === "sm",
