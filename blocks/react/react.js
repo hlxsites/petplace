854f61769af6e5122742b46b34b28c4502f04cf8
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-07-08 16:45:30 -0300

    feat: update React app block

--- blocks/react/react.js
+++ blocks/react/react.js
@@ -2,5 +2,5 @@ export default async function decorate(block) {
   block.id = 'react-root';
   block.innerHTML = '';
 
-  import('./react-index-BMPd4vMx.js');
+  import('./react-index-DvCHp2xf.js');
 }
