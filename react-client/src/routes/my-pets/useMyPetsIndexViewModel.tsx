aabc64a1c3eec52c717b2c82fe0d857e236cd433
Darlan Tódero ten Caten <darlantc@me.com>
2024-07-09 10:43:46 -0300

    refactor: moved mocked data to a specific place

--- react-client/src/routes/my-pets/useMyPetsIndexViewModel.tsx
+++ react-client/src/routes/my-pets/useMyPetsIndexViewModel.tsx
@@ -1,25 +1,10 @@
 import { LoaderFunction, useLoaderData } from "react-router-dom";
+import { getPetsList } from "~/mocks/MockRestApiServer";
 import { LoaderData } from "~/types/LoaderData";
 
 export const loader = (() => {
   return {
-    pets: [
-      {
-        name: "Buddy",
-        img: "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg",
-        isProtected: true,
-      },
-      {
-        name: "Max",
-        img: "https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg",
-        isProtected: false,
-      },
-      {
-        name: "Charlie",
-        img: "https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2023/07/top-20-small-dog-breeds.jpeg.jpg",
-        isProtected: true,
-      },
-    ],
+    pets: getPetsList(),
   };
 }) satisfies LoaderFunction;
 
