d1aa9d3fbd1dfcbef1e37d1d269b4f87d6520bfc
ankyc-phi <146023473+ankyc-phi@users.noreply.github.com>
2024-07-09 11:30:41 -0400

    page alignment for grid in mobile and desktop (#577)
    Branch Commits:
    - adding gen ai back in
    - adding attribute setting back
    - setting up content alignment
    - css changes for grid layout
    - changes for divs attached to ads
    - breadcrumbs added to page
    - minor changes to element positioning
    
    Jira Ticket:
    https://pethealthinc.atlassian.net/browse/PM-343
    
    Test URLs:
    - Before: https://main--petplace--hlxsites.hlx.page/
    - After:
    - https://feat-new-article-page-alignment--petplace--hlxsites.hlx.live/
    -
    https://feat-new-article-page-alignment--petplace--hlxsites.hlx.live/?martech=off

--- templates/article-page/article-page.js
+++ templates/article-page/article-page.js
@@ -1,5 +1,14 @@
-import { buildBlock, getMetadata, toClassName } from '../../scripts/lib-franklin.js';
+import {
+  buildBlock,
+  decorateBlock,
+  getMetadata,
+  loadBlock,
+  toClassName,
+} from '../../scripts/lib-franklin.js';
+import { isMobile, createBreadCrumbs } from '../../scripts/scripts.js';
 import { pushToDataLayer } from '../../scripts/utils/helpers.js';
+
+const GENAI_TOOLTIP = 'Try our AI powered discovery tool and get all your questions answered';
 
 function createTableOfContents(main) {
   const hasToc = getMetadata('has-toc');
@@ -19,27 +28,76 @@ function createTableOfContents(main) {
   }
 }
 
-function createAutoBlockSection(main, blockName, gridName) {
-  const gridNameValue = gridName || blockName;
-  const section = document.createElement('div');
-  // section.classList.add('article-template-autoblock', `article-template-grid-${gridNameValue}`);
-  section.classList.add(gridNameValue);
-
-  main.append(section);
-  return section;
-}
-
-function createTemplateBlock(main, blockName, gridName, elems = []) {
-  const section = createAutoBlockSection(main, blockName, gridName);
+async function createTemplateBlock(container, blockName, elems = []) {
+  const wrapper = document.createElement('div');
+  container.append(wrapper);
 
   const block = buildBlock(blockName, { elems });
-  section.append(block);
+  wrapper.append(block);
+
+  decorateBlock(block);
+  await loadBlock(block);
 }
+
+async function sectionGenAi(main) {
+  const genAIDiv = document.createElement('div');
+  genAIDiv.classList.add('section');
+  genAIDiv.classList.add('genai-search');
+
+  const genAIMeta = document.createElement('meta');
+  genAIMeta.setAttribute('itemprop', 'description');
+  genAIMeta.setAttribute(
+    'content',
+    document.head.querySelector('meta[name="description"]').content,
+  );
+  genAIDiv.append(genAIMeta);
+
+  const articleContainer = main.querySelector('.section:nth-of-type(2)');
+  const genaiBlock = buildBlock('genai-search', '');
+  const genAITitle = document.createElement('h2');
+  const genAISubtitle = document.createElement('h2');
+  genAITitle.innerText = 'Learn even more with...  ';
+  genAISubtitle.innerText = 'AI Powered PetPlace Discovery';
+
+  genAIDiv.append(genAITitle);
+  genAIDiv.append(genAISubtitle);
+  genAIDiv.append(genaiBlock);
+  articleContainer.append(genAIDiv);
+
+  // genAIBlock.insertBefore(secondHeadline);
+
+  decorateBlock(genaiBlock);
+  await loadBlock(genaiBlock);
+}
+
+const createGenAISearchCTA = () => {
+  const headerSearchButton = document.createElement('div');
+  headerSearchButton.className = 'header-search';
+  headerSearchButton.innerHTML = `<a data-modal="/tools/search"><img src="${window.hlx.codeBasePath}/icons/ai_generate_white.svg"><span class="tooltip">${GENAI_TOOLTIP}</span></a>`;
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY >= 68) {
+      headerSearchButton.classList.add('scrolled'); // New position when scrolled to the threshold
+    } else {
+      headerSearchButton.classList.remove('scrolled'); // Original position
+    }
+  });
+
+  headerSearchButton.addEventListener('click', async () => {
+    await pushToDataLayer({
+      event: 'genai_floater',
+      element_type: 'button',
+    });
+    document.location.pathname = '/discovery';
+  });
+
+  return headerSearchButton;
+};
 
 export function loadEager(document) {
   const main = document.querySelector('main');
 
-  // content
+  // top
   createTableOfContents(main);
   createTemplateBlock(main, 'article-author');
 
@@ -49,6 +107,53 @@ export function loadEager(document) {
 
   // bottom
   createTemplateBlock(main, 'related-reading');
+
+  // same attribute setting as earlier
+  main.setAttribute('itemscope', '');
+  const articleType = toClassName(getMetadata('type'));
+
+  if (articleType === 'faq') {
+    main.setAttribute('itemtype', 'https://schema.org/FAQPage');
+    [...main.querySelectorAll(':scope > div > :is(h1,h2,h3)')]
+      .filter((h) => h.textContent.endsWith('?') || h.textContent.match(/#\d+/))
+      .forEach((h) => {
+        if (h.nodeName === 'H1') {
+          const meta = document.createElement('meta');
+          meta.setAttribute('itemprop', 'name');
+          meta.setAttribute('content', h.textContent);
+          h.after(meta);
+        } else {
+          h.setAttribute('itemprop', 'name');
+        }
+        const question = document.createElement('div');
+        question.setAttribute('itemscope', '');
+        question.setAttribute('itemprop', 'mainEntity');
+        question.setAttribute('itemtype', 'https://schema.org/Question');
+        if (h.nodeName === 'H1') {
+          h.after(question);
+          question.append(question.nextElementSibling);
+        } else {
+          h.replaceWith(question);
+          question.append(h);
+        }
+        const answer = document.createElement('div');
+        answer.setAttribute('itemscope', '');
+        answer.setAttribute('itemprop', 'acceptedAnswer');
+        answer.setAttribute('itemtype', 'https://schema.org/Answer');
+        question.append(answer);
+        const div = document.createElement('div');
+        div.setAttribute('itemprop', 'text');
+        answer.append(div);
+        while (
+          question.nextElementSibling
+          && question.nextElementSibling.tagName !== h.nodeName
+        ) {
+          div.append(question.nextElementSibling);
+        }
+      });
+  } else {
+    main.setAttribute('itemtype', 'https://schema.org/BlogPosting');
+  }
 }
 
 export async function loadLazy(document) {
@@ -61,15 +166,71 @@ export async function loadLazy(document) {
   authorDiv.classList.remove('section');
   const heroImgContainer = main.querySelectorAll('p')[0];
   heroImgContainer.classList.add('hero-pic-div');
+
   heroTitleSection.append(articleTitle);
   heroTitleSection.append(authorDiv);
   heroTitleSection.append(heroImgContainer);
   main.prepend(heroTitleSection);
 
+  const contentSection = main.querySelectorAll('.section')[1];
+  contentSection.classList.add('article-content-container');
+
   const { adsenseFunc } = await import('../../scripts/adsense.js');
   adsenseFunc('article', 'create');
 
-  // genai block will go here below
+  // genai block code is unchanged from before
+  sectionGenAi(main);
+
+  if (!isMobile()) {
+    const contentDiv = document.createElement('div');
+    contentDiv.classList.add('content-left');
+    const heroTitleDiv = document.querySelector('.hero-title-container');
+    const blogSection = document.querySelector('.article-content-container');
+    contentDiv.append(heroTitleDiv);
+    contentDiv.append(blogSection);
+
+    const sidebarDiv = document.createElement('div');
+    sidebarDiv.classList.add('sidebar-right');
+    const popularDiv = document.querySelector('.popular-articles-container');
+    const compareDiv = document.querySelector('.article-cta-container');
+    sidebarDiv.append(popularDiv);
+    sidebarDiv.append(compareDiv);
+
+    main.append(contentDiv);
+    main.append(sidebarDiv);
+  }
+
+  const body = main.parentNode;
+  const breadcrumbContainer = document.createElement('div');
+  body.insertBefore(breadcrumbContainer, main);
+
+  const heading = main.querySelector('h1');
+  const breadcrumbData = await createBreadCrumbs(
+    [
+      {
+        url: `${window.hlx.contentBasePath}/pet-insurance`,
+        path: 'Pet Insurance',
+        color: 'black',
+        label: 'Pet Insurance',
+      },
+      {
+        url: window.location,
+        path: heading.innerText,
+        color: 'black',
+        label: heading.innerText,
+      },
+    ],
+    { chevronAll: true, chevronIcon: 'chevron-large', useHomeLabel: true },
+  );
+  breadcrumbData.querySelectorAll('.icon.icon-chevron').forEach((icon) => {
+    icon.classList.replace('icon-chevron', 'icon-chevron-large');
+  });
+  createTemplateBlock(breadcrumbContainer, 'breadcrumb', [breadcrumbData]);
+
+  if (document.body.classList.contains('article-page')) {
+    const contentDiv = document.querySelector('.default-content-wrapper');
+    contentDiv.append(createGenAISearchCTA());
+  }
 }
 
 export async function loadDelayed() {
@@ -82,4 +243,10 @@ export async function loadDelayed() {
 
   const { adsenseFunc } = await import('../../scripts/adsense.js');
   adsenseFunc('article', articleCat);
+
+  if (!isMobile()) {
+    const sidebarDiv = document.querySelector('.sidebar-right');
+    const skyscraperAd = document.querySelector('.skyscraper');
+    sidebarDiv.append(skyscraperAd);
+  }
 }
