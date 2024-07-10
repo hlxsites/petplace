a7046ab9673a3cc1b12c2800407b0ced4bbf120f
Abhinav Saraswat <absarasw@abhinavs-mbp.corp.adobe.com>
2024-07-10 14:28:22 +0530

    Adding github workflow to generate variations

---
+++ scripts/generate-variation.js
@@ -0,0 +1,5 @@
+import {generateVariations} from '@aem-screens/franklin-variation-creator';
+
+(async () => {
+    await generateVariations();
+})();
---
+++ scripts/upload-variation.js
@@ -0,0 +1,5 @@
+import {uploadVariations} from '@aem-screens/franklin-variation-creator';
+
+(async () => {
+    await uploadVariations();
+})();
