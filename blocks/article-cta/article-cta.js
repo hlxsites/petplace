1f034ec76f8bffa4ad220c89eed42bff5319e3cb
ankyc-phi <ankit.curchorcar@pethealthinc.com>
2024-07-10 10:46:38 -0400

    removed double border on cta

--- blocks/article-cta/article-cta.js
+++ blocks/article-cta/article-cta.js
@@ -1,4 +1,4 @@
-import { isTablet } from '../../scripts/scripts.js';
+// import { isTablet } from '../../scripts/scripts.js';
 
 // TODO: this will come in as a fragment at a later date
 export default async function decorate(block) {
@@ -11,9 +11,10 @@ export default async function decorate(block) {
   details.textContent = 'Compare the top pet insurance plans all in one place to find the one that works best for you and your pet';
 
   img.classList.add('article-cta-img');
-  img.src = isTablet()
-    ? '/icons/compare-plans-tablet.svg'
-    : '/icons/compare-plans-screen.svg';
+  img.src = '/icons/compare-plans-screen.svg';
+  // img.src = isTablet()
+  //   ? '/icons/compare-plans-tablet.svg'
+  //   : '/icons/compare-plans-screen.svg';
   button.textContent = 'Compare Plans';
   button.href = '/pet-insurance';
 
