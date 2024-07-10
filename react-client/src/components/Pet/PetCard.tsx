ac78575067917c45fb8f697209a7d04a575f36df
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-07-10 16:21:33 -0300

    feat: improve PetCard component making it more generic

--- react-client/src/components/Pet/PetCard.tsx
+++ react-client/src/components/Pet/PetCard.tsx
@@ -1,40 +1,58 @@
 import { classNames } from "~/util/styleUtil";
 import { Card, Icon } from "../design-system";
+import useCardBase, { UseCardBase } from "../design-system/card/useCardBase";
+import { isBoolean } from "lodash";
+import { ReactNode } from "react";
 
-type PetCardProps = {
-  name: string;
+type PetCardProps = UseCardBase & {
+  children: ReactNode;
+  hasShadow?: boolean;
   img?: string;
-  isProtected: boolean;
+  isProtected?: boolean;
+  name: string;
 };
 
-export const PetCard = ({ name, img, isProtected, ...props }: PetCardProps) => {
+export const PetCard = ({
+  children,
+  hasShadow,
+  img,
+  isProtected,
+  name,
+  variant,
+  ...props
+}: PetCardProps) => {
+  const { className: baseClassName } = useCardBase({
+    variant,
+  });
+  const isSimpleCard = isBoolean(isProtected);
+
   return (
-    <Card {...props} hasShadow={true} radius="sm">
-      <div className="lg:max-h-[306px] max-h-[251px] w-full">
-        <div className="lg:h-[246px] relative flex h-[191px] w-full justify-end">
+    <Card {...props} hasShadow={hasShadow} radius="sm">
+      <div className={`${isSimpleCard ? "" : "lg:flex"}`}>
+        <div className={classNames(baseClassName)}>
           <img
             src={img}
             alt={name}
-            className="lg:h-[246px] inset-0 h-[191px] w-full rounded-t-xl object-cover"
+            className={`inset-0 w-full ${isSimpleCard ? "rounded-t-xl" : ""} object-cover`}
           />
-          <div
-            className={classNames(
-              "absolute right-4 top-4 flex h-8 w-8 items-center justify-center rounded-full",
-              {
-                "bg-success-background text-success-contrast": isProtected,
-                "bg-error-background text-error-contrast": !isProtected,
-              }
-            )}
-          >
-            <Icon
-              display={isProtected ? "shieldGood" : "shieldOff"}
-              size={16}
-            />
-          </div>
+          {isSimpleCard && (
+            <div
+              className={classNames(
+                "absolute right-4 top-4 flex h-8 w-8 items-center justify-center rounded-full",
+                {
+                  "bg-success-background text-success-contrast": isProtected,
+                  "bg-error-background text-error-contrast": !isProtected,
+                }
+              )}
+            >
+              <Icon
+                display={isProtected ? "shieldGood" : "shieldOff"}
+                size={16}
+              />
+            </div>
+          )}
         </div>
-        <div className="p-base text-2xl font-bold leading-none text-black">
-          {name}
-        </div>
+        {children}
       </div>
     </Card>
   );
