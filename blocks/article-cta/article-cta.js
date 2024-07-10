4bac15f54aa3dd340de72c8c8d9c926995f64cc0
ankyc-phi <ankit.curchorcar@pethealthinc.com>
2024-07-10 11:12:43 -0400

    unable to find image, enabling back again

--- blocks/article-cta/article-cta.js
+++ blocks/article-cta/article-cta.js
@@ -1,4 +1,4 @@
-// import { isTablet } from '../../scripts/scripts.js';
+import { isTablet } from '../../scripts/scripts.js';
 
 // TODO: this will come in as a fragment at a later date
 export default async function decorate(block) {
@@ -8,13 +8,14 @@ export default async function decorate(block) {
   const button = document.createElement('button');
 
   heading.textContent = 'Pet Insurance: A Pet Care Must Have';
-  details.textContent = 'Compare the top pet insurance plans all in one place to find the one that works best for you and your pet';
+  details.textContent =
+    'Compare the top pet insurance plans all in one place to find the one that works best for you and your pet';
 
   img.classList.add('article-cta-img');
-  img.src = '/icons/compare-plans-screen.svg';
-  // img.src = isTablet()
-  //   ? '/icons/compare-plans-tablet.svg'
-  //   : '/icons/compare-plans-screen.svg';
+  // img.src = '/icons/compare-plans-screen.svg';
+  img.src = isTablet()
+    ? '/icons/compare-plans-tablet.svg'
+    : '/icons/compare-plans-screen.svg';
   button.textContent = 'Compare Plans';
   button.href = '/pet-insurance';
 
