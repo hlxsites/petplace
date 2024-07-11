04b604cca03f2c910404010f90ebaff1b1c34b1c
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-07-11 16:12:55 -0300

    feat: add class name to Header component to control its style

--- react-client/src/components/design-system/header/Header.tsx
+++ react-client/src/components/design-system/header/Header.tsx
@@ -1,8 +1,10 @@
 import { Title } from "../text/Title";
 import { LinkIconButton } from "../button/LinkIconButton";
 import { ComponentProps, ReactNode } from "react";
+import { classNames } from "~/util/styleUtil";
 
 type HeaderProps = {
+  mbHeader?: "large" | "small";
   linkIconButtonProps?: ComponentProps<typeof LinkIconButton>;
   pageTitle: string;
   primaryElement?: ReactNode;
@@ -19,13 +21,16 @@ export const Header = ({
     relative: "path",
     to: "..",
   },
+  mbHeader = "large",
   pageTitle,
   primaryElement,
   secondaryElement,
   shouldRenderBackButton,
 }: HeaderProps) => {
+  const { className } = useHeaderBase(mbHeader);
+
   return (
-    <div className="mb-large lg:mb-xxlarge">
+    <div className={className}>
       <div className="mb-large flex items-center justify-between lg:m-0">
         <div className="flex items-center">
           {shouldRenderBackButton && (
@@ -39,3 +44,11 @@ export const Header = ({
     </div>
   );
 };
+
+function useHeaderBase(mb: HeaderProps["mbHeader"]) {
+  const className = classNames({
+    "mb-large lg:mb-xxlarge": mb === "large",
+    "mb-small": mb === "small",
+  });
+  return { className };
+}
