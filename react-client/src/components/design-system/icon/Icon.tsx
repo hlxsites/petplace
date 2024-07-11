b22567630016363484972fd0d897906d5a1b11c6
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-07-11 16:12:01 -0300

    feat: create instance of new icons on Icon component

--- react-client/src/components/design-system/icon/Icon.tsx
+++ react-client/src/components/design-system/icon/Icon.tsx
@@ -1,19 +1,27 @@
 import { classNames } from "~/util/styleUtil";
 import AddIcon from "./assets/add-icon.svg";
 import AppsIcon from "./assets/apps-icon.svg";
+import BendArrowDownIcon from "./assets/bend-arrow-down.svg";
 import CheckIcon from "./assets/check-icon.svg";
 import ChevronLeftIcon from "./assets/chevron-left-icon.svg";
+import CpuChipIcon from "./assets/cpu-chip-icon.svg";
+import PawIcon from "./assets/paw-icon.svg";
 import ShieldGoodIcon from "./assets/shield-good-icon.svg";
 import ShieldOffIcon from "./assets/shield-off-icon.svg";
+import TransferIcon from "./assets/transfer-icon.svg";
 import WarningTriangleIcon from "./assets/warning-triangle-icon.svg";
 
 const IconMap = Object.freeze({
   add: <AddIcon />,
   apps: <AppsIcon />,
+  bendArrowDown: <BendArrowDownIcon />,
   check: <CheckIcon />,
   chevronLeft: <ChevronLeftIcon />,
+  cpuChip: <CpuChipIcon />,
+  paw: <PawIcon />,
   shieldGood: <ShieldGoodIcon />,
   shieldOff: <ShieldOffIcon />,
+  transfer: <TransferIcon />,
   warningTriangle: <WarningTriangleIcon />,
 });
 
