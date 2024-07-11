17294bcd3c99422b61455375ce58601b2aebe109
Mateus Pereira <mateusp.s@outlook.com>
2024-07-10 21:57:38 -0300

    feat: add drawer

---
+++ react-client/src/components/design-system/drawer/Drawer.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+import { Title } from "../text/Title";
+import { IconButton } from "../button/IconButton";
+
+interface DrawerProps {
+  children: ReactNode;
+  title: string;
+  onClose: () => void;
+}
+
+const Drawer = ({ children, title, onClose }: DrawerProps) => {
+  return (
+    <div className="w-full rounded-t-2xl bg-neutral-white p-xlarge border border-border-secondary lg:h-screen lg:w-[336px] lg:rounded-none">
+      <div className="mb-small flex items-center justify-between">
+        <Title>{title}</Title>
+        <IconButton icon="closeXMark" label="Close drawer" className="text-neutral-600" variant="link" onClick={onClose} />
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default Drawer;
