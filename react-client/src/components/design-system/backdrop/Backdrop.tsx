10855a829f4cb654d92923f7105bdda513f8b704
Mateus Pereira <mateusp.s@outlook.com>
2024-07-11 17:37:36 -0300

    feat: add drawer animation

--- react-client/src/components/design-system/backdrop/Backdrop.tsx
+++ react-client/src/components/design-system/backdrop/Backdrop.tsx
@@ -1,12 +1,23 @@
 import { ReactNode } from "react";
+import { classNames } from "~/util/styleUtil";
 
 interface BackdropProps {
   children: ReactNode;
+  isOn: boolean;
+  turnOff?: () => void;
 }
 
-const Backdrop = ({ children }: BackdropProps) => {
+const Backdrop = ({ children, isOn, turnOff }: BackdropProps) => {
   return (
-    <div className={"absolute left-0 top-0 flex h-screen w-full backdrop-blur-sm items-end justify-center lg:justify-end"}>
+    <div
+      className={classNames(
+        "fade-in-element absolute right-0 top-0 flex h-screen items-end justify-center overflow-hidden backdrop-blur-sm lg:justify-end",
+        {
+          "fade-unfaded w-full": isOn,
+        }
+      )}
+      onClick={() => (turnOff ? turnOff() : () => {})}
+    >
       {children}
     </div>
   );
--- react-client/src/components/design-system/drawer/Drawer.tsx
+++ react-client/src/components/design-system/drawer/Drawer.tsx
@@ -1,21 +1,45 @@
 import { ReactNode } from "react";
 import { Title } from "../text/Title";
 import { IconButton } from "../button/IconButton";
+import { classNames } from "~/util/styleUtil";
+import Backdrop from "../backdrop/Backdrop";
 
 interface DrawerProps {
   children: ReactNode;
   title: string;
   onClose: () => void;
+  isOpen: boolean;
 }
 
-export const Drawer = ({ children, title, onClose }: DrawerProps) => {
+export const Drawer = ({ children, title, onClose, isOpen }: DrawerProps) => {
   return (
-    <div className="w-full rounded-t-2xl bg-neutral-white p-xlarge border border-border-secondary lg:h-screen lg:w-[336px] lg:rounded-none" data-testid="drawer">
-      <div className="mb-small flex items-center justify-between">
-        <Title>{title}</Title>
-        <IconButton icon="closeXMark" label="Close drawer" className="text-neutral-600" variant="link" onClick={onClose} />
+    <Backdrop isOn={isOpen} turnOff={onClose}>
+      <div
+        className={classNames(
+          "w-full rounded-t-2xl border border-border-secondary bg-neutral-white p-xlarge ease-in-out lg:h-screen lg:w-[336px] lg:rounded-none",
+          {
+            inactive: !isOpen,
+          }
+        )}
+        data-testid="drawer"
+        onClick={handleClick}
+      >
+        <div className="mb-small flex items-center justify-between">
+          <Title>{title}</Title>
+          <IconButton
+            icon="closeXMark"
+            label="Close drawer"
+            className="text-neutral-600"
+            variant="link"
+            onClick={onClose}
+          />
+        </div>
+        {children}
       </div>
-      {children}
-    </div>
+    </Backdrop>
   );
+
+  function handleClick(event: React.MouseEvent<HTMLDivElement>) {
+    event.stopPropagation();
+  }
 };
