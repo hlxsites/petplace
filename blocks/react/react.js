1920e3c169f9bcb730d8f9046869c8cedb653274
Darlan Tódero ten Caten <darlantc@me.com>
2024-07-16 11:49:16 -0300

    refactor: improved PetCard component API

--- blocks/react/react.js
+++ blocks/react/react.js
@@ -2,5 +2,5 @@ export default async function decorate(block) {
   block.id = 'react-root';
   block.innerHTML = '';
 
-  import('./react-index-Zdqh73Ex.js');
+  import('./react-index-BAoj1ifx.js');
 }
