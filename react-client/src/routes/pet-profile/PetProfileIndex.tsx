0c001dbc6cfd6c9702fd64ba997d8997c3a83622
Ju <89787597+yuliaribeiro@users.noreply.github.com>
2024-07-18 10:15:49 -0300

    80899 add pet profile info (#586)
    This PR is responsible to render all information when clicking to view
    an animal profile
    
    
    https://github.com/hlxsites/petplace/assets/89787597/4423e4ff-7d9d-4940-8a15-c737738426b4
    
    
    
    Test URLs:
    - Before: https://main--petplace--hlxsites.hlx.page/
    - After:
    -
    https://80899-add-pet-profile-info--petplace--hlxsites.hlx.page/account/my-pets
    -
    https://80899-add-pet-profile-info--petplace--hlxsites.hlx.page/account/my-pets/?martech=off
    
    ---------
    
    Co-authored-by: Darlan Tódero ten Caten <darlantc@me.com>

--- react-client/src/routes/pet-profile/PetProfileIndex.tsx
+++ react-client/src/routes/pet-profile/PetProfileIndex.tsx
@@ -2,9 +2,12 @@ import { Layout } from "~/components/design-system/layout/Layout";
 import { usePetProfileIndexViewModel } from "./usePetProfileIndexViewModel";
 import { Header } from "~/components/design-system/header/Header";
 import { Button } from "~/components/design-system";
+import { PetCard } from "~/components/Pet/PetCard";
+import { PetCardInfo } from "~/components/Pet/PetCardInfo";
 
 export const PetProfileIndex = () => {
   const { petInfo } = usePetProfileIndexViewModel();
+  if (!petInfo) return;
 
   return (
     <Layout>
@@ -13,7 +16,9 @@ export const PetProfileIndex = () => {
         primaryElement={renderActionsButton()}
         shouldRenderBackButton
       />
-      <div>{petInfo}</div>
+      <PetCard img={petInfo.img} name={petInfo.name} variant="lg">
+        <PetCardInfo {...petInfo} name={petInfo.name} />
+      </PetCard>
     </Layout>
   );
 };
