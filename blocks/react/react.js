3c525fab28f50009f5d143b5e0d78d10ccd06646
Darlan Tódero ten Caten <darlantc@me.com>
2024-07-18 12:43:15 -0300

    feat: update React app block

--- blocks/react/react.js
+++ blocks/react/react.js
@@ -2,5 +2,5 @@ export default async function decorate(block) {
     block.id = "react-root";
     block.innerHTML = "";
 
-    import("./react-index-BniKh8U7.js");
+    import("./react-index-DOMrg0CL.js");
 }
