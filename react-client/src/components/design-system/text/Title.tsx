6a870b2bfd3c0dd421db3e1d0dd10e6ea1deca73
Mateus Pereira <mateusp.s@outlook.com>
2024-07-18 15:59:15 -0300

    feat: add title id prop for accessibility reasons

--- react-client/src/components/design-system/text/Title.tsx
+++ react-client/src/components/design-system/text/Title.tsx
@@ -1,13 +1,14 @@
 type TitleProps = {
   children: string;
+  id?: string;
   level?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 };
 
-export const Title = ({ children, level = "h1" }: TitleProps) => {
+export const Title = ({ children, level = "h1", id }: TitleProps) => {
   const Comp = level;
 
   return (
-    <Comp className="lg:text-3xl text-xl font-bold text-neutral-950">
+    <Comp className="lg:text-3xl text-xl font-bold text-neutral-950" id={id}>
       {children}
     </Comp>
   );
