b0f13f74107624dfb26576d0533d1e94e62d71bc
Darlan Tódero ten Caten <darlantc@me.com>
2024-07-19 10:33:21 -0300

    chore: changed react build strategy to avoid hash names

--- blocks/react/react.js
+++ blocks/react/react.js
@@ -1,10 +1,6 @@
 export default async function decorate(block) {
   block.id = 'react-root';
   block.innerHTML = '';
-<<<<<<< HEAD
 
-  import('./react-index-Buy9QCt2.js');
-=======
-  import('./react-index-Buy9QCt2.js');
->>>>>>> 81519-fix-text-titles
+  import('./react-index.js');
 }
