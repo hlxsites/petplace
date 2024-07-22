f75b87d2fa70f8b4d00b6e3cabd8f09b48255797
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-07-22 10:16:04 -0300

    feat: create function to get pet info to render on pet profile

---
+++ react-client/src/components/Pet/getPetInfoTab.tsx
@@ -0,0 +1,63 @@
+import { PetInfo } from "~/mocks/MockRestApiServer";
+import { Text } from "../design-system/text/Text";
+
+type GetPetInfoTabProps = Omit<
+  PetInfo,
+  "id" | "img" | "isProtected" | "microchipNumber" | "name"
+>;
+
+export function getPetInfoTab({
+  age,
+  breed,
+  dateOfBirth,
+  mixedBreed,
+  sex,
+  spayedNeutered,
+  species,
+}: GetPetInfoTabProps) {
+  const getFields = [
+    {
+      label: "Age",
+      value: age,
+    },
+    {
+      label: "Species",
+      value: species,
+    },
+    {
+      label: "Sex",
+      value: sex,
+    },
+    {
+      label: "Breed",
+      value: breed,
+    },
+    {
+      label: "DOB",
+      value: dateOfBirth,
+    },
+    {
+      label: "Mixed breed",
+      value: mixedBreed,
+    },
+    {
+      label: "Spayed/Neutered",
+      value: spayedNeutered,
+    },
+  ];
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2">
+      {getFields.map(({ label, value }) => (
+        <div className="pb-xsmall" key={label}>
+          <span>
+            <Text size="sm" fontFamily="raleway" fontWeight="bold">
+              {label}
+            </Text>
+            : <Text size="sm">{value ?? ""}</Text>
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+}
