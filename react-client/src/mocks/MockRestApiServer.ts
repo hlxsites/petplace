46288fbebf0144b6e00f10d193b4b60818129635
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-07-22 10:11:59 -0300

    feat: update mock to accept new fields

--- react-client/src/mocks/MockRestApiServer.ts
+++ react-client/src/mocks/MockRestApiServer.ts
@@ -1,31 +1,46 @@
 export type PetInfo = {
+  age?: string | undefined;
   breed?: string;
+  dateOfBirth?: string;
   id: string;
   img?: string;
+  mixedBreed?: string;
   isProtected?: boolean;
   microchipNumber?: number;
   name: string;
   sex?: string;
+  spayedNeutered?: string;
+  species?: string;
 };
 
 const PETS_LIST: PetInfo[] = [
   {
+    age: "Young",
     breed: "Some Dog",
+    dateOfBirth: "03/05/2024",
     id: "buddy",
     img: "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg",
     isProtected: true,
     microchipNumber: 1290,
+    mixedBreed: "Yes",
     name: "Buddy",
     sex: "Male",
+    spayedNeutered: "No",
+    species: "Dog",
   },
   {
+    age: "Senior",
     breed: "Orange Cat",
+    dateOfBirth: "07/07/2014",
     id: "lily",
     img: "https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg",
     isProtected: false,
     microchipNumber: 8645,
+    mixedBreed: "No",
     name: "Lily",
     sex: "Female",
+    spayedNeutered: "Yes",
+    species: "Cat",
   },
   {
     breed: "Flufy dog",
@@ -35,6 +50,8 @@ const PETS_LIST: PetInfo[] = [
     microchipNumber: 3856,
     name: "Charlie",
     sex: "Male",
+    spayedNeutered: "Yes",
+    species: "Dog",
   },
 ];
 
