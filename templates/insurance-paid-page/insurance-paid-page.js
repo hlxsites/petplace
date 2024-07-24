45f84fbee862175af103af60274809bf7b2105ac
Daniel O'Kane <daniel.okane@herodigital.com>
2024-07-24 10:29:19 -0400

    feature/insurance-paid-blog-template-new: requested PR changes

--- templates/insurance-paid-page/insurance-paid-page.js
+++ templates/insurance-paid-page/insurance-paid-page.js
@@ -44,26 +44,29 @@ export async function loadEager(document) {
 
   // same attribute setting as earlier
   main.setAttribute('itemscope', '');
-}
 
-export async function loadLazy(document) {
-  const main = document.querySelector('main');
   const heroTitleSection = document.createElement('div');
   heroTitleSection.classList.add('hero-title-container', 'section');
 
   const articleTitle = main.querySelector('h1');
-  const authorDiv = main.querySelector('.article-author-container');
-  authorDiv.classList.remove('section');
   const heroImgContainer = main.querySelectorAll('p')[0];
   heroImgContainer.classList.add('hero-pic-div');
 
   heroTitleSection.append(articleTitle);
-  heroTitleSection.append(authorDiv);
   heroTitleSection.append(heroImgContainer);
   main.prepend(heroTitleSection);
+}
+
+export async function loadLazy(document) {
+  const main = document.querySelector('main');
+  const authorDiv = main.querySelector('.article-author-container');
+  authorDiv.classList.remove('section');
+  const heroTitleSection = main.querySelector('.hero-title-container');
 
   const contentSection = main.querySelectorAll('.section')[1];
   contentSection.classList.add('article-content-container');
+
+  heroTitleSection.prepend(authorDiv);
 
   if (!isMobile()) {
     const contentDiv = document.createElement('div');
