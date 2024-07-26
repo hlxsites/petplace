193cc945b2dead3ae246a2ebe1827638cad798e9
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-07-26 00:33:23 -0300

    feat: implement svg files to Icon component

--- react-client/src/components/design-system/icon/Icon.tsx
+++ react-client/src/components/design-system/icon/Icon.tsx
@@ -12,17 +12,24 @@ import ChevronLeftIcon from "./assets/chevron-left-icon.svg";
 import ChevronUp from "./assets/chevron-up.svg";
 import CloseXMarkIcon from "./assets/close-x-mark-icon.svg";
 import CpuChipIcon from "./assets/cpu-chip-icon.svg";
+import DocFileIcon from "./assets/doc-file-icon.svg";
+import DownloadIcon from "./assets/download-icon.svg";
 import EllipseIcon from "./assets/ellipse-icon.svg";
 import FileIcon from "./assets/file-icon.svg";
 import InformationIcon from "./assets/information-icon.svg";
+import JpgFileIcon from "./assets/jpg-file-icon.svg";
 import MedicineIcon from "./assets/medicine-icon.svg";
 import PawIcon from "./assets/paw-icon.svg";
 import PippetIcon from "./assets/pippet-icon.svg";
+import PdfFileIcon from "./assets/pdf-file-icon.svg";
+import PngFileIcon from "./assets/png-file-icon.svg";
 import SearchIcon from "./assets/search-icon.svg";
 import ShieldGoodIcon from "./assets/shield-good-icon.svg";
 import ShieldOffIcon from "./assets/shield-off-icon.svg";
 import SyringeIcon from "./assets/syringe-icon.svg";
 import TransferIcon from "./assets/transfer-icon.svg";
+import TrashIcon from "./assets/trash-icon.svg";
+import TxtFileIcon from "./assets/txt-file-icon.svg";
 import WarningTriangleIcon from "./assets/warning-triangle-icon.svg";
 import CloseXMarkIconRegular from "./assets/x-mark-icon-regular.svg";
 
@@ -39,17 +46,24 @@ const IconMap = Object.freeze({
   closeXMark: <CloseXMarkIcon />,
   closeXMarkRegular: <CloseXMarkIconRegular />,
   cpuChip: <CpuChipIcon />,
+  docFile: <DocFileIcon />,
+  download: <DownloadIcon />,
   ellipse: <EllipseIcon />,
   file: <FileIcon />,
   medicine: <MedicineIcon />,
   information: <InformationIcon />,
+  jpgFile: <JpgFileIcon />,
   paw: <PawIcon />,
-  search: <SearchIcon />,
+  pdfFile: <PdfFileIcon />,
   pippet: <PippetIcon />,
+  pngFile: <PngFileIcon />,
+  search: <SearchIcon />,
   shieldGood: <ShieldGoodIcon />,
   shieldOff: <ShieldOffIcon />,
   syringe: <SyringeIcon />,
   transfer: <TransferIcon />,
+  trash: <TrashIcon />,
+  txtFile: <TxtFileIcon />,
   warningTriangle: <WarningTriangleIcon />,
 });
 
