00226b2e67ccfa6133a7819ceb507e43fb6655f1
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-07-29 19:28:47 -0300

    feat: add unit test to cover the functionality to download files on PetCardRecord

--- react-client/src/components/Pet/PetCardRecord.test.tsx
+++ react-client/src/components/Pet/PetCardRecord.test.tsx
@@ -1,11 +1,31 @@
 import { render, screen } from "@testing-library/react";
-import { PetCardRecord } from "./PetCardRecord";
 import { ComponentProps } from "react";
 import userEvent from "@testing-library/user-event";
+import { PetCardRecord } from "./PetCardRecord";
+import * as downloadFunctions from "../../util/downloadFunctions";
 
-const { getByRole, queryByRole, getByLabelText, queryByLabelText } = screen;
+const { getByRole, queryByRole } = screen;
+
+const deleteButton = /delete file/i;
+const downloadButton = /download file/i;
 
 describe("PetCardRecord", () => {
+  beforeEach(() => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        ok: true,
+        blob: () =>
+          Promise.resolve(
+            new Blob(["file content"], { type: "application/octet-stream" })
+          ),
+      })
+    ) as jest.Mock;
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   it.each(["doc", "jpg", "pdf", "png", "txt"] as ComponentProps<
     typeof PetCardRecord
   >["fileType"][])(
@@ -26,10 +46,10 @@ describe("PetCardRecord", () => {
     getRenderer();
 
     expect(
-      getByLabelText(/download file/i).querySelector("svg")
+      getByRole("button", { name: downloadButton }).querySelector("svg")
     ).toHaveAttribute("data-file-name", "SvgDownloadIcon");
     expect(
-      getByRole("button", { name: /delete file/i }).querySelector("svg")
+      getByRole("button", { name: deleteButton }).querySelector("svg")
     ).toHaveAttribute("data-file-name", "SvgTrashIcon");
   });
 
@@ -61,16 +81,18 @@ describe("PetCardRecord", () => {
   it("should render download and delete actions when isUploading=false", () => {
     getRenderer();
 
-    expect(getByLabelText("download file")).toBeInTheDocument();
-    expect(getByRole("button", { name: "delete file" })).toBeInTheDocument();
+    expect(getByRole("button", { name: downloadButton })).toBeInTheDocument();
+    expect(getByRole("button", { name: deleteButton })).toBeInTheDocument();
   });
 
   it("should NOT render download and delete actions when isUploading=true", () => {
     getRenderer({ isUploadingFile: true });
 
-    expect(queryByLabelText("download file")).not.toBeInTheDocument();
     expect(
-      queryByRole("button", { name: "delete file" })
+      queryByRole("button", { name: downloadButton })
+    ).not.toBeInTheDocument();
+    expect(
+      queryByRole("button", { name: deleteButton })
     ).not.toBeInTheDocument();
   });
 
@@ -79,52 +101,37 @@ describe("PetCardRecord", () => {
     getRenderer({ onClick });
 
     expect(onClick).not.toHaveBeenCalled();
-    await userEvent.click(getByRole("button", { name: "delete file" }));
+    await userEvent.click(getByRole("button", { name: deleteButton }));
     expect(onClick).toHaveBeenCalledTimes(1);
   });
 
-  it.each(["Test records", "Lily Medical Records"])(
-    "should download the file with its own name",
-    (fileName) => {
-      getRenderer({ fileName });
+  it("should download the file with expected values when download button is clicked", async () => {
+    const downloadFileSpy = jest.spyOn(downloadFunctions, "downloadFile");
 
-      expect(getByLabelText("download file")).toHaveAttribute(
-        "download",
-        fileName
-      );
-    }
-  );
-
-  it.each(["/medical/download/", "vaccine/download"])(
-    "should get download file from given path",
-    (downloadPath) => {
-      getRenderer({ downloadPath });
-
-      expect(getByLabelText("download file")).toHaveAttribute(
-        "href",
-        downloadPath
-      );
-    }
-  );
-
-  it("should render download action with noopener and noreferrer options to provide extra layer of security when downloading external files", () => {
     getRenderer();
+    expect(downloadFileSpy).not.toHaveBeenCalled();
 
-    expect(getByLabelText("download file")).toHaveAttribute(
-      "rel",
-      "noopener noreferrer"
-    );
+    await userEvent.click(getByRole("button", { name: downloadButton }));
+
+    expect(downloadFileSpy).toHaveBeenCalledTimes(1);
+    expect(downloadFileSpy).toHaveBeenCalledWith({
+      downloadPath: "http://example.com/file.jpg",
+      fileName: "Test name",
+      fileType: "jpg",
+    });
   });
 });
 
 function getRenderer({
+  downloadPath = "http://example.com/file.jpg",
   fileName = "Test name",
-  fileType = "doc",
+  fileType = "jpg",
   isUploadingFile = false,
   ...props
 }: Partial<ComponentProps<typeof PetCardRecord>> = {}) {
   return render(
     <PetCardRecord
+      downloadPath={downloadPath}
       fileName={fileName}
       fileType={fileType}
       isUploadingFile={isUploadingFile}
