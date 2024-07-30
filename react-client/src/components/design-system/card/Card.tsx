d35bf4f845ee5e40f31fb9345d1d707d97c3d8cd
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-07-30 14:49:54 -0300

    feat: add new props to Card component and fix how components were calling it

--- react-client/src/components/design-system/card/Card.tsx
+++ react-client/src/components/design-system/card/Card.tsx
@@ -2,28 +2,34 @@ import type { ReactNode } from "react";
 import { classNames } from "~/util/styleUtil";
 
 export type CardProps = {
+  backgroundColor?: "blue-100" | "neutral-white";
+  border?: "blue-100" | "neutral-300";
   children: ReactNode;
-  hasShadow?: boolean;
   radius?: "sm" | "base";
   role?: JSX.IntrinsicElements["div"]["role"];
+  shadow?: "elevation-1" | "elevation-3";
 };
 
 export const Card = ({
+  backgroundColor = "neutral-white",
+  border = "neutral-300",
   children,
-  hasShadow,
   radius = "base",
   role,
+  shadow,
 }: CardProps) => {
   return (
     <div
-      className={classNames(
-        "overflow-hidden border border-solid border-[#d0d0d6] bg-neutral-white",
-        {
-          "rounded-2xl": radius === "base",
-          "rounded-xl": radius === "sm",
-          "shadow-elevation-1": hasShadow,
-        }
-      )}
+      className={classNames("overflow-hidden border border-solid", {
+        "bg-blue-100": backgroundColor === "blue-100",
+        "bg-neutral-white": backgroundColor === "neutral-white",
+        "border-blue-100": border === "blue-100",
+        "border-neutral-300": border === "neutral-300",
+        "rounded-2xl": radius === "base",
+        "rounded-xl": radius === "sm",
+        "shadow-elevation-1": shadow === "elevation-1",
+        "shadow-elevation-3": shadow === "elevation-3",
+      })}
       role={role}
     >
       {children}
