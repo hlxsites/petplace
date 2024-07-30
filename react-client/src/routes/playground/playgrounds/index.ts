4f27f12bae28886b1e20a83bc7b97473aa842bb1
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-07-30 13:59:02 -0300

    feat: add petDocumentsView on playground index

--- react-client/src/routes/playground/playgrounds/index.ts
+++ react-client/src/routes/playground/playgrounds/index.ts
@@ -3,3 +3,4 @@ export * from "./DialogPlayground";
 export * from "./FormBuilderPlayground";
 export * from "./TextPlayground";
 export * from "./PetCardRecordPlayground";
+export * from "./PetDocumentsViewPlayground";
