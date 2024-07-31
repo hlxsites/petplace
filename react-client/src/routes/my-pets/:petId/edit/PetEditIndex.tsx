2260ba77f9f958841c96aad1f0ddeb052702a54b
Darlan Tódero ten Caten <darlantc@me.com>
2024-07-31 18:51:54 -0300

    feat: added ped profile edit form

--- react-client/src/routes/my-pets/:petId/edit/PetEditIndex.tsx
+++ react-client/src/routes/my-pets/:petId/edit/PetEditIndex.tsx
@@ -1,7 +1,36 @@
+import { DisplayForm } from "~/components/design-system";
+import { petProfileFormSchema } from "../form/petForm";
 import { usePetProfileContext } from "../usePetProfileLayoutViewModel";
 
 export const PetEditIndex = () => {
   const { petInfo } = usePetProfileContext();
 
-  return <p>Edit {petInfo.name}</p>;
+  return (
+    <DisplayForm
+      onChange={(props) => {
+        console.log("onChange values", props);
+      }}
+      onSubmit={({ event, values }) => {
+        event.preventDefault();
+
+        console.log("onSubmit values", values);
+      }}
+      schema={petProfileFormSchema}
+      variables={{
+        // This could come from an API request, for example
+        breedOptions: [
+          "Poodle",
+          "Golden Retriever",
+          "Labrador",
+          "Pug",
+          "Beagle",
+        ],
+        breedTypeOptions: [],
+        colorOptions: ["Black", "White", "Brown", "Grey", "Golden"],
+      }}
+      values={{
+        ...petInfo,
+      }}
+    />
+  );
 };
---
+++ react-client/src/routes/my-pets/:petId/form/petForm.tsx
@@ -0,0 +1,148 @@
+import { FormSchema, Text } from "~/components/design-system";
+
+export const petProfileFormSchema: FormSchema = {
+  id: "pet-profile-form",
+  children: [
+    {
+      elementType: "input",
+      description:
+        "This can be your pet's current name, or a new name that you chose.",
+      id: "name",
+      label: "Pet name",
+      errorMessage: "Please enter your pet's name.",
+      requiredCondition: true,
+      type: "text",
+    },
+    {
+      elementType: "row",
+      children: [
+        {
+          elementType: "input",
+          id: "type",
+          label: "Is your pet a",
+          options: ["Dog", "Cat"],
+          requiredCondition: true,
+          type: "select",
+        },
+        {
+          elementType: "input",
+          id: "sex",
+          label: "What's their gender?",
+          options: ["Male", "Female"],
+          requiredCondition: true,
+          type: "select",
+        },
+      ],
+    },
+    {
+      elementType: "row",
+      children: [
+        {
+          description: `If you don't see it in the list below, select "other" at very bottom of the list.`,
+          elementType: "input",
+          id: "breed",
+          label: "Breed",
+          options: "{{breedOptions|string[]}}",
+          optionsType: "dynamic",
+          type: "select",
+        },
+        {
+          description: `If you don't see your pet's color, please select "other" located at the bottom of the list.`,
+          elementType: "input",
+          id: "color",
+          label: "Color",
+          options: "{{colorOptions|string[]}}",
+          optionsType: "dynamic",
+          requiredCondition: true,
+          type: "select",
+        },
+      ],
+    },
+    {
+      elementType: "row",
+      children: [
+        {
+          elementType: "input",
+          id: "breed-type",
+          label: "What's their breed type?",
+          options: "{{breedTypeOptions|string[]}}",
+          optionsType: "dynamic",
+          requiredCondition: true,
+          type: "select",
+        },
+        {
+          elementType: "input",
+          id: "spayedNeutered",
+          label: "Are they spayed or neutered?",
+          requiredCondition: true,
+          type: "boolean",
+        },
+      ],
+    },
+    {
+      elementType: "row",
+      children: [
+        {
+          elementType: "input",
+          id: "birth-month",
+          label: "Birth month",
+          placeholder: "Month",
+          requiredCondition: true,
+          options: [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+          ],
+          type: "select",
+        },
+        {
+          elementType: "input",
+          id: "birth-year",
+          label: "Birth year",
+          placeholder: "Year",
+          requiredCondition: true,
+          type: "number",
+        },
+      ],
+    },
+    {
+      content: (
+        <Text size="base">
+          Please ensure that all the information is complete and accurate. We
+          may contact you periodically to ensure your information remains
+          accurate and up to date.
+        </Text>
+      ),
+      elementType: "html",
+    },
+    {
+      elementType: "input",
+      hideLabel: true,
+      id: "consent",
+      label: "Consent to release information",
+      errorMessage: "You must consent to release your information.",
+      options: [
+        "Do you consent to the release of my name, email, address and telephone number to anyone who finds your pet? You may withdraw your consent at any time.",
+      ],
+      requiredCondition: true,
+      type: "checkboxGroup",
+    },
+    {
+      className: "w-full mt-xxlarge",
+      elementType: "button",
+      id: "submit",
+      label: "Save pet",
+      type: "submit",
+    },
+  ],
+  version: 0,
+};
