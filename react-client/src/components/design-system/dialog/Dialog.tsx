94b535198dd7a8eedf9c7ede537b8ce26484fa4c
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-07-31 10:50:57 -0300

    feat: add to Dialog component the ability to hide the close button and align the content

--- react-client/src/components/design-system/dialog/Dialog.tsx
+++ react-client/src/components/design-system/dialog/Dialog.tsx
@@ -3,19 +3,34 @@ import { DialogBase } from "../dialogBase/DialogBase";
 import { DialogCommonProps } from "../types/DialogBaseTypes";
 
 type DialogProps = DialogCommonProps & {
+  align?: "center" | "right";
+  hideCloseButton?: boolean;
   fullWidth?: boolean;
 };
 
-export const Dialog = ({ children, fullWidth, ...rest }: DialogProps) => {
+export const Dialog = ({
+  align,
+  children,
+  fullWidth,
+  hideCloseButton,
+  ...rest
+}: DialogProps) => {
   return (
     <DialogBase
       {...rest}
       className={{
-        closeButton: "absolute right-[2px] top-[-41px] text-neutral-white",
+        closeButton: classNames(
+          "absolute right-[2px] top-[-41px] text-neutral-white",
+          {
+            hidden: hideCloseButton,
+          }
+        ),
         modal: classNames(
           "fixed inset-0 z-50 m-auto max-h-max max-w-max rounded-2xl bg-neutral-white p-xlarge ease-out md:w-1/2 md:max-w-[50%]",
           {
-            ["md:w-full md:max-w-full"]: fullWidth,
+            "md:w-full md:max-w-full": fullWidth,
+            "text-center": align === "center",
+            "text-right": align === "right",
           }
         ),
       }}
