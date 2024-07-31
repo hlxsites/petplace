e85b2b6d96f39b866037b11e8775e15875af46c9
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-07-31 19:47:46 -0300

    feat: adjust the Card component to be flexible when using some props

--- react-client/src/components/design-system/card/Card.tsx
+++ react-client/src/components/design-system/card/Card.tsx
@@ -1,14 +1,5 @@
-import type { ReactNode } from "react";
 import { classNames } from "~/util/styleUtil";
-
-export type CardProps = {
-  backgroundColor?: "blue-100" | "neutral-white";
-  border?: "blue-100" | "neutral-300";
-  children: ReactNode;
-  radius?: "sm" | "base";
-  role?: JSX.IntrinsicElements["div"]["role"];
-  shadow?: "elevation-1" | "elevation-3";
-};
+import { CardProps } from "../types/CardTypes";
 
 export const Card = ({
   backgroundColor = "neutral-white",
@@ -20,16 +11,17 @@ export const Card = ({
 }: CardProps) => {
   return (
     <div
-      className={classNames("overflow-hidden border border-solid", {
-        "bg-blue-100": backgroundColor === "blue-100",
-        "bg-neutral-white": backgroundColor === "neutral-white",
-        "border-blue-100": border === "blue-100",
-        "border-neutral-300": border === "neutral-300",
-        "rounded-2xl": radius === "base",
-        "rounded-xl": radius === "sm",
-        "shadow-elevation-1": shadow === "elevation-1",
-        "shadow-elevation-3": shadow === "elevation-3",
-      })}
+      className={classNames(
+        "overflow-hidden border border-solid",
+        backgroundColor,
+        border,
+        {
+          "rounded-2xl": radius === "base",
+          "rounded-xl": radius === "sm",
+          "shadow-elevation-1": shadow === "elevation-1",
+          "shadow-elevation-3": shadow === "elevation-3",
+        }
+      )}
       role={role}
     >
       {children}
