b0312ea6f3aad740e97474ed64cf34f43846a114
Mateus Pereira <mateusp.s@outlook.com>
2024-08-02 19:32:27 -0300

    feat: add lost pet updates

--- react-client/src/mocks/MockRestApiServer.ts
+++ react-client/src/mocks/MockRestApiServer.ts
@@ -11,6 +11,18 @@ export type PetInfo = {
   sex?: string;
   spayedNeutered?: string;
   species?: string;
+  missingStatus: MissingStatus;
+  lostPetHistory: LostPetUpdate[];
+};
+
+export type MissingStatus = "missing" | "found";
+
+export type LostPetUpdate = {
+  date: number;
+  update: number;
+  status: MissingStatus;
+  id: number;
+  note?: string;
 };
 
 const PETS_LIST: PetInfo[] = [
@@ -22,11 +34,28 @@ const PETS_LIST: PetInfo[] = [
     img: "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg",
     isProtected: true,
     microchipNumber: 1290,
+    missingStatus: "found",
     mixedBreed: "Yes",
     name: "Buddy",
     sex: "Male",
     spayedNeutered: "No",
     species: "Dog",
+    lostPetHistory: [
+      {
+        date: 1722300534,
+        update: 1722354747,
+        status: "missing",
+        id: 1234567,
+        note: "Lost report from submitted",
+      },
+      {
+        date: 1722430534,
+        update: 1722460747,
+        status: "found",
+        id: 2234567,
+        note: "",
+      },
+    ],
   },
   {
     age: "Senior",
@@ -36,11 +65,63 @@ const PETS_LIST: PetInfo[] = [
     img: "https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg",
     isProtected: false,
     microchipNumber: 8645,
+    missingStatus: "missing",
     mixedBreed: "No",
     name: "Lily",
     sex: "Female",
     spayedNeutered: "Yes",
     species: "Cat",
+    lostPetHistory: [
+      {
+        date: 1722300534,
+        update: 1722354747,
+        status: "missing",
+        id: 1637427,
+        note: "Lost report from submitted",
+      },
+      {
+        date: 1722430534,
+        update: 1722460747,
+        status: "found",
+        id: 2637427,
+        note: "",
+      },
+      {
+        date: 1722433434,
+        update: 1722466747,
+        status: "missing",
+        id: 3637427,
+        note: "Lost report from submitted",
+      },
+      {
+        date: 1722430534,
+        update: 1722460747,
+        status: "found",
+        id: 4637427,
+        note: "",
+      },
+      {
+        date: 1722433434,
+        update: 1722466747,
+        status: "missing",
+        id: 5637427,
+        note: "Lost report from submitted",
+      },
+      {
+        date: 6,
+        update: 1722460747,
+        status: "found",
+        id: 6637427,
+        note: "",
+      },
+      {
+        date: 7,
+        update: 1722466747,
+        status: "missing",
+        id: 7637427,
+        note: "Lost report from submitted",
+      },
+    ],
   },
   {
     breed: "Flufy dog",
@@ -48,17 +129,18 @@ const PETS_LIST: PetInfo[] = [
     img: "https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2023/07/top-20-small-dog-breeds.jpeg.jpg",
     isProtected: true,
     microchipNumber: 3856,
+    missingStatus: "found",
     name: "Charlie",
     sex: "Male",
     spayedNeutered: "Yes",
     species: "Dog",
+    lostPetHistory: [],
   },
 ];
 
 export const getPetsList = () => {
   return PETS_LIST;
 };
-
 export const getPetById = (id: string) => {
   return getPetsList()?.find((pet) => pet.id === id);
 };
--- react-client/src/routes/my-pets/:petId/PetProfileIndex.tsx
+++ react-client/src/routes/my-pets/:petId/PetProfileIndex.tsx
@@ -1,37 +1,148 @@
-import { Button, Card, Text } from "~/components/design-system";
+import { useState } from "react";
+import {
+  Button,
+  Collapse,
+  Title,
+  Text,
+  Icon,
+  IconKeys,
+  TextSpan,
+  StyleProps,
+} from "~/components/design-system";
 import { Header } from "~/components/design-system/header/Header";
 import { Layout } from "~/components/design-system/layout/Layout";
 import { PetCard } from "~/components/Pet/PetCard";
 import { PetCardInfo } from "~/components/Pet/PetCardInfo";
 import { usePetProfileContext } from "./usePetProfileLayoutViewModel";
-import { PetAlertMessage } from "~/components/Pet/PetAlertMessage";
+import { LostPetUpdate, MissingStatus } from "~/mocks/MockRestApiServer";
+import { PaginatedTable } from "~/components/design-system/table/paginatedTable/PaginatedTable";
+import { classNames } from "~/util/styleUtil";
+
+const columns = [
+  { key: "id", width: "100px", label: "Case ID" },
+  { key: "date", width: "200px", label: "Opened" },
+  { key: "update", width: "200px", label: "Last Update" },
+  { key: "status", width: "180px", label: "Status" },
+  { key: "note", label: "Note" },
+];
+
+const ITEMS_PER_PAGE = 5;
 
 export const PetProfileIndex = () => {
   const { petInfo } = usePetProfileContext();
+  const tableRows = (() => {
+    return petInfo.lostPetHistory.map(convertUpdateToRow);
+  })();
+
+  const [isTableVisible, setIsTableVisible] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentRows, setCurrentRows] = useState(
+    tableRows.slice(0, ITEMS_PER_PAGE)
+  );
 
   return (
     <Layout>
-      <div className="pb-xxlarge">
-        <PetAlertMessage petName={petInfo.name} />
-      </div>
       <Header
         pageTitle="Pet Profile"
         primaryElement={renderActionsButton()}
         shouldRenderBackButton
       />
-      <div className="grid gap-large">
-        <PetCard
-          classNames={{ root: "lg:flex" }}
-          img={petInfo.img}
-          name={petInfo.name}
-          variant="lg"
-        >
-          <PetCardInfo {...petInfo} name={petInfo.name} />
-        </PetCard>
-        {renderPetInsuranceSection()}
-      </div>
+      <PetCard
+        classNames={{ root: "lg:flex" }}
+        img={petInfo.img}
+        name={petInfo.name}
+        variant="lg"
+      >
+        <PetCardInfo {...petInfo} name={petInfo.name} />
+      </PetCard>
+      {renderLostPetUpdates()}
     </Layout>
   );
+
+  function renderLostPetUpdates() {
+    return (
+      <div className="mt-large">
+        <Collapse
+          isOpen={isTableVisible}
+          onOpenChange={setIsTableVisible}
+          title={<Title level="h4">Lost Pets Status Update</Title>}
+        >
+          {renderDescriptionMessage()}
+          <PaginatedTable
+            columns={columns}
+            rows={currentRows}
+            currentPage={currentPage}
+            didChangePagination={handlePagination}
+            itemsCount={tableRows.length}
+            itemsPerPage={ITEMS_PER_PAGE}
+          />
+          {renderReportButton()}
+        </Collapse>
+      </div>
+    );
+  }
+
+  function renderDescriptionMessage() {
+    return (
+      <div className="mb-large">
+        <Text size="base">
+          {currentRows.length
+            ? "Track your pet's status."
+            : "Report Your pet as lost in case i goes missing."}
+        </Text>
+      </div>
+    );
+  }
+
+  function convertUpdateToRow({
+    date,
+    update,
+    status,
+    id,
+    note,
+  }: LostPetUpdate) {
+    return {
+      data: {
+        date: new Date(date).toLocaleString(),
+        update: new Date(update).toLocaleString(),
+        status: convertStatus(status),
+        id,
+        // eslint-disable-next-line no-extra-boolean-cast
+        note: !!note ? note : "-",
+      },
+      key: `${id}-${date}`,
+      isSelectable: false,
+    };
+  }
+
+  function convertStatus(status: LostPetUpdate["status"]) {
+    const { message, textColor, bgColor } = convertInfo(status);
+    return (
+      <div
+        className={classNames("flex w-full justify-center rounded-md", bgColor)}
+      >
+        <TextSpan color={textColor}>{message}</TextSpan>
+      </div>
+    );
+  }
+
+  function renderReportButton() {
+    const { icon, iconColor, message } = reportingVariables(
+      petInfo.missingStatus
+    );
+    return (
+      <Button variant="secondary" fullWidth>
+        <Icon display={icon} className={`mr-base ${iconColor}`} /> {message}
+      </Button>
+    );
+  }
+
+  function handlePagination(page: number, itemsPerPage: number) {
+    setCurrentPage(page);
+    const offsetStartIndex = itemsPerPage * (page - 1);
+    const offsetFinishIndex = offsetStartIndex + itemsPerPage;
+    setCurrentRows(tableRows.slice(offsetStartIndex, offsetFinishIndex));
+  }
 };
 
 function renderActionsButton() {
@@ -57,18 +168,32 @@ function renderActionsButton() {
   );
 }
 
-function renderPetInsuranceSection() {
-  return (
-    <Card>
-      <div className="grid grid-cols-1 items-center justify-items-center gap-large p-large md:items-start md:justify-items-start">
-        <Text fontFamily="raleway" fontWeight="bold" size="lg">
-          See pet's insurance in MyPetHealth
-        </Text>
+function reportingVariables(status: MissingStatus) {
+  return {
+    missing: {
+      icon: "checkCircle" as IconKeys,
+      iconColor: "text-brand-main",
+      message: "Report pet as found",
+    },
+    found: {
+      icon: "warning" as IconKeys,
+      iconColor: "text-yellow-300",
+      message: "Report pet as missing",
+    },
+  }[status];
+}
 
-        <Button fullWidth={true} variant="secondary">
-          View insurance details
-        </Button>
-      </div>
-    </Card>
-  );
+function convertInfo(status: MissingStatus) {
+  return {
+    missing: {
+      bgColor: "bg-yellow-100",
+      textColor: "yellow-500" as StyleProps["color"],
+      message: "Reported missing",
+    },
+    found: {
+      bgColor: "bg-green-100",
+      textColor: "green-500" as StyleProps["color"],
+      message: "I found my pet",
+    },
+  }[status];
 }
