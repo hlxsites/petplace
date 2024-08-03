379150ebf399f6088d63ac646aa168884aba415e
Darlan Tódero ten Caten <darlantc@me.com>
2024-08-03 19:10:40 -0400

    refactor: moved common input styles to a generic place & improved disabled style

--- react-client/src/components/design-system/form/utils/formStyleUtils.ts
+++ react-client/src/components/design-system/form/utils/formStyleUtils.ts
@@ -5,5 +5,7 @@ const INPUT_ERROR_COLOR = "text-text-danger-default";
 
 export const FORM_STYLES = {
   inputRoot: INPUT_ROOT_CLASSNAMES,
+  input:
+    "h-full w-full bg-neutral-white p-base outline-none placeholder:text-text-hinted disabled:cursor-not-allowed disabled:bg-background-disabled disabled:text-text-disabled",
   inputError: INPUT_ERROR_COLOR,
 };
