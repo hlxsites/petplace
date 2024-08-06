da11b20af9fba8d44a1ddbee141258381863dec1
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-08-06 15:56:17 -0300

    feat: update playgrounds

--- react-client/src/routes/playground/playgrounds/PetCardPetWatchPlayground.tsx
+++ react-client/src/routes/playground/playgrounds/PetCardPetWatchPlayground.tsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+import { Button, Drawer } from "~/components/design-system";
 import { PetCardPetWatch } from "~/components/Pet/PetCardPetWatch";
 
 export const PetCardPetWatchPlayground = () => {
-  return <PetCardPetWatch />;
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div>
+      <Button onClick={() => setIsOpen(true)}>Open drawer to 24PetWatch</Button>
+      <Drawer
+        ariaLabel="petWatch"
+        id="petWatchPlayground"
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      >
+        <PetCardPetWatch />
+      </Drawer>
+    </div>
+  );
 };
--- react-client/src/routes/playground/playgrounds/PetCardRecordPlayground.tsx
+++ react-client/src/routes/playground/playgrounds/PetCardRecordPlayground.tsx
@@ -1,41 +1,68 @@
-import { useState } from "react";
-import { Button } from "~/components/design-system/button/Button";
+import { ComponentProps, useState } from "react";
+import { Button, Drawer } from "~/components/design-system";
 import { PetCardRecord } from "~/components/Pet/PetCardRecord";
 
+const PLAYGROUND_RECORD_OPTIONS: ComponentProps<
+  typeof PetCardRecord
+>["record"][] = [
+  {
+    downloadPath:
+      "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg",
+    fileName: "Dog image",
+    fileType: "jpg",
+  },
+  {
+    downloadPath:
+      "https://training.github.com/downloads/pt_BR/github-git-cheat-sheet.pdf",
+    fileName: "PDF gitHub",
+    fileType: "pdf",
+  },
+];
+
 export const PetCardRecordPlayground = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const [isUploading, setIsUploading] = useState(false);
 
   return (
-    <div className="grid max-w-[300px] gap-xlarge">
-      <div className="grid gap-small">
-        <PetCardRecord
-          record={{
-            downloadPath:
-              "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg",
-            fileName: "Dog image",
-            fileType: "jpg",
-          }}
-        />
+    <div>
+      <Button onClick={() => setIsOpen(true)}>
+        Open drawer to pet record component
+      </Button>
+      <Drawer
+        ariaLabel="pet card record on playground"
+        id="petCardRecordPlayground"
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      >
+        <div className="grid gap-small">
+          {PLAYGROUND_RECORD_OPTIONS.map(
+            ({ downloadPath, fileName, fileType }) => (
+              <PetCardRecord
+                record={{
+                  downloadPath,
+                  fileName,
+                  fileType,
+                }}
+              />
+            )
+          )}
 
-        <PetCardRecord
-          record={{
-            downloadPath:
-              "https://training.github.com/downloads/pt_BR/github-git-cheat-sheet.pdf",
-            fileName: "PDF gitHub",
-            fileType: "pdf",
-          }}
-        />
-      </div>
-
-      <Button onClick={() => setIsUploading((prev) => !prev)}>Upload</Button>
-      {isUploading && (
-        <PetCardRecord
-          record={{
-            fileName: "Upload test",
-          }}
-          isUploadingFile={isUploading}
-        />
-      )}
+          <Button
+            className="my-xxxlarge"
+            onClick={() => setIsUploading((prev) => !prev)}
+          >
+            Upload
+          </Button>
+          {isUploading && (
+            <PetCardRecord
+              record={{
+                fileName: "Upload test",
+              }}
+              isUploadingFile={isUploading}
+            />
+          )}
+        </div>
+      </Drawer>
     </div>
   );
 };
