6cdabd13d0d0e2a20c60b45b36d5343b28471e82
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-08-09 14:38:05 -0300

    feat: add more unit tests to cover the petWatchSection

--- react-client/src/components/Pet/sections/PetWatchSection.tsx
+++ react-client/src/components/Pet/sections/PetWatchSection.tsx
@@ -3,7 +3,6 @@ import {
   PET_WATCH_TAGS,
 } from "~/routes/my-pets/:petId/utils/petServiceConstants";
 import { Button, Card, Tag, Text } from "../../design-system";
-import petWatchLogo from "@images/24-pet-watch-logo.png";
 import { PetServiceTypes } from "~/routes/my-pets/:petId/types/PetServicesTypes";
 
 type PetWatchSectionProp = {
@@ -11,23 +10,15 @@ type PetWatchSectionProp = {
 };
 
 export const PetWatchSection = ({ petServiceType }: PetWatchSectionProp) => {
-  // TODO: delete the log bellow
-  console.log("petServiceType on petWatchSection component", petServiceType);
-
-  // TODO: change bellow to use petServiceType when is ready
-  const { buttonLabel, icon, message } = PET_WATCH_OFFERS["lifetimePlus"];
-  const { label, tagStatus } = PET_WATCH_TAGS["lifetimePlus"];
+  const { buttonLabel, icon, message } = PET_WATCH_OFFERS[petServiceType];
+  const { label, tagStatus } = PET_WATCH_TAGS[petServiceType];
 
   return (
     <>
       <Card>
         <div className="grid gap-large p-large">
           <div className="flex items-center justify-between">
-            <img
-              className="max-h-[20px]"
-              alt="24 Pet Watch logo"
-              src={petWatchLogo}
-            />
+            <img className="max-h-[20px]" alt="24 Pet Watch logo" src={""} />
             <Tag label={label} tagStatus={tagStatus} />
           </div>
           <Text size="base">{message}</Text>
