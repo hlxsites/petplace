9d2e034e605bfe8584d58e2fb0c2cae97e7b8593
Darlan Tódero ten Caten <darlantc@me.com>
2024-08-10 18:28:34 -0300

    chore: updated tailwind config to safelist text and bg color classes

--- react-client/tailwind.config.js
+++ react-client/tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   mode: "jit",
   plugins: [],
-  safelist: ["aria-selected:true", "aria-selected:false"],
+  safelist: [
+    "aria-selected:true",
+    "aria-selected:false",
+    {
+      pattern: /bg-+/,
+    },
+    {
+      pattern: /text-+/,
+    },
+  ],
 
   theme: CUSTOM_THEME,
 };
