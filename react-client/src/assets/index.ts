4262bf657a4037cc228df7bf0aaa61508ad3ddcf
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-08-12 10:15:07 -0300

    feat: add logo images in the project

--- react-client/src/assets/index.ts
+++ react-client/src/assets/index.ts
@@ -1,5 +1,7 @@
 export const ASSET_IMAGES = {
+  petcoLogo: getImageUrl("petco-logo.png"),
   petWatchLogo: getImageUrl("24-pet-watch-logo.png"),
+  roverLogo: getImageUrl("rover-logo.png"),
 };
 
 function getImageUrl(filename: string) {
