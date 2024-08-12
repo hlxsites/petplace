f63a02bf37ed0743e317988826dcec187b8b5673
Mateus Pereira <mateusp.s@outlook.com>
2024-08-12 16:31:49 -0300

    feat: improved playground

--- react-client/src/routes/playground/playgrounds/OnboardingDialogPlayground.tsx
+++ react-client/src/routes/playground/playgrounds/OnboardingDialogPlayground.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 import { OnboardingDialog } from "~/routes/my-pets/:petId/OnboardingDialog";
 
-
 export const OnboardingDialogPlayground = () => {
-    return (
-        <OnboardingDialog />
-    )
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    searchParams.set("is-onboarding", "true");
+    setSearchParams(searchParams);
+  }, []);
+
+  return <OnboardingDialog />;
 };
