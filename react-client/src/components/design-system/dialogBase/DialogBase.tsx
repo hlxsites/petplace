1ceda891c05e0ca8a15665249fd0957e13722ae3
Darlan Tódero ten Caten <darlantc@me.com>
2024-08-13 11:38:22 -0300

    feat: dialog base apply some style to body element to prevent scroll

--- react-client/src/components/design-system/dialogBase/DialogBase.tsx
+++ react-client/src/components/design-system/dialogBase/DialogBase.tsx
@@ -28,6 +28,11 @@ export const DialogBase = ({
   });
 
   useEffect(() => {
+    document.body.style.overflow = isOpen ? "hidden" : "visible";
+    document.body.style.position = isOpen ? "relative" : "static";
+  }, [isOpen]);
+
+  useEffect(() => {
     function handleKeyDown(event: KeyboardEvent) {
       if (!isClosing && isOpen && event.key === "Escape") {
         onCloseWithAnimation?.();
