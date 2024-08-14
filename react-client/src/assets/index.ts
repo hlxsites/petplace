ea0c52c1b4ae8227f7e51293e419a14ab2563db9
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-08-14 15:52:53 -0300

    feat: add petplace logo in the project

--- react-client/src/assets/index.ts
+++ react-client/src/assets/index.ts
@@ -1,6 +1,7 @@
 export const ASSET_IMAGES = {
   petWatchLogo: getImageUrl("24-pet-watch-logo.png"),
   comfyDogAndCat: getImageUrl("onboarding-comfy-dog-and-cat.png"),
+  petPlaceLogo: getImageUrl("pet-place-logo.png"),
 };
 
 function getImageUrl(filename: string) {
