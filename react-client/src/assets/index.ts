f07ffc2afa3174aa0a0ea7c1669ec1168c1c1bb8
Mateus Pereira <mateusp.s@outlook.com>
2024-08-14 00:02:50 -0300

    chore: add images

--- react-client/src/assets/index.ts
+++ react-client/src/assets/index.ts
@@ -1,5 +1,8 @@
 export const ASSET_IMAGES = {
   comfyDogAndCat: getImageUrl("onboarding-comfy-dog-and-cat.png"),
+  friendlyDogAndCat: getImageUrl("onboarding-friendly-dog-and-cat.png"),
+  petServices: getImageUrl("onboarding-pet-services.png"),
+  petServicesSm: getImageUrl("onboarding-pet-services-sm.png"),
   petPlaceLogo: getImageUrl("pet-place-logo.png"),
   petWatchLogo: getImageUrl("24-pet-watch-logo.png"),
 };
