f2fa7dfaf195176e404ab1038523a857ed5cbab0
Darlan Tódero ten Caten <darlantc@me.com>
2024-08-15 11:26:22 -0300

    refactor: improved  how to handle body display style

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -5,6 +5,7 @@ import { AppRouter } from "./routes/AppRoute";
 function App() {
   useEffect(() => {
     document.body.className = "";
+    document.body.style.display = "block";
   }, []);
 
   return <AppRouter />;
