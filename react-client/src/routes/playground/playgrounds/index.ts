87ee454212abe1f7756c06b220c4e67bd9be4c6a
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-08-19 15:39:42 -0300

    feat: create the second page playground

--- react-client/src/routes/playground/playgrounds/index.ts
+++ react-client/src/routes/playground/playgrounds/index.ts
@@ -9,6 +9,7 @@ export * from "./DropdownMenuPlayground";
 export * from "./FormBuilderPlayground";
 export * from "./MembershipCardPlayground";
 export * from "./OnboardingDialogPlayground";
+export * from "./CheckoutSecondPagePlayground";
 export * from "./PetCardRecordPlayground";
 export * from "./PetDocumentsViewPlayground";
 export * from "./StepProgressPlayground";
