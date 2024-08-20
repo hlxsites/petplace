a1f193eb957ad1a394d2b449e7fbec92faaf1f84
Mateus Pereira <mateusp.s@outlook.com>
2024-08-20 16:30:21 -0300

    feat: improved OnboardingContent

--- react-client/src/routes/my-pets/:petId/onboarding/OnboardingContent.tsx
+++ react-client/src/routes/my-pets/:petId/onboarding/OnboardingContent.tsx
@@ -1,35 +1,59 @@
 import { ReactNode } from "react";
-import { Button, Text } from "~/components/design-system";
+import { Button, Icon, IconKeys, Loading, Text } from "~/components/design-system";
 import { Title } from "~/components/design-system/text/Title";
+import { classNames } from "~/util/styleUtil";
 
 type OnboardingContentProps = {
   alignment: "center" | "left";
-  children: ReactNode;
+  buttonDisabled?: boolean;
+  buttonLabel?: string;
+  children?: ReactNode;
   hideButton?: boolean;
+  icon?: IconKeys;
+  iconClassName?: string;
   message?: string;
   onNextStep: () => void;
-  step: number;
+  preTitle?: boolean;
   title: string;
 };
 
 export const OnboardingContent = ({
   alignment,
+  buttonDisabled,
+  buttonLabel,
   children,
   hideButton,
+  icon,
+  iconClassName,
   message,
   onNextStep,
-  step,
+  preTitle,
   title,
 }: OnboardingContentProps) => {
-  const buttonLabel = step < 2 ? "Get Started" : "Next";
-
   return (
     <div className="flex flex-col gap-large">
+      {preTitle && (
+
+      <div className="flex w-full justify-center md:w-fit">
+      {icon ? (
+        <div
+          className={classNames(
+            "flex h-[64px] w-[64px] items-center justify-center rounded-full",
+            iconClassName
+          )}
+        >
+          <Icon display={icon} size={25} />
+        </div>
+      ) : (
+        <Loading size={64} />
+      )}
+    </div>
+      )}
       <Title level="h2" align={alignment}>{title}</Title>
       {!!message && <Text size="base" align={alignment}>{message}</Text>}
-      {children}
+      {!!children && children}
       {!hideButton && (
-        <Button onClick={onNextStep} fullWidth>
+        <Button onClick={onNextStep} fullWidth disabled={buttonDisabled} className="disabled:text-neutral-500">
           {buttonLabel}
         </Button>
       )}
