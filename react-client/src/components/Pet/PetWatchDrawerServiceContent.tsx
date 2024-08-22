571352a9516efba101cdad8a30007eed30edb8bb
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-08-22 09:44:33 -0300

    feat: refactor the component to accept the petWatchDrawerBody and petWatchDrawerHeader

--- react-client/src/components/Pet/PetWatchDrawerServiceContent.tsx
+++ react-client/src/components/Pet/PetWatchDrawerServiceContent.tsx
@@ -1,68 +1,20 @@
-import { ASSET_IMAGES } from "~/assets";
 import { useContentDetails } from "~/hooks/useContentDetails";
-import { PET_WATCH_OPTIONS } from "~/routes/my-pets/:petId/utils/petWatchConstants";
-import { IconButton, Text, TextSpan } from "../design-system";
-import { PetCardPetWatch } from "./PetCardPetWatch";
-import { PetServiceDetailsCard } from "./PetServiceDetailsCard";
+import { PetWatchDrawerHeader } from "./PetWatchDrawerHeader";
+import { PetWatchDrawerBody } from "./PetWatchDrawerBody";
 
 export const PetWatchDrawerServiceContent = () => {
   const { handleContentChange, contentDetails } = useContentDetails();
 
   return (
     <>
-      {drawerHeader()}
-      {drawerBody()}
-    </>
-  );
-
-  function drawerHeader() {
-    if (!contentDetails) return <MainHeader />;
-
-    return (
-      <div className="mb-xxlarge">
-        <div className="flex items-center">
-          <IconButton
-            icon="chevronLeft"
-            label="go back"
-            variant="link"
-            className="text-orange-300-contrast"
-            onClick={handleContentChange()}
-          />
-          <TextSpan fontWeight="bold">{contentDetails?.title}</TextSpan>
-        </div>
-        {contentDetails.subtitle && (
-          <div className="-mt-small ml-[26px] lg:ml-[42px]">
-            <Text color="tertiary-600">{contentDetails?.subtitle}</Text>
-          </div>
-        )}
-      </div>
-    );
-  }
-
-  function drawerBody() {
-    if (contentDetails) return <PetServiceDetailsCard {...contentDetails} />;
-
-    return (
-      <div className="grid gap-small pt-large">
-        {PET_WATCH_OPTIONS.map(({ id, ...props }) => (
-          <PetCardPetWatch
-            key={props.label}
-            onClick={handleContentChange(id)}
-            {...props}
-          />
-        ))}
-      </div>
-    );
-  }
-};
-
-export const MainHeader = () => {
-  return (
-    <>
-      <img className="pb-small" src={ASSET_IMAGES.petWatchLogo} />
-      <Text color="tertiary-600" size="base">
-        Here is all the available benefits and perks
-      </Text>
+      <PetWatchDrawerHeader
+        contentDetails={contentDetails}
+        onClick={handleContentChange()}
+      />
+      <PetWatchDrawerBody
+        contentDetails={contentDetails}
+        onClick={handleContentChange}
+      />
     </>
   );
 };
