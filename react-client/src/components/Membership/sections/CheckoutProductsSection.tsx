94287649a02ee69b061df8ddbef1abd6746e82c5
Mateus Pereira <mateusp.s@outlook.com>
2024-08-26 19:47:04 -0300

    feat: add CheckoutItemDetailsDrawer to section

--- react-client/src/components/Membership/sections/CheckoutProductsSection.tsx
+++ react-client/src/components/Membership/sections/CheckoutProductsSection.tsx
@@ -1,8 +1,11 @@
+import { Text } from "~/components/design-system";
 import { CheckoutProduct, Colors, Sizes } from "~/mocks/MockRestApiServer";
+import { classNames } from "~/util/styleUtil";
+import { CartItemImages } from "../CartItemImages";
 import { CheckoutProductCard } from "../CheckoutProductCard";
 import { CheckoutProductColorSize } from "../CheckoutProductColorSize";
-import { Text } from "~/components/design-system";
-import { classNames } from "~/util/styleUtil";
+import { CheckoutItemDetailsDrawer } from "../CheckoutItemDetailsDrawer";
+import { useCartItemsDetails } from "../hooks/useCartItemDetails";
 
 type CheckoutProductsSectionProps = {
   products: CheckoutProduct[];
@@ -12,62 +15,58 @@ export const CheckoutProductsSection = ({
   products,
 }: CheckoutProductsSectionProps) => {
   const productsLength = products.length;
+  const { item, openItemDetails, goBack } = useCartItemsDetails();
 
   return (
-    <div
-      className={classNames("grid grid-cols-1 gap-large", {
-        "lg:grid-cols-3": productsLength > 2,
-        "lg:grid-cols-2": productsLength >= 1 && productsLength <= 2,
-      })}
-    >
-      {products.map(
-        ({
-          availableColors,
-          availableSizes,
-          description,
-          img,
-          title,
-          ...rest
-        }) => (
-          <CheckoutProductCard
-            {...rest}
-            product={renderProduct(img, title)}
-            productSpecifications={renderProductSpecifications(
-              description,
-              availableColors,
-              availableSizes
-            )}
-            title={title}
-          />
-        )
-      )}
-    </div>
+    <>
+      <div
+        className={classNames("grid grid-cols-1 gap-large", {
+          "lg:grid-cols-3": productsLength > 2,
+          "lg:grid-cols-2": productsLength >= 1 && productsLength <= 2,
+        })}
+      >
+        {products.map(
+          ({
+            availableColors,
+            availableSizes,
+            description,
+            id,
+            images,
+            title,
+            ...rest
+          }) => (
+            <CheckoutProductCard
+              {...rest}
+              product={<CartItemImages images={images} name={title} />}
+              productSpecifications={renderProductSpecifications(
+                description,
+                availableColors,
+                availableSizes
+              )}
+              title={title}
+              onClick={() => openItemDetails(id)}
+            />
+          )
+        )}
+      </div>
+      {item && <CheckoutItemDetailsDrawer item={item} onClose={goBack} />}
+    </>
   );
-
-  function renderProduct(img: string, name: string) {
-    return (
-      <img
-        alt={`image of product: ${name}`}
-        className="h-full w-full object-contain"
-        src={img}
-      />
-    );
-  }
-
-  function renderProductSpecifications(
-    description?: string,
-    availableColors?: Colors[],
-    availableSizes?: Sizes[]
-  ) {
-    return description ? (
-      <Text>{description}</Text>
-    ) : (
-      availableColors && availableSizes && (
-        <CheckoutProductColorSize
-          productColors={availableColors}
-          productSizes={availableSizes}
-        />
-      )
-    );
-  }
 };
+
+function renderProductSpecifications(
+  description?: string,
+  availableColors?: Colors[],
+  availableSizes?: Sizes[]
+) {
+  return description ? (
+    <Text>{description}</Text>
+  ) : (
+    availableColors && availableSizes && (
+      <CheckoutProductColorSize
+        productColors={availableColors}
+        productSizes={availableSizes}
+      />
+    )
+  );
+}
