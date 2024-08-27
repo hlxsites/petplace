62b1f4956c238326686e70dd9a23334099df271c
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-08-27 16:57:23 -0300

    feat: create the section to compare the plans

---
+++ react-client/src/components/Membership/sections/MembershipComparingPlansSection.tsx
@@ -0,0 +1,47 @@
+import { Collapse, Title } from "~/components/design-system";
+import { MembershipComparingPlanTable } from "../MembershipComparingPlanTable";
+import { useState } from "react";
+import { Locales } from "../types/MembershipTypes";
+import {
+  CA_MEMBERSHIP_PLANS,
+  MEMBERSHIP_COMPARE_PLANS,
+  MEMBERSHIP_COMPARING_PLANS_BUTTONS,
+  US_MEMBERSHIP_PLANS,
+} from "../utils/membershipConstants";
+
+type MembershipComparingPlansSectionProps = {
+  locale: Locales;
+};
+
+export const MembershipComparingPlansSection = ({
+  locale,
+}: MembershipComparingPlansSectionProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const isCanadaLocale = locale === "ca";
+
+  return (
+    <div className="w-full">
+      <Collapse
+        isOpen={isOpen}
+        onOpenChange={setIsOpen}
+        title={<Title level="h2">Compare plans</Title>}
+      >
+        <MembershipComparingPlanTable
+          actions={getActionButtons()}
+          columns={getMembershipTitles()}
+          rows={MEMBERSHIP_COMPARE_PLANS}
+        />
+      </Collapse>
+    </div>
+  );
+
+  function getMembershipTitles() {
+    return isCanadaLocale ? CA_MEMBERSHIP_PLANS : US_MEMBERSHIP_PLANS;
+  }
+
+  function getActionButtons() {
+    return isCanadaLocale
+      ? MEMBERSHIP_COMPARING_PLANS_BUTTONS.slice(1)
+      : MEMBERSHIP_COMPARING_PLANS_BUTTONS;
+  }
+};
