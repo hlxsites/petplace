5747536693c40aa2f16d69e2090979f3430e3376
Mateus Pereira <mateusp.s@outlook.com>
2024-08-27 16:28:25 -0300

    test: improved CheckoutConclusionModal

--- react-client/src/components/Membership/CheckoutConclusionModal.test.tsx
+++ react-client/src/components/Membership/CheckoutConclusionModal.test.tsx
@@ -1,12 +1,14 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { ComponentProps } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 import { CheckoutConclusionModal } from "./CheckoutConclusionModal";
 
-const { getByText, getByRole } = screen;
+const { getByText, getByRole, queryByText } = screen;
 
 const DEFAULT_ID = "test-id";
+const PET_PROFILE = "Pet Profile Page";
 
 describe("CheckoutConclusionModal", () => {
   it("should render correct title", () => {
@@ -18,7 +20,7 @@ describe("CheckoutConclusionModal", () => {
     ).toBeInTheDocument();
   });
 
-  it("should render correct title", () => {
+  it("should render correct description text", () => {
     getRenderer();
     expect(
       getByText(
@@ -27,8 +29,16 @@ describe("CheckoutConclusionModal", () => {
     ).toBeInTheDocument();
   });
 
+  it("should navigate to pets id", async () => {
+    getRenderer();
+    expect(queryByText("Pet Profile Page")).not.toBeInTheDocument();
+
+    await userEvent.click(getByRole("button", { name: "Back to pet profile" }));
+    expect(getByText("Pet Profile Page")).toBeInTheDocument();
+  });
+
   it.each(["Back to pet profile", "See my benefits"])(
-    "should render correct title",
+    "should render correct buttons",
     (expected) => {
       getRenderer();
       expect(getByRole("button", { name: expected })).toBeInTheDocument();
@@ -39,9 +49,20 @@ describe("CheckoutConclusionModal", () => {
 function getRenderer({
   petId = DEFAULT_ID,
 }: Partial<ComponentProps<typeof CheckoutConclusionModal>> = {}) {
-  return render(
-    <BrowserRouter>
-      <CheckoutConclusionModal petId={petId} />
-    </BrowserRouter>
-  );
+  return {
+    ...render(
+      <MemoryRouter initialEntries={[`/checkout/${petId}`]}>
+        <Routes>
+          <Route
+            path={`/checkout/:petId`}
+            element={<CheckoutConclusionModal petId={petId} />}
+          />
+          <Route
+            path={`/my-pets/${DEFAULT_ID}`}
+            element={<div>{PET_PROFILE}</div>}
+          />
+        </Routes>
+      </MemoryRouter>
+    ),
+  };
 }
