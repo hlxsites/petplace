c946d6a1a8ba993739ddc572859cf8d372d4c576
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-08-29 13:35:01 -0300

    feat: create the changePassword component

---
+++ react-client/src/components/MyAccount/ChangePassword.tsx
@@ -0,0 +1,20 @@
+import { Button, Card, Text, Title } from "../design-system";
+
+type ChangePasswordProps = {
+  onChangePassword?: () => void;
+};
+
+export const ChangePassword = ({ onChangePassword }: ChangePasswordProps) => {
+  return (
+    <Card role="region">
+      <div className="grid grid-cols-1 items-center justify-between gap-xlarge p-xlarge lg:flex">
+        <div className="grid gap-small">
+          <Title level="h3">Change password</Title>
+          <Text size="16">Create new account password</Text>
+        </div>
+
+        <Button onClick={onChangePassword}>Change password</Button>
+      </div>
+    </Card>
+  );
+};
