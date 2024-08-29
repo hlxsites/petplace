54dfbea011599160044606a4dfe9a86c4cd0dfa0
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-08-29 17:25:19 -0300

    fix: remove unnecessary call of hook on the project

--- react-client/src/routes/account/AccountIndex.tsx
+++ react-client/src/routes/account/AccountIndex.tsx
@@ -1,14 +1,10 @@
 import { Header } from "~/components/design-system/header/Header";
 import { Layout } from "~/components/design-system/layout/Layout";
-import { useAccountIndexViewModel } from "./useAccountIndexViewModel";
 
 export const AccountIndex = () => {
-  const { accountInfo } = useAccountIndexViewModel();
-
   return (
     <Layout>
       <Header pageTitle="My Account" />
-      {accountInfo}
     </Layout>
   );
 };
