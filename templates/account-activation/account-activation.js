20b2ec8f1dd8f5199147082433b97299d2b98002
Darlan Tódero ten Caten <darlantc@me.com>
2024-08-30 11:34:32 -0300

    feat: removed no-markup template in favor of account-activation template

--- templates/account-activation/account-activation.js
+++ templates/account-activation/account-activation.js
@@ -1,6 +1,8 @@
 import { getDefaultMsalInstance } from '../../scripts/lib/msal/msal-instance.js';
 
 function parseJwt(token) {
+  if (!token) return null;
+
   const base64Url = token.split('.')[1];
   const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
   const jsonPayload = decodeURIComponent(window.atob(base64).split('').map((c) => `%${(`00${c.charCodeAt(0).toString(16)}`).slice(-2)}`).join(''));
@@ -8,22 +10,28 @@ function parseJwt(token) {
   return JSON.parse(jsonPayload);
 }
 
-export default async function decorate(block) {
-  block.innerHTML = 'Please wait...';
+export async function loadEager(document) {
+  const main = document.querySelector('main');
+
+  // cleanup body html
+  document.body.innerHTML = '';
+  document.body.appendChild(main);
 
   const searchParams = new URLSearchParams(window.location.search);
   const token = searchParams.get('token');
 
   const claim = parseJwt(token);
-  const { email } = claim;
+  if (claim) {
+    const { email } = claim;
 
-  const msalInstance = await getDefaultMsalInstance();
-  const loginRequest = {
-    extraQueryParameters: {
-      id_token_hint: token,
-      login_hint: email,
-      source: 'petplace-account-activation',
-    },
-  };
-  msalInstance.loginRedirect(loginRequest);
+    const msalInstance = await getDefaultMsalInstance();
+    const loginRequest = {
+      extraQueryParameters: {
+        id_token_hint: token,
+        login_hint: email,
+        source: 'petplace-account-activation',
+      },
+    };
+    msalInstance.loginRedirect(loginRequest);
+  }
 }
