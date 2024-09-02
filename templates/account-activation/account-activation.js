c78c278201b3a13f1e0764d1e472b40db333f773
Darlan Tódero ten Caten <darlantc@me.com>
2024-09-02 10:37:02 -0300

    feat: improved error handling on account-activation template

--- templates/account-activation/account-activation.js
+++ templates/account-activation/account-activation.js
@@ -4,11 +4,15 @@ import { getDefaultMsalInstance } from '../../scripts/lib/msal/msal-instance.js'
 function parseJwt(token) {
   if (!token) return null;
 
-  const base64Url = token.split('.')[1];
-  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  const jsonPayload = decodeURIComponent(window.atob(base64).split('').map((c) => `%${(`00${c.charCodeAt(0).toString(16)}`).slice(-2)}`).join(''));
+  try {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(window.atob(base64).split('').map((c) => `%${(`00${c.charCodeAt(0).toString(16)}`).slice(-2)}`).join(''));
 
-  return JSON.parse(jsonPayload);
+    return JSON.parse(jsonPayload);
+  } catch (_) {
+    return null;
+  }
 }
 
 export async function loadEager(document) {
