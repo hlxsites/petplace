6b34f823acfa05834ed0e41b33764f73e06a87f1
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-09-02 13:18:12 -0300

    feat: create the AccountDetailsTabContent component and cover its usage with unit tests

---
+++ react-client/src/routes/account/components/AccountDetailsTabContent.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen, within } from "@testing-library/react";
+import { AccountDetailsTabContent } from "./AccountDetailsTabContent";
+
+const { getByRole } = screen;
+
+describe("AccountDetailsTabContent", () => {
+  it("should render card with 'region' role to ensure accessibility", () => {
+    getRenderer();
+    expect(getByRole("region")).toBeInTheDocument();
+  });
+
+  it("should render card with expected title", () => {
+    getRenderer();
+    expect(
+      within(getByRole("region")).getByRole("heading", {
+        name: /Contact info/i,
+      })
+    ).toBeInTheDocument();
+  });
+});
+
+function getRenderer() {
+  return render(<AccountDetailsTabContent />);
+}
