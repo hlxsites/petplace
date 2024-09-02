d4f6ee9a98955f36601abe7f639b3fb827b317dc
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-09-02 17:01:08 -0300

    feat: add sections on the paymentInformationTabContent

--- react-client/src/routes/account/components/PaymentInformationTabContent.tsx
+++ react-client/src/routes/account/components/PaymentInformationTabContent.tsx
@@ -1,9 +1,30 @@
 import { Title } from "~/components/design-system";
+import { BasicActionCard } from "~/components/design-system/card/BasicActionCard";
 
 export const PaymentInformationTabContent = () => {
+  const actionsSections = [
+    {
+      buttonLabel: "Manage payment settings",
+      message: "Verify, update, or change your payment settings.",
+      title: "Payment settings",
+    },
+    {
+      buttonLabel: "Submit a claim",
+      message: "Direct link to your claim submissions on mypethealth.com.",
+      title: "Submit a claim",
+    },
+  ];
   return (
     <div className="grid gap-large">
       <Title level="h3">Payment information</Title>
+      {actionsSections.map(({ buttonLabel, ...rest }) => (
+        <BasicActionCard
+          buttonLabel={buttonLabel}
+          buttonProps={{ variant: "secondary" }}
+          key={buttonLabel}
+          {...rest}
+        />
+      ))}
     </div>
   );
 };
