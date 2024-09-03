ece853a4462e759cdd1d216daca050d101377a70
Mateus Pereira <mateusp.s@outlook.com>
2024-09-03 00:02:40 -0300

    feat: add checkbox variant

--- react-client/src/components/design-system/checkbox/Checkbox.tsx
+++ react-client/src/components/design-system/checkbox/Checkbox.tsx
@@ -8,6 +8,8 @@ import {
 } from "../form/types/formTypes";
 import { Icon } from "../icon/Icon";
 
+type CheckboxVariant = "orange" | "purple";
+
 type CheckboxGroupType =
   InputWithoutFormBuilderProps<ElementInputCheckboxGroup>;
 
@@ -15,16 +17,19 @@ type CheckboxProps = Omit<
   Omit<ComponentPropsWithoutRef<typeof RadixCheckbox.Root>, "onChange">,
   "children"
 > &
-  Omit<CheckboxGroupType, "value" | "options" | "onChange">;
+  Omit<CheckboxGroupType, "value" | "options" | "onChange"> & {
+    variant?: CheckboxVariant;
+  };
 
 export const Checkbox = forwardRef<
   ElementRef<typeof RadixCheckbox.Root>,
   CheckboxProps
->(({ className, hideLabel, label, ...props }, ref) => {
+>(({ className, hideLabel, label, variant = "orange", ...props }, ref) => {
+  const { indicatorClassName, rootClassName } = useCheckboxVariant(variant);
   const labelElement = (() => {
     if (hideLabel) return null;
     return (
-      <label className="font-franklin text-sm" htmlFor={props.id}>
+      <label className="text-sm font-franklin" htmlFor={props.id}>
         {label}
       </label>
     );
@@ -34,18 +39,32 @@ export const Checkbox = forwardRef<
     <div className="grid grid-cols-[auto,_1fr] items-center gap-small">
       <RadixCheckbox.Root
         aria-label={hideLabel ? label : undefined}
-        className={classNames(
-          "flex h-5 w-5 items-center justify-center rounded-md border-solid border-[#6E6D73] bg-white p-0 focus:outline-none",
-          className
-        )}
+        className={classNames(rootClassName, className)}
         ref={ref}
         {...props}
       >
-        <RadixCheckbox.Indicator className="absolute flex h-5 w-5 items-center justify-center rounded-md bg-[#C74D3F] text-white">
-          <Icon display="check" />
+        <RadixCheckbox.Indicator className={indicatorClassName}>
+          <Icon display="checkSolo" />
         </RadixCheckbox.Indicator>
       </RadixCheckbox.Root>
       {labelElement}
     </div>
   );
 });
+
+function useCheckboxVariant(variant: CheckboxVariant) {
+  const rootClassName = classNames(
+    "flex h-5 w-5 items-center justify-center rounded-md border-solid border-[#6E6D73] bg-white p-0 focus:outline-none",
+    { "focus:bg-purple-300 hover:bg-purple-300": variant === "purple" }
+  );
+
+  const indicatorClassName = classNames(
+    "absolute flex h-5 w-5 items-center justify-center rounded-md text-white",
+    {
+      "bg-orange-300-contrast": variant === "orange",
+      "bg-purple-300": variant === "purple",
+    }
+  );
+
+  return { indicatorClassName, rootClassName };
+}
