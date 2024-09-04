53c3d27e8bbe4fd9fb8bb67f264c2646e7fd1482
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-09-03 23:22:38 -0300

    feat: add unit tests to cover the notificationsTabContent

--- react-client/src/routes/account/components/NotificationsTabContent.test.tsx
+++ react-client/src/routes/account/components/NotificationsTabContent.test.tsx
@@ -1,5 +1,6 @@
-import { render, screen, within } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { NotificationsTabContent } from "./NotificationsTabContent";
+import { userEvent } from "@testing-library/user-event";
 
 const { getByRole } = screen;
 
@@ -11,18 +12,39 @@ describe("NotificationsTabContent", () => {
     ).toBeInTheDocument();
   });
 
-  it("should render card with 'region' role to ensure accessibility", () => {
-    getRenderer();
-    expect(getByRole("region")).toBeInTheDocument();
+  it("should render expected form 'account-notifications-form'", () => {
+    const { container } = getRenderer();
+    expect(
+      container.querySelector("form[id='account-notifications-form']")
+    ).toBeInTheDocument();
   });
 
-  it("should render card with expected title", () => {
-    getRenderer();
+  it("should render the 'Offers and Resources' section", () => {
+    const { container } = getRenderer();
     expect(
-      within(getByRole("region")).getByRole("heading", {
-        name: /Offers and Resources/i,
-      })
+      container.querySelector("section[aria-label='Offers and Resources']")
     ).toBeInTheDocument();
+  });
+
+  it("should render the 'Notifications' section", () => {
+    const { container } = getRenderer();
+    expect(
+      container.querySelector("section[aria-label='Notifications']")
+    ).toBeInTheDocument();
+  });
+
+  it("should render the save button disabled when no value in the form is changed", () => {
+    getRenderer();
+
+    expect(getByRole("button", { name: "Save changes" })).toBeDisabled();
+  });
+
+  it("should render the save button enabled when a value in the form is changed", async () => {
+    getRenderer();
+
+    expect(getByRole("button", { name: "Save changes" })).toBeDisabled();
+    await userEvent.click(getByRole("checkbox", { name: "Cat" }));
+    expect(getByRole("button", { name: "Save changes" })).toBeEnabled();
   });
 });
 
