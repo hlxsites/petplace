2a63d5373e40163cd447f1f12a0a47b81eefddea
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-09-05 14:54:50 -0300

    feat: Update useAccountViewModel to fetch data from mock

--- react-client/src/routes/account/AccountIndex.tsx
+++ react-client/src/routes/account/AccountIndex.tsx
@@ -1,12 +1,12 @@
 import { Header } from "~/components/design-system/header/Header";
 import { Layout } from "~/components/design-system/layout/Layout";
-import { MyAccountInfoSection } from "~/components/MyAccount/section/MyAccountInfoSection";
+import { MyAccountInfoTabs } from "./components/MyAccountInfoTabs";
 
 export const AccountIndex = () => {
   return (
     <Layout>
       <Header pageTitle="My Account" />
-      <MyAccountInfoSection />
+      <MyAccountInfoTabs />
     </Layout>
   );
 };
--- react-client/src/routes/account/useAccountIndexViewModel.ts
+++ react-client/src/routes/account/useAccountIndexViewModel.ts
@@ -1,16 +1,28 @@
-import { LoaderFunction, useLoaderData } from "react-router-dom";
+import {
+  LoaderFunction,
+  useLoaderData,
+  useOutletContext,
+} from "react-router-dom";
+import { getAuthLogin, getLostPetsHistory } from "~/mocks/MockRestApiServer";
 import { LoaderData } from "~/types/LoaderData";
 
 export const loader = (() => {
   return {
-    accountInfo: [],
+    isExternalLogin: getAuthLogin(),
+    lostPetsHistory: getLostPetsHistory(),
   };
 }) satisfies LoaderFunction;
 
 export const useAccountIndexViewModel = () => {
-  const { accountInfo } = useLoaderData() as LoaderData<typeof loader>;
+  const { isExternalLogin, lostPetsHistory } = useLoaderData() as LoaderData<
+    typeof loader
+  >;
 
   return {
-    accountInfo,
+    isExternalLogin,
+    lostPetsHistory,
   };
 };
+
+export const useAccountContext = () =>
+  useOutletContext<ReturnType<typeof useAccountIndexViewModel>>();
