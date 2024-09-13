2b78c6fe0fe56d59b62d972dccf11240914aa326
Mateus Pereira <mateusp.s@outlook.com>
2024-09-13 00:01:36 -0300

    feat: add authUtil for login origin check

---
+++ react-client/src/util/authTypes.ts
@@ -0,0 +1,11 @@
+export type ParsedToken = {
+  extension_EmailOptIn: boolean;
+  extension_SMSOptIn: boolean;
+  postalCode: string;
+  extension_CatNewsletter: boolean;
+  extension_DogNewsletter: boolean;
+  extension_CustRelationId: string;
+  given_name: string;
+  family_name: string;
+  emails: string[];
+};
--- react-client/src/util/authUtil.ts
+++ react-client/src/util/authUtil.ts
@@ -1,3 +1,4 @@
+import { ParsedToken } from "./authTypes";
 import { AUTH_TOKEN } from "./envUtil";
 import { invariantResponse } from "./invariant";
 
@@ -17,4 +18,33 @@ export function requireAuthToken() {
   });
 
   return authToken;
+}
+
+export function parseJwt(token: string) {
+  try {
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    const jsonPayload = decodeURIComponent(
+      window
+        .atob(base64)
+        .split("")
+        .map((c) => `%${`00${c.charCodeAt(0).toString(16)}`.slice(-2)}`)
+        .join("")
+    );
+    const parsedValue = JSON.parse(jsonPayload) as ParsedToken;
+
+    return parsedValue;
+  } catch (e) {
+    console.error("Error parsing token", e);
+  }
+}
+
+export function checkIsExternalLogin() {
+  const parsedJwt = parseJwt(requireAuthToken());
+  if (!parsedJwt) return undefined;
+
+  return (
+    parsedJwt.extension_CustRelationId &&
+    Number(parsedJwt.extension_CustRelationId) !== 0
+  );
 }
