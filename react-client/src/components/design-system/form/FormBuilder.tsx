df594c1ed73722f7f601738d2009d7d93987b634
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-09-12 21:39:49 -0300

    fix: remove unnecessary code that was cause linter issue

--- react-client/src/components/design-system/form/FormBuilder.tsx
+++ react-client/src/components/design-system/form/FormBuilder.tsx
@@ -312,7 +312,6 @@ export const FormBuilder = ({
 
         const { repeaterId, index } = repeaterMetadata;
         const processedValue = (() => {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
           const current = prev[repeaterId];
           if (current && Array.isArray(current)) {
             return (current as FormValues[]).map((item, i) => {
