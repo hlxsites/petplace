db1576d27ee8915a5d526a52a115523881cfdee7
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-09-16 00:00:44 -0300

    feat: update component to accept being rendered with new business rules

--- react-client/src/components/Pet/PetWatchDrawerBody.tsx
+++ react-client/src/components/Pet/PetWatchDrawerBody.tsx
@@ -1,24 +1,49 @@
 import { PetWatchServiceProps } from "~/routes/my-pets/petId/utils/petServiceDetails";
-import { PET_WATCH_OPTIONS } from "~/routes/my-pets/petId/utils/petWatchConstants";
-import { PetCardPetWatch } from "./PetCardPetWatch";
 import { PetServiceDetailsCard } from "./PetServiceDetailsCard";
+import { PetServices } from "~/domain/models/pet/PetModel";
+import { PetWatchServices } from "./PetWatchServices";
+import { shouldRenderStandardServiceDrawer } from "~/util/petWatchServiceUtils";
+import { Button, Text } from "../design-system";
+import { PET_WATCH_ANNUAL_UNAVAILABLE_OPTIONS } from "~/routes/my-pets/petId/utils/petWatchConstants";
+import { PetCardPetWatch } from "./PetCardPetWatch";
 
 type PetWatchDrawerBodyProps = {
   contentDetails?: PetWatchServiceProps;
   onClick: (label?: string) => () => void;
+  serviceStatus: PetServices["membershipStatus"];
 };
 
 export const PetWatchDrawerBody = ({
   contentDetails,
   onClick,
+  serviceStatus,
 }: PetWatchDrawerBodyProps) => {
   if (contentDetails) return <PetServiceDetailsCard {...contentDetails} />;
 
   return (
-    <div className="grid gap-small pt-large">
-      {PET_WATCH_OPTIONS.map(({ id, ...props }) => (
-        <PetCardPetWatch key={props.label} onClick={onClick(id)} {...props} />
-      ))}
+    <div className="grid gap-xlarge">
+      <PetWatchServices onClick={onClick} serviceStatus={serviceStatus} />
+      {shouldRenderStandardServiceDrawer(serviceStatus) ? (
+        <Button fullWidth>Upgrade membership</Button>
+      ) : null}
+      {serviceStatus === "Annual member" && renderAnnualService()}
     </div>
   );
+
+  function renderAnnualService() {
+    return (
+      <div className="grid gap-large">
+        <Text color="tertiary-600">
+          Upgrade your membership to unlock the following benefits:
+        </Text>
+
+        <div className="grid gap-small">
+          {PET_WATCH_ANNUAL_UNAVAILABLE_OPTIONS.map(({ id, ...props }) => (
+            <PetCardPetWatch key={id} onClick={onClick} {...props} />
+          ))}
+        </div>
+        <Button fullWidth>Upgrade membership</Button>
+      </div>
+    );
+  }
 };
