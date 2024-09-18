f9fe6b174f2f7ec5daebf547e8eae447a9fda2f8
Mateus Pereira <mateusp.s@outlook.com>
2024-09-18 18:19:02 -0300

    fix: class name

--- react-client/src/domain/useCases/user/accountDetailsUseCaseFactory.ts
+++ react-client/src/domain/useCases/user/accountDetailsUseCaseFactory.ts
@@ -2,13 +2,13 @@ import { HttpClientRepository } from "~/domain/repository/HttpClientRepository";
 import { GetAccountDetailsRepository } from "~/domain/repository/user/GetAccountDetailsRepository";
 import { ENABLE_MOCK } from "~/util/envUtil";
 import { GetAccountDetailsUseCase } from "./GetAccountDetailsUseCase";
-import { MockGetAccountDetailsCase } from "./MockGetAccountDetailsUseCase";
+import { MockGetAccountDetailsUseCase } from "./MockGetAccountDetailsUseCase";
 
 export default function (
   authToken: string,
   httpClient?: HttpClientRepository
 ): GetAccountDetailsRepository {
-  if (ENABLE_MOCK) return new MockGetAccountDetailsCase();
+  if (ENABLE_MOCK) return new MockGetAccountDetailsUseCase();
 
   return new GetAccountDetailsUseCase(authToken, httpClient);
 }
