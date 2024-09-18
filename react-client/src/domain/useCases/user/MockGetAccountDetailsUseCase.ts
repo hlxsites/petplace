1b7739c6b7359b9b2225d7f511b750251adbe9a4
Mateus Pereira <mateusp.s@outlook.com>
2024-09-18 18:20:00 -0300

    feat: improve mocks

--- react-client/src/domain/useCases/user/MockGetAccountDetailsUseCase.ts
+++ react-client/src/domain/useCases/user/MockGetAccountDetailsUseCase.ts
@@ -1,16 +1,27 @@
 import { AccountDetailsModel } from "~/domain/models/user/UserModels";
 import { GetAccountDetailsRepository } from "../../repository/user/GetAccountDetailsRepository";
 
-export class MockGetAccountDetailsCase implements GetAccountDetailsRepository {
+export class MockGetAccountDetailsUseCase implements GetAccountDetailsRepository {
   async query(): Promise<AccountDetailsModel> {
     // Simulate a delay
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
     return {
-      email: "johene@email.com",
-      name: "Johene",
-      phoneNumber: "(234) 123-4566",
-      surname: "Smith",
+      address: {
+        address1: "Rua da Glória",
+        address2: "",
+        city: "Rio de Janeiro",
+        country: "BR",
+        intersection: "",
+        state: "RJ",
+        zipCode: "012345",
+      },
+      contactConsent: true,
+      email: "e.jones@email.com",
+      informationConsent: true,
+      name: "Jones",
+      defaultPhone: "762-786-2364",
+      surname: "Elliot",
     };
   }
 }
