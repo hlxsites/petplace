f759dd70961f634a9b0cb20f9b3e3dfdcf93f483
Mateus Pereira <mateusp.s@outlook.com>
2024-09-18 18:18:28 -0300

    feat: add external login handler

--- react-client/src/domain/useCases/user/GetAccountDetailsUseCase.ts
+++ react-client/src/domain/useCases/user/GetAccountDetailsUseCase.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { HttpClientRepository } from "~/domain/repository/HttpClientRepository";
-import { AccountDetailsModel } from "../../models/user/UserModels";
+import { checkIsExternalLogin } from "~/util/authUtil";
+import { AccountDetailsModel, ExternalAccountDetailsModel, InternalAccountDetailsModel } from "../../models/user/UserModels";
 import { GetAccountDetailsRepository } from "../../repository/user/GetAccountDetailsRepository";
 import { PetPlaceHttpClientUseCase } from "../PetPlaceHttpClientUseCase";
 
@@ -15,10 +16,20 @@ export class GetAccountDetailsUseCase implements GetAccountDetailsRepository {
     }
   }
 
-  query = async (): Promise<AccountDetailsModel | null> => {
+  async query(): Promise<AccountDetailsModel | null> {
+    const isExternalLogin = checkIsExternalLogin()
+
     try {
-      const result = await this.httpClient.get("adopt/api/UserProfile");
-      if (result.data) return convertToAccountDetailsModel(result.data);
+      let result;
+
+      if (isExternalLogin) {
+        result = await this.httpClient.get("adopt/api/UserProfile");
+        if (result.data) return convertToExternalAccountDetailsModel(result.data);
+
+      } else {
+        result = await this.httpClient.get("adopt/api/User");
+        if (result.data) return convertToInternalAccountDetailsModel(result.data);
+      }
 
       return null;
     } catch (error) {
@@ -28,9 +39,9 @@ export class GetAccountDetailsUseCase implements GetAccountDetailsRepository {
   };
 }
 
-function convertToAccountDetailsModel(
+function convertToInternalAccountDetailsModel(
   data: unknown
-): AccountDetailsModel | null {
+): InternalAccountDetailsModel | null {
   if (!data) return null;
 
   const serverResponseSchema = z.object({
@@ -44,7 +55,7 @@ function convertToAccountDetailsModel(
     const { data, error, success } = serverResponseSchema.safeParse(userData);
     if (success) return data;
 
-    console.error("Error parsing user data", { userData, error });
+    console.error("Error parsing internal user data", { userData, error });
     return null;
   };
 
@@ -55,7 +66,111 @@ function convertToAccountDetailsModel(
   return {
     email: Email ?? "",
     name: FirstName ?? "",
-    phoneNumber: PhoneNumber ?? "",
+    defaultPhone: PhoneNumber ? `${PhoneNumber}|Home` : "",
     surname: LastName ?? "",
   };
+}
+
+
+function convertToExternalAccountDetailsModel(
+  data: unknown
+): ExternalAccountDetailsModel | null {
+  if (!data) return null;
+
+  const serverResponseSchema = z.object({
+    Address: z.object({
+      Unit: z.string().nullish(),
+      Street: z.string().nullish(),
+      Intersection: z.string().nullish(),
+      City: z.string().nullish(),
+      PostalCode: z.string().nullish(),
+      Country: z.string().nullish(),
+      StateProvince: z.string().nullish(),
+    }),
+    BusinessPhone: z.string().nullish(),
+    CellularPhone: z.string().nullish(),
+    ContactConsent: z.boolean(),
+    Email: z.string(),
+    FirstName: z.string(),
+    HomePhone: z.string().nullish(),
+    InformationReleaseConsent: z.boolean(),
+    LastName: z.string(),
+    PhoneNumber: z.string(),
+    PhoneType: z.string(),
+  });
+
+  const parseAddressData = (addressData: unknown) => {
+    const { data, error, success } =
+      serverResponseSchema.safeParse(addressData);
+    if (success) return data;
+
+    console.error("Error parsing external user data", { addressData, error });
+    return null;
+  };
+
+  const address = parseAddressData(data);
+  if (!address) return null;
+
+  const {
+    Address: {
+      City,
+      Country,
+      Intersection,
+      PostalCode,
+      StateProvince,
+      Street,
+      Unit,
+    },
+    BusinessPhone,
+    CellularPhone,
+    ContactConsent,
+    Email,
+    FirstName,
+    HomePhone,
+    InformationReleaseConsent,
+    LastName,
+    PhoneNumber,
+    PhoneType,
+  } = address;
+
+  return {
+    address: {
+      address1: Street ?? "",
+      address2: Unit ?? "",
+      city: City ?? "",
+      country: Country ?? "",
+      intersection: Intersection ?? "",
+      state: StateProvince ?? "",
+      zipCode: PostalCode ?? "",
+    },
+    contactConsent: ContactConsent,
+    email: Email,
+    informationConsent: InformationReleaseConsent,
+    name: FirstName,
+    defaultPhone:
+      PhoneNumber && PhoneType
+        ? `${PhoneNumber}|${managePhoneTypes()}`
+        : "",
+    secondaryPhone: findSecondaryPhoneNumber(),
+    surname: LastName,
+  };
+
+  function managePhoneTypes(){
+    let type: string = PhoneType.replace("Phone", "")
+
+    switch(type) {
+      case "Business":
+        return "Work";
+      case "Cellular":
+        return "Mobile";
+      default: return "Home";
+    }
+  }
+
+  function findSecondaryPhoneNumber(){
+    if (BusinessPhone?.length && BusinessPhone !== PhoneNumber) return `${BusinessPhone}|Work`
+    if (CellularPhone?.length && CellularPhone !== PhoneNumber) return `${CellularPhone}|Mobile`
+    if (HomePhone?.length && HomePhone !== PhoneNumber) return `${HomePhone}|Home`
+    return ""
+  }
 }
