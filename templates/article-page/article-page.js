9c6c6d808949a412f7586d21e8c08d97fa57b5b2
ankyc-phi <146023473+ankyc-phi@users.noreply.github.com>
2024-09-19 10:33:43 -0400

    feat: move TOC in article after hero (#762)
    Jira Ticket:
    https://pethealthinc.atlassian.net/browse/PM-481
    
    Related to: #732
    
    Test URLs:
    - Before:
    https://main--petplace--hlxsites.hlx.page/article/general/pet-insurance/pet-insurance-review
    - After:
    https://pm481-mauricio-toc--petplace--hlxsites.hlx.page/article/general/pet-insurance/pet-insurance-review
    
    Co-authored-by: Mauricio Ochoa <mauricio.ochoa@herodigital.com>

--- templates/article-page/article-page.js
+++ templates/article-page/article-page.js
@@ -22,15 +22,9 @@ function createTableOfContents(main) {
   }
   const tocDiv = document.createElement('div');
   tocDiv.classList.add('toc');
-
-  // if there is a disclosure, add the toc after it, otherwise add it after the h1
-  const title = main.querySelector('h1');
-  const disclosure = main.querySelector('.disclosure');
-  if (title.nextElementSibling === disclosure) {
-    disclosure.after(tocDiv);
-  } else {
-    title.after(tocDiv);
-  }
+  const heroContainer = main.querySelector('.hero-container');
+  // add TOC as last item of the Hero Container
+  heroContainer.append(tocDiv);
 }
 
 async function createTemplateBlock(container, blockName, elems = []) {
