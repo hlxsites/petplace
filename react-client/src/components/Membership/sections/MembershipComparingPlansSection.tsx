99920d860ac8320f112000123533f65ce947d82d
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-09-20 11:54:51 -0300

    feat: update component to accept new prop from view model

--- react-client/src/components/Membership/sections/MembershipComparingPlansSection.tsx
+++ react-client/src/components/Membership/sections/MembershipComparingPlansSection.tsx
@@ -2,14 +2,11 @@ import { useState } from "react";
 import { Collapse, Title } from "~/components/design-system";
 import { useCheckoutIndexViewModel } from "~/routes/checkout/useCheckoutIndexViewModel";
 import { MembershipComparingPlanTable } from "../MembershipComparingPlanTable";
-import {
-  MEMBERSHIP_COMPARE_PLANS,
-  MEMBERSHIP_COMPARING_PLANS_BUTTONS,
-} from "../utils/membershipConstants";
+import { MEMBERSHIP_COMPARE_PLANS } from "../utils/membershipConstants";
 
 export const MembershipComparingPlansSection = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const { availablePlans, isCanadaLocale } = useCheckoutIndexViewModel();
+  const { actionButtons, availablePlans } = useCheckoutIndexViewModel();
 
   return (
     <div className="hidden w-full md:block">
@@ -20,17 +17,11 @@ export const MembershipComparingPlansSection = () => {
         padding="large"
       >
         <MembershipComparingPlanTable
-          actions={getActionButtons()}
+          actions={actionButtons}
           columns={availablePlans}
           rows={MEMBERSHIP_COMPARE_PLANS}
         />
       </Collapse>
     </div>
   );
-
-  function getActionButtons() {
-    return isCanadaLocale
-      ? MEMBERSHIP_COMPARING_PLANS_BUTTONS.slice(1)
-      : MEMBERSHIP_COMPARING_PLANS_BUTTONS;
-  }
 };
