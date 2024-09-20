a9973626b46de43b78624cb12f57982ad0c76111
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-09-20 20:27:35 -0300

    feat: add mock value in the MockGetCheckoutUseCase

--- react-client/src/domain/useCases/checkout/MockGetCheckoutUseCase.ts
+++ react-client/src/domain/useCases/checkout/MockGetCheckoutUseCase.ts
@@ -4,7 +4,26 @@ import { CheckoutResolvedData } from "./GetCheckoutUseCase";
 export class MockGetCheckoutUseCase implements GetCheckoutRepository {
   query = async (): Promise<CheckoutResolvedData | null> => {
     await new Promise((resolve) => setTimeout(resolve, 1000));
-    // TODO: Implement the mock data
-    return null;
+
+    return {
+      actionButtons: [{ label: "Get the best value", isPrimary: true }],
+      availablePlans: ["Annual Protection"],
+      plans: [
+        {
+          buttonLabel: "Get 1 Year Protection",
+          membershipDescriptionOffers: [
+            { offerLabel: "Get help finding your lost pet." },
+            {
+              offerLabel: "Direct connection to your pet's finder.",
+              isNotAvailableOnPlan: true,
+            },
+          ],
+          price: "$1.95",
+          priceInfo: "For the first year, $19.95/year thereafter",
+          subTitle: "Keep Your Pet Safe All Year",
+          title: "Annual Protection",
+        },
+      ],
+    };
   };
 }
