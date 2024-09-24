2e61fcd4d4f1c7531569d96ef13d1b82e600d145
Darlan Tódero ten Caten <darlantc@me.com>
2024-09-24 10:56:52 -0300

    feat: added mocked pet info use case

---
+++ react-client/src/domain/useCases/pet/getPetInfoUseCaseFactory.ts
@@ -0,0 +1,14 @@
+import { HttpClientRepository } from "~/domain/repository/HttpClientRepository";
+import { GetPetInfoRepository } from "~/domain/repository/pet/GetPetInfoRepository";
+import { ENABLE_MOCK } from "~/util/envUtil";
+import { GetPetInfoUseCase } from "./GetPetInfoUseCase";
+import { MockGetPetInfoUseCase } from "./MockGetPetInfoUseCase";
+
+export default function (
+  authToken: string,
+  httpClient?: HttpClientRepository
+): GetPetInfoRepository {
+  if (ENABLE_MOCK) return new MockGetPetInfoUseCase();
+
+  return new GetPetInfoUseCase(authToken, httpClient);
+}
