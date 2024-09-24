fa4a2ca948088dfaebdfe4d2efeabc29d75d2e57
Darlan Tódero ten Caten <darlantc@me.com>
2024-09-24 10:45:34 -0300

    refactor: we're now using images directly from prod site since we host them on the SharePoint now

--- react-client/src/assets/index.ts
+++ react-client/src/assets/index.ts
@@ -1,7 +1,7 @@
 // Images are hosted on SharePoint on the following folder:
-  // https://adobe.sharepoint.com/sites/HelixProjects/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2FHelixProjects%2FShared%20Documents%2Fsites%2Fpetplace%2Fimages%2Freact&viewid=124ad5f1%2D60ba%2D476c%2Dade8%2D7c1df274ee95
+// https://adobe.sharepoint.com/sites/HelixProjects/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2FHelixProjects%2FShared%20Documents%2Fsites%2Fpetplace%2Fimages%2Freact&viewid=124ad5f1%2D60ba%2D476c%2Dade8%2D7c1df274ee95
 
-  // In order to use new images on the project, upload them to the SharePoint folder and update the filename here
+// In order to use new images on the project, upload them to the SharePoint folder and update the filename here
 
 export const ASSET_IMAGES = {
   catAvatar: getImageUrl("cat-avatar.svg"),
@@ -17,6 +17,6 @@ export const ASSET_IMAGES = {
 };
 
 function getImageUrl(filename: string) {
-  const baseURL = `${window.location.origin}/images/react/`;
+  const baseURL = "https://www.petplace.com/images/react/";
   return `${baseURL}${filename}`;
 }
