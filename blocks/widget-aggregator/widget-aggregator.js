2b70bfb139f3ce128d44e7766b4d39dde7734a92
Amol Anand <amol.anand@gmail.com>
2024-09-24 16:58:39 -0400

    testing widget

--- blocks/widget-aggregator/widget-aggregator.js
+++ blocks/widget-aggregator/widget-aggregator.js
@@ -1,61 +1,54 @@
 /* eslint-disable no-undef */
-/* eslint-disable func-names */
-export default async function decorate(block) {
-  const blockMetadata = {};
+import { readBlockConfig } from '../../scripts/lib-franklin.js';
 
-  // eslint-disable-next-line no-restricted-syntax
-  for (const row of block.children) {
-    const key = row.children[0].textContent;
-    const value = row.children[1].innerText;
-    if (key) {
-      blockMetadata[key] = value;
-    }
-  }
-  const { source, script } = blockMetadata;
+export default async function decorate(block) {
+  const config = readBlockConfig(block);
+  const { source, script } = config;
+  console.log('config', JSON.stringify(config));
+  block.innerText = '';
+  const widget = document.createElement('div');
+  widget.setAttribute('id', 'petplace-quote-form');
+  widget.setAttribute('brand', 'petplace');
+  widget.setAttribute('source', source);
+  block.appendChild(widget);
 
   if (script) {
-    block.setAttribute('id', 'petplace-quote-form');
-
-    const widget = document.createElement('script');
-    widget.setAttribute('brand', 'petplace');
-    widget.setAttribute('source', source);
-    widget.text = `
-    (function (doc, tag, id) {
-      let js = doc.getElementsByTagName(tag)[0];
-      if (doc.getElementById(id)) return;
-      js = doc.createElement(tag);
-      js.id = id;
-      js.src = 'https://dev-quote.petted.com/Scripts/lib/widgets/petplace/quote-form/widget.min.js';
-      js.async = true;
-      js.type = "text/javascript";
-      doc.body.appendChild(js);
-    })(document, 'script', 'petplace-quote-engine');
-
-    function waitForQuoteEngine() {
-      setTimeout(() => {
-        if (window.QuoteEngine) {
-          window.QuoteEngine.setOptions({
-            targetId: "petplace-quote-form",
-            redirectUrl: "https://dev-quote.petted.com/quote",
-            baseUrl: "https://dev-quote.petted.com/",
-            urlParam: {
-              source: 'petplace-widget',
-              utm_source: '',
-              utm_medium: '',
-              utm_campaign: '',
-              utm_content: '',
-              utm_term: '',
-            },
-            refCode: 'petplace',
-          });
-          window.QuoteEngine.init();
+    const scriptTag = document.createElement('script');
+    scriptTag.src = script;
+    scriptTag.async = true;
+    scriptTag.setAttribute('brand', 'petplace');
+    scriptTag.setAttribute('source', source);
+    // make code in script to be treated as JavaScript module
+    // scriptTag.type = 'module';
+    scriptTag.onload = () => {
+      console.log('Script loaded successfuly');
+      window.addEventListener('load', () => {
+        if (typeof QuoteEngine === 'undefined') {
+          // Failed to load Quote Form Widget
+          console.error('Failed to load Quote Engine Widget');
+          return;
         }
-      }, 100);
-    }
 
-    waitForQuoteEngine();`;
-
-    block.innerText = '';
-    document.body.append(widget);
+        QuoteEngine.setOptions({
+          targetId: 'petplace-quote-form',
+          redirectUrl: 'https://dev-quote.petted.com/quote',
+          baseUrl: 'https://dev-quote.petted.com/',
+          urlParam: {
+            source: 'petplace-widget',
+            utm_source: '',
+            utm_medium: '',
+            utm_campaign: '',
+            utm_content: '',
+            utm_term: '',
+          },
+          refCode: 'petplace',
+        });
+        QuoteEngine.init();
+      }, false);
+    };
+    scriptTag.onerror = () => {
+      console.log('Error occurred while loading script');
+    };
+    document.body.appendChild(scriptTag);
   }
 }
