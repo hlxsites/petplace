cee3b37e1a5926244cbd89edcada54416c24e81e
Darlan Tódero ten Caten <darlantc@me.com>
2024-09-24 10:56:36 -0300

    refactor: moved mock data close to the use case that will use it

--- react-client/src/mocks/MockRestApiServer.ts
+++ react-client/src/mocks/MockRestApiServer.ts
@@ -1,11 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 
 import { PetDocument } from "~/domain/models/pet/PetDocument";
-import {
-  LostPetUpdate,
-  MissingStatus,
-  PetModel,
-} from "~/domain/models/pet/PetModel";
+import { LostPetUpdate, MissingStatus } from "~/domain/models/pet/PetModel";
+import { PETS_LIST } from "~/domain/useCases/pet/mocks/petsListMock";
 
 const PET_RECORDS = {
   buddy: {
@@ -111,162 +108,6 @@ export type CheckoutProduct = {
   title: string;
   price: string;
 };
-
-const PETS_LIST: PetModel[] = [
-  {
-    age: "Young",
-    breed: "Some Dog",
-    dateOfBirth: "03/05/2024",
-    id: "buddy",
-    img: "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg",
-    isProtected: true,
-    microchip: "1290",
-    missingStatus: "found",
-    mixedBreed: false,
-    name: "Buddy",
-    onboardCompleted: false,
-    sex: "Male",
-    spayedNeutered: false,
-    species: "Dog",
-    documentationStatus: "none",
-    lostPetHistory: [
-      {
-        date: 1722300534,
-        update: 1722354747,
-        status: "missing",
-        id: 1234567,
-        note: "Lost report from submitted",
-      },
-      {
-        date: 628021800000,
-        update: 1722460747,
-        status: "found",
-        id: 2234567,
-        note: "",
-        foundedBy: {
-          finderName: "Erica Wong",
-          contact: [
-            {
-              date: 1722430534,
-              methodContact: "Phone Call",
-              phoneNumber: "289-218-6754",
-            },
-            {
-              date: 1722430534,
-              methodContact: "Text Message",
-              phoneNumber: "289-218-6754",
-            },
-            {
-              date: 1722430534,
-              methodContact: "Email",
-              email: "dana.rayman@pethealthinc.com",
-            },
-          ],
-        },
-      },
-    ],
-  },
-  {
-    age: "Senior",
-    breed: "Orange Cat",
-    dateOfBirth: "07/07/2014",
-    id: "lily",
-    img: "https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg",
-    isProtected: false,
-    microchip: "8645",
-    missingStatus: "missing",
-    mixedBreed: true,
-    name: "Lily",
-    sex: "Female",
-    spayedNeutered: true,
-    species: "Cat",
-    lostPetHistory: [
-      {
-        date: 1722300534,
-        update: 1722354747,
-        status: "missing",
-        id: 1637427,
-        note: "Lost report from submitted",
-      },
-      {
-        date: 1722430534,
-        update: 1722460747,
-        status: "found",
-        id: 2637427,
-        note: "",
-        foundedBy: {
-          finderName: "Erica Wong",
-          contact: [
-            {
-              date: 1722430534,
-              methodContact: "Phone Call",
-              phoneNumber: "289-218-6754",
-            },
-            {
-              date: 1722430534,
-              methodContact: "Text Message",
-              phoneNumber: "289-218-6754",
-            },
-            {
-              date: 1722430534,
-              methodContact: "Email",
-              email: "dana.rayman@pethealthinc.com",
-            },
-          ],
-        },
-      },
-      {
-        date: 1722433434,
-        update: 1722466747,
-        status: "missing",
-        id: 3637427,
-        note: "Lost report from submitted",
-      },
-      {
-        date: 1722430534,
-        update: 1722460747,
-        status: "found",
-        id: 4637427,
-        note: "",
-        foundedBy: null,
-      },
-      {
-        date: 1722433434,
-        update: 1722466747,
-        status: "missing",
-        id: 5637427,
-        note: "Lost report from submitted",
-      },
-      {
-        date: 6,
-        update: 1722460747,
-        status: "found",
-        id: 6637427,
-        note: "",
-      },
-      {
-        date: 7,
-        update: 1722466747,
-        status: "missing",
-        id: 7637427,
-        note: "Lost report from submitted",
-      },
-    ],
-  },
-  {
-    breed: "Flufy dog",
-    id: "charlie",
-    img: "https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2023/07/top-20-small-dog-breeds.jpeg.jpg",
-    isProtected: true,
-    microchip: "3856",
-    missingStatus: "found",
-    name: "Charlie",
-    sex: "Male",
-    spayedNeutered: true,
-    species: "Dog",
-    lostPetHistory: [],
-  },
-];
 
 const BytetagSlide: CheckoutProduct = {
   availableColors: ["black"],
@@ -418,10 +259,6 @@ export const getPetDocuments = ({
   } catch (_) {
     return [];
   }
-};
-
-export const getPetById = (id: string) => {
-  return PETS_LIST.find((pet) => pet.id === id) || PETS_LIST[0];
 };
 
 export const getProductsList = () => {
