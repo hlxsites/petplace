276cfbf3eb2b5500f07fee3e3919300f7e608603
Mateus Pereira <mateusp.s@outlook.com>
2024-09-25 01:06:21 -0300

    refactor: account details types

--- react-client/src/domain/useCases/user/AccountDetailsUseCase.ts
+++ react-client/src/domain/useCases/user/AccountDetailsUseCase.ts
@@ -1,47 +1,24 @@
 import { z } from "zod";
 import { HttpClientRepository } from "~/domain/repository/HttpClientRepository";
 import { checkIsExternalLogin, readJwtClaim } from "~/util/authUtil";
-import { AccountDetailsModel, ExternalAccountDetailsModel, } from "../../models/user/UserModels";
+import {
+  AccountDetailsModel,
+  ExternalAccountDetailsModel,
+} from "../../models/user/UserModels";
 
 import { AccountDetailsRepository } from "~/domain/repository/user/AccountDetailsRepository";
 import { PetPlaceHttpClientUseCase } from "../PetPlaceHttpClientUseCase";
 import { parseData } from "../util/parseData";
-
-const serverInternalAccountSchema = z.object({
-  FirstName: z.string().nullish(),
-  LastName: z.string().nullish(),
-  PhoneNumber: z.string().nullish(),
-  ZipCode: z.string().nullish(),
-});
-
-const serverExternalAccountSchema = z.object({
-  Address: z.object({
-    Unit: z.string().nullish(),
-    Street: z.string().nullish(),
-    City: z.string().nullish(),
-    PostalCode: z.string().nullish(),
-    Country: z.string().nullish(),
-    StateProvince: z.string().nullish()
-  }),
-  Email: z.string().nullish(),
-  FirstName: z.string().nullish(),
-  LastName: z.string().nullish(),
-  ZipCode: z.string().nullish(),
-  PhoneType: z.string().nullish(),
-  PhoneNumber: z.string().nullish(),
-  HomePhone: z.string().nullish(),
-  BusinessPhone: z.string().nullish(),
-  CellularPhone: z.string().nullish(),
-  ContactConsent: z.boolean(),
-  InformationReleaseConsent: z.boolean()
-});
-
-export type PutExternalAccountDetailsRequest = z.infer<typeof serverExternalAccountSchema>;
-export type PutInternalAccountDetailsRequest = z.infer<typeof serverInternalAccountSchema>;
+import {
+  PutExternalAccountDetailsRequest,
+  PutInternalAccountDetailsRequest,
+  serverExternalAccountSchema,
+  serverInternalAccountSchema,
+} from "./userTypesAndSchemas";
 
 export class AccountDetailsUseCase implements AccountDetailsRepository {
   private httpClient: HttpClientRepository;
-  private isInternalLogin = !checkIsExternalLogin()
+  private isInternalLogin = !checkIsExternalLogin();
 
   constructor(authToken: string, httpClient?: HttpClientRepository) {
     if (httpClient) {
@@ -52,24 +29,23 @@ export class AccountDetailsUseCase implements AccountDetailsRepository {
   }
 
   async query(): Promise<AccountDetailsModel | null> {
-
     try {
       let result = await this.httpClient.get("adopt/api/User");
       let accountDetails: AccountDetailsModel | null;
       if (result.data) {
         accountDetails = convertToAccountDetailsModel(result.data);
-        if(this.isInternalLogin) return accountDetails
+        if (this.isInternalLogin) return accountDetails;
 
         // The server doesn't update name and surname for the API of external login,
         // so we use the values from the internal login API
         result = await this.httpClient.get("adopt/api/UserProfile");
-        if (result.data){
+        if (result.data) {
           accountDetails = {
             ...convertToExternalAccountDetailsModel(result.data),
             name: accountDetails?.name,
-            surname: accountDetails?.surname
+            surname: accountDetails?.surname,
           };
-          return accountDetails
+          return accountDetails;
         }
       }
 
@@ -78,11 +54,14 @@ export class AccountDetailsUseCase implements AccountDetailsRepository {
       console.error("AccountDetailsUseCase query error", error);
       return null;
     }
-  };
+  }
 
   mutate = async (data: AccountDetailsModel): Promise<boolean> => {
     const zipCode = readJwtClaim()?.postalCode;
-    let internalAccountBody = convertToServerInternalAccountDetails(data, zipCode);
+    const internalAccountBody = convertToServerInternalAccountDetails(
+      data,
+      zipCode
+    );
 
     try {
       if (serverInternalAccountSchema.safeParse(internalAccountBody).success) {
@@ -92,13 +71,16 @@ export class AccountDetailsUseCase implements AccountDetailsRepository {
 
         if (result.statusCode === 204 && this.isInternalLogin) return true;
 
-        if(!this.isInternalLogin){
-          let externalAccountBody = convertToServerExternalAccountDetails(data as ExternalAccountDetailsModel);
+        if (!this.isInternalLogin) {
+          const externalAccountBody = convertToServerExternalAccountDetails(
+            data as ExternalAccountDetailsModel
+          );
           const result2 = await this.httpClient.put("adopt/api/UserProfile", {
             body: JSON.stringify(externalAccountBody),
           });
 
-          if (result.statusCode === 204 && result2.statusCode === 204) return true;
+          if (result.statusCode === 204 && result2.statusCode === 204)
+            return true;
         }
       }
 
@@ -147,40 +129,11 @@ function convertToServerInternalAccountDetails(
 function convertToExternalAccountDetailsModel(
   data: unknown
 ): ExternalAccountDetailsModel | null {
-  const serverResponseSchema = z.object({
-    Address: z.object({
-      Unit: z.string().nullish(),
-      Street: z.string().nullish(),
-      Intersection: z.string().nullish(),
-      City: z.string().nullish(),
-      PostalCode: z.string().nullish(),
-      Country: z.string().nullish(),
-      StateProvince: z.string().nullish(),
-    }),
-    BusinessPhone: z.string().nullish(),
-    CellularPhone: z.string().nullish(),
-    ContactConsent: z.boolean(),
-    Email: z.string(),
-    FirstName: z.string(),
-    HomePhone: z.string().nullish(),
-    InformationReleaseConsent: z.boolean(),
-    LastName: z.string(),
-    PhoneNumber: z.string(),
-    PhoneType: z.string(),
-  });
-
-  const address = parseData(serverResponseSchema, data);
+  const address = parseData(serverExternalAccountSchema, data);
   if (!address) return null;
 
   const {
-    Address: {
-      City,
-      Country,
-      PostalCode,
-      StateProvince,
-      Street,
-      Unit,
-    },
+    Address: { City, Country, PostalCode, StateProvince, Street, Unit },
     BusinessPhone,
     CellularPhone,
     ContactConsent,
@@ -204,38 +157,40 @@ function convertToExternalAccountDetailsModel(
     },
     contactConsent: ContactConsent,
     defaultPhone:
-      PhoneNumber && PhoneType
-        ? `${PhoneNumber}|${managePhoneTypes()}`
-        : "",
-    email: Email,
+      PhoneNumber && PhoneType ? `${PhoneNumber}|${managePhoneTypes()}` : "",
+    email: Email ?? "",
     informationConsent: InformationReleaseConsent,
-    name: FirstName,
+    name: FirstName ?? "",
     secondaryPhone: findSecondaryPhoneNumber(),
-    surname: LastName,
+    surname: LastName ?? "",
   };
 
-  function managePhoneTypes(){
-    let type: string = PhoneType.replace("Phone", "")
+  function managePhoneTypes() {
+    const type: string = PhoneType?.replace("Phone", "") ?? "";
 
-    switch(type) {
+    switch (type) {
       case "Business":
         return "Work";
       case "Cellular":
         return "Mobile";
-      default: return "Home";
+      default:
+        return "Home";
     }
   }
 
-  function findSecondaryPhoneNumber(){
-    if (BusinessPhone?.length && BusinessPhone !== PhoneNumber) return `${BusinessPhone}|Work`
-    if (CellularPhone?.length && CellularPhone !== PhoneNumber) return `${CellularPhone}|Mobile`
-    if (HomePhone?.length && HomePhone !== PhoneNumber) return `${HomePhone}|Home`
-    return ""
+  function findSecondaryPhoneNumber() {
+    if (BusinessPhone?.length && BusinessPhone !== PhoneNumber)
+      return `${BusinessPhone}|Work`;
+    if (CellularPhone?.length && CellularPhone !== PhoneNumber)
+      return `${CellularPhone}|Mobile`;
+    if (HomePhone?.length && HomePhone !== PhoneNumber)
+      return `${HomePhone}|Home`;
+    return "";
   }
 }
 
 function convertToServerExternalAccountDetails(
-  data: ExternalAccountDetailsModel,
+  data: ExternalAccountDetailsModel
 ): PutExternalAccountDetailsRequest {
   const address = {
     City: data.address?.city,
@@ -244,46 +199,46 @@ function convertToServerExternalAccountDetails(
     StateProvince: data.address?.state,
     Street: data.address?.address1,
     Unit: data.address?.address2,
+  };
 
-  }
+  const [defaultPhone, defaultType] = data.defaultPhone?.split("|") ?? ["", ""];
 
-  const [defaultPhone, defaultType] = data.defaultPhone?.split("|") ?? ["", ""]
-
-
-  function managePhoneTypes(){
+  function managePhoneTypes() {
     let phoneType;
-    switch(defaultType) {
+    switch (defaultType) {
       case "Mobile":
         phoneType = "Cellular";
-        break
+        break;
       case "Work":
         phoneType = "Business";
-        break
-      default: phoneType = "Home";
+        break;
+      default:
+        phoneType = "Home";
     }
 
-    return phoneType.concat("Phone")
+    return phoneType.concat("Phone");
   }
 
-  function manageSecondaryPhone(){
-    const [phone, type] = data.secondaryPhone?.split("|") ?? ["", ""]
+  function manageSecondaryPhone() {
+    const [phone, type] = data.secondaryPhone?.split("|") ?? ["", ""];
     const phones = {
       BusinessPhone: "",
       CellularPhone: "",
-      HomePhone: "",}
+      HomePhone: "",
+    };
 
-      switch(type) {
-        case "Phone":
-          phones.CellularPhone = phone;
-          break
-        case "Work":
-          phones.BusinessPhone = phone;
-          break
-        default:
-          phones.HomePhone = phone;
-      }
+    switch (type) {
+      case "Phone":
+        phones.CellularPhone = phone;
+        break;
+      case "Work":
+        phones.BusinessPhone = phone;
+        break;
+      default:
+        phones.HomePhone = phone;
+    }
 
-      return phones
+    return phones;
   }
 
   return {
@@ -292,6 +247,6 @@ function convertToServerExternalAccountDetails(
     Address: address,
     ContactConsent: data.contactConsent ?? false,
     InformationReleaseConsent: data.informationConsent ?? false,
-    ...manageSecondaryPhone()
+    ...manageSecondaryPhone(),
   };
 }
---
+++ react-client/src/domain/useCases/user/userTypesAndSchemas.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+export const serverInternalAccountSchema = z.object({
+  FirstName: z.string().nullish(),
+  LastName: z.string().nullish(),
+  PhoneNumber: z.string().nullish(),
+  ZipCode: z.string().nullish(),
+});
+
+export const serverExternalAccountSchema = z.object({
+  Address: z.object({
+    Unit: z.string().nullish(),
+    Street: z.string().nullish(),
+    City: z.string().nullish(),
+    PostalCode: z.string().nullish(),
+    Country: z.string().nullish(),
+    StateProvince: z.string().nullish(),
+  }),
+  Email: z.string().nullish(),
+  FirstName: z.string().nullish(),
+  LastName: z.string().nullish(),
+  ZipCode: z.string().nullish(),
+  PhoneType: z.string().nullish(),
+  PhoneNumber: z.string().nullish(),
+  HomePhone: z.string().nullish(),
+  BusinessPhone: z.string().nullish(),
+  CellularPhone: z.string().nullish(),
+  ContactConsent: z.boolean(),
+  InformationReleaseConsent: z.boolean(),
+});
+
+export type PutExternalAccountDetailsRequest = z.infer<typeof serverExternalAccountSchema>;
+export type PutInternalAccountDetailsRequest = z.infer<typeof serverInternalAccountSchema>;
