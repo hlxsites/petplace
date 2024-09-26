95bb5b761ca3c82d1fe5d8ce1d534504d07876cf
Mateus Pereira <mateusp.s@outlook.com>
2024-09-26 18:43:44 -0300

    feat: add notification models

--- react-client/src/domain/models/user/UserModels.ts
+++ react-client/src/domain/models/user/UserModels.ts
@@ -14,3 +14,29 @@ export type AccountDetailsModel = {
   surname: string;
   zipCode: string;
 };
+
+export type MissingStatus = "missing" | "found";
+
+export type ContactDone = {
+  date?: number;
+  email?: string;
+};
+
+export type FoundedByInfo = {
+  contact?: ContactDone[];
+  finderName?: string;
+  finderOrganization?: string;
+  finderPhoneNumber?: string;
+};
+
+export type LostPetUpdateModel = {
+  date: number;
+  id: string;
+  communicationId: string;
+  note?: string;
+  petId: string;
+  petName: string;
+  status: MissingStatus;
+  update: number;
+  foundedBy?: FoundedByInfo;
+};
