2348fa77b16a8c4591535b4047f18f7358e4b1a3
ankyc-phi <ankit.curchorcar@pethealthinc.com>
2024-09-27 13:33:47 -0400

    changed widget loading method

--- blocks/widget-aggregator/widget-aggregator.js
+++ blocks/widget-aggregator/widget-aggregator.js
@@ -21,19 +21,7 @@ export default async function decorate(block) {
 
     block.append(frame);
   } else if (script === 'widget') {
-    const widget = document.createElement('script');
-    widget.setAttribute('brand', 'petplace');
-    widget.setAttribute('source', source);
-    widget.text = `
-    (function (doc, tag, id) {
-      let js = doc.getElementsByTagName(tag)[0];
-      if (doc.getElementById(id)) return;
-      js = doc.createElement(tag);
-      js.id = id;
-      js.src = 'https://dev-quote.petted.com/Scripts/lib/widgets/petplace/quote-form/widget.min.js';
-      js.async = true;
-      js.type = "text/javascript";
-      js.onload = function() {
+    loadScript('https://dev-quote.petted.com/Scripts/lib/widgets/petplace/quote-form/widget.min.js', { async: true }).then(() => {
       if (window.QuoteEngine) {
         window.QuoteEngine.setOptions({
           targetId: "petplace-quote-form",
@@ -50,12 +38,7 @@ export default async function decorate(block) {
           refCode: 'petplace',
         });
         window.QuoteEngine.init();
-        }
-      };
-      doc.body.appendChild(js);
-    })(document, 'script', 'petplace-quote-engine');`;
-
-    block.innerText = '';
-    document.body.append(widget);
+      }
+    });
   }
 }
