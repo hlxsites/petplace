81ca9bd08f59ede059e0104e321fdf573676b641
Mateus Pereira <mateusp.s@outlook.com>
2024-09-28 04:07:15 -0300

    feat: implement getStatesUseCase

---
+++ react-client/src/domain/useCases/lookup/MockGetStatesUseCase.ts
@@ -0,0 +1,24 @@
+import { GetStatesRepository } from "../../repository/lookup/GetStatesRepository";
+
+export class MockGetStatesUseCase implements GetStatesRepository {
+  async query(): Promise<string[] | []> {
+    // Simulate a delay
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    return [
+      "Alberta",
+      "British Columbia",
+      "Manitoba",
+      "New Brunswick",
+      "Newfoundland and Labrador",
+      "Nova Scotia",
+      "Northwest Territories",
+      "Nunavut",
+      "Ontario",
+      "Prince Edward Island",
+      "Quebec",
+      "Saskatchewan",
+      "Yukon",
+    ];
+  }
+}
