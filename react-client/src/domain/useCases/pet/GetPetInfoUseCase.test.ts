6b398ff12c31377392b38059905d9a48c0c7faa2
Mateus Pereira <mateusp.s@outlook.com>
2024-10-01 15:33:36 -0300

    test: update GetPetInfoUseCase

--- react-client/src/domain/useCases/pet/GetPetInfoUseCase.test.ts
+++ react-client/src/domain/useCases/pet/GetPetInfoUseCase.test.ts
@@ -6,60 +6,100 @@ import getPetInfoMock from "./mocks/getPetInfoMock.json";
 // We don't care about the implementation while running those tests
 jest.mock("../PetPlaceHttpClientUseCase", () => {});
 
-describe("GetPetsInfoUseCase", () => {
-  it("should return null when there is no data", async () => {
-    const httpClient = new MockHttpClient({ data: null });
-    const sut = makeSut(httpClient);
-    const result = await sut.query("petId");
-    expect(result).toBeNull();
-  });
+describe("GetPetInfoUseCase", () => {
+  describe("GET", () => {
+    it("should return null when there is no data", async () => {
+      const httpClient = new MockHttpClient({ data: null });
+      const sut = makeSut(httpClient);
+      const result = await sut.query("petId");
+      expect(result).toBeNull();
+    });
 
-  it("should return the correct pet info", async () => {
-    const httpClient = new MockHttpClient({ data: getPetInfoMock });
-    const sut = makeSut(httpClient);
-    const result = await sut.query("petId");
+    it("should return the correct pet info", async () => {
+      const httpClient = new MockHttpClient({ data: getPetInfoMock });
+      const sut = makeSut(httpClient);
+      const result = await sut.query("petId");
 
-    expect(result).toStrictEqual({
-      age: "2",
-      breed: "Labrador",
-      dateOfBirth: "2020-01-01T00:00:00",
-      id: "A123",
-      img: undefined,
-      locale: "US",
-      membershipStatus: "Annual member",
-      microchip: "123456789",
-      mixedBreed: false,
-      name: "Buddy",
-      policyInsurance: [],
-      products: [],
-      sex: "Male",
-      spayedNeutered: true,
-      sourceType: "PetPoint",
-      species: "Dog",
+      expect(result).toStrictEqual({
+        age: "2",
+        breed: "Labrador",
+        dateOfBirth: "2020-01-01T00:00:00",
+        id: "A123",
+        img: undefined,
+        locale: "US",
+        membershipStatus: "Annual member",
+        microchip: "123456789",
+        mixedBreed: false,
+        name: "Buddy",
+        policyInsurance: [],
+        products: [],
+        sex: "Male",
+        spayedNeutered: true,
+        sourceType: "PetPoint",
+        species: "Dog",
+      });
+    });
+
+    it("should return null when the data doesn't match the schema", async () => {
+      const invalidMockData = {
+        Age: "2",
+        Breed: "Labrador",
+        DateOfBirth: "invalid-date",
+        Id: "A123",
+      };
+      const httpClient = new MockHttpClient({ data: invalidMockData });
+      const sut = makeSut(httpClient);
+      const result = await sut.query("petId");
+
+      expect(result).toBeNull();
+    });
+
+    it("should return null when there is an error", async () => {
+      const httpClient = new MockHttpClient({
+        error: new Error("Error"),
+      });
+      const sut = makeSut(httpClient);
+      const result = await sut.query("petId");
+      expect(result).toBeNull();
     });
   });
 
-  it("should return null when the data doesn't match the schema", async () => {
-    const invalidMockData = {
-      Age: "2",
-      Breed: "Labrador",
-      DateOfBirth: "invalid-date",
-      Id: "A123",
+
+  describe("PUT", () => {
+    const validPetInfo = {
+      Id: "AU234NOUH76",
+      Name: "Zion",
+      Sex: "1",
+      DateOfBirth: "2023-06-28T00:00:00",
+      Neutered: true,
+      SpeciesId: 1,
+      BreedId: 5654345,
+      MixedBreed: false,
     };
-    const httpClient = new MockHttpClient({ data: invalidMockData });
-    const sut = makeSut(httpClient);
-    const result = await sut.query("petId");
 
-    expect(result).toBeNull();
-  });
-
-  it("should return null when there is an error", async () => {
-    const httpClient = new MockHttpClient({
-      error: new Error("Error"),
+    it("should update successfully without data returning", async () => {
+      const httpClient = new MockHttpClient({ statusCode: 204 });
+      const sut = makeSut(httpClient);
+      const result = await sut.mutate(validPetInfo);
+      expect(result).toBe(true);
     });
-    const sut = makeSut(httpClient);
-    const result = await sut.query("petId");
-    expect(result).toBeNull();
+
+    it("should return false when data contains invalid characters", async () => {
+      const invalidDetails = { ...validPetInfo, MixedBreed: "True" };
+      const sut = makeSut();
+      // @ts-expect-error the test aims to stop wrong type values
+      const result = await sut.mutate(invalidDetails);
+      expect(result).toBe(false);
+    });
+
+    it("should handle a server error correctly", async () => {
+      const httpClient = new MockHttpClient({
+        error: new Error("Server error"),
+      });
+      const sut = makeSut(httpClient);
+      const result = await sut.mutate(validPetInfo);
+      expect(result).toBe(false);
+    });
   });
 });
 
