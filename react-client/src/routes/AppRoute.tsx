b37df2b4fbec38e2eb10a369f94c005c9b3f0a44
Mateus Pereira <mateusp.s@outlook.com>
2024-10-02 10:15:57 -0300

    chore: enable shouldRevalidate on every case

--- react-client/src/routes/AppRoute.tsx
+++ react-client/src/routes/AppRoute.tsx
@@ -77,10 +77,7 @@ const routes: PetPlaceRouteObject[] = [
             element: <PetProfileLayout />,
             id: "petProfile",
             loader: PetProfileLayoutLoader,
-            shouldRevalidate: ({ currentParams, nextParams }) => {
-              // We want to revalidate the pet profile layout when the pet ID changes
-              return currentParams.petId !== nextParams.petId;
-            },
+            shouldRevalidate: () => true,
             path: AppRoutePaths.petProfile,
             children: [
               {
