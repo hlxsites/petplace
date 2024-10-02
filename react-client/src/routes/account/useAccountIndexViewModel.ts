f8d992714cc6b94221f395eae8f79e0ddc6afc18
Darlan Tódero ten Caten <darlantc@me.com>
2024-10-02 10:32:50 -0300

    refactor: split useAccountIndexViewModel into another viewModel

--- react-client/src/routes/account/useAccountIndexViewModel.ts
+++ react-client/src/routes/account/useAccountIndexViewModel.ts
@@ -12,7 +12,6 @@ import { CountryModel } from "~/domain/models/lockup/CountryModel";
 import { CountryStateModel } from "~/domain/models/lockup/CountryStateModel";
 import {
   AccountDetailsModel,
-  AccountEmergencyContactModel,
   ExternalAccountDetailsModel,
   InternalAccountDetailsModel,
 } from "~/domain/models/user/UserModels";
@@ -27,11 +26,10 @@ import {
   accountAddressIds,
   accountAgreementsIds,
   baseAccountDetailsIds,
-  emergencyContactFormSchema,
-  emergencyContactIds,
   externalAccountDetailsFormSchema,
   internalAccountDetailsFormSchema,
 } from "./form/accountForms";
+import { useAccountEmergencyContactViewModel } from "./useAccountEmergencyContactViewModel";
 import { AccountRootLoaderData } from "./useAccountRootViewModel";
 
 export const loader = (() => {
@@ -68,28 +66,22 @@ export const useAccountIndexViewModel = () => {
   } = useLoaderData<typeof loader>();
   const accountRootData = useRouteMatchesData<AccountRootLoaderData>("account");
 
-  // TODO: split this view model into smaller parts
+  const emergencyContactsForm = useAccountEmergencyContactViewModel({
+    deleteEmergencyContactMutation,
+    emergencyContactsQuery,
+    isExternalLogin: !!accountRootData?.isExternalLogin,
+    submitEmergencyContactsMutation,
+  });
 
   const [countryStateList, setCountryStateList] = useState<CountryStateModel[]>(
     []
   );
 
   const initialAccountFormValuesRef = useRef<FormValues>({});
-  const originalEmergencyContactsRef = useRef<AccountEmergencyContactModel[]>(
-    []
-  );
-  const initialEmergencyContactsFormValue =
-    getEmergencyContactsInitialFormValue(originalEmergencyContactsRef.current);
 
   const [accountFormValues, setAccountFormValues] = useState<FormValues>({});
-  const [emergencyContactsFormValues, setEmergencyContactsFormValues] =
-    useState<FormValues>({});
   const [isLoadingAccount, setIsLoadingAccount] = useState(true);
-  const [isLoadingEmergencyContacts, setIsLoadingEmergencyContacts] =
-    useState(true);
   const [isSubmittingAccount, setIsSubmittingAccount] = useState(false);
-  const [isSubmittingEmergencyContacts, setIsSubmittingEmergencyContacts] =
-    useState(false);
 
   const isExternalLogin = !!accountRootData?.isExternalLogin;
 
@@ -103,10 +95,6 @@ export const useAccountIndexViewModel = () => {
   const isDirtyAccountForm = !isEqual(
     accountFormValues,
     initialAccountFormValuesRef.current
-  );
-  const isDirtyEmergencyContactsForm = !isEqual(
-    emergencyContactsFormValues,
-    initialEmergencyContactsFormValue
   );
 
   const fetchAccountForm = useCallback(async () => {
@@ -145,18 +133,6 @@ export const useAccountIndexViewModel = () => {
     void fetchAccountForm();
   }, [fetchAccountForm, selectedCountry]);
 
-  const fetchEmergencyContacts = useCallback(async () => {
-    const list = await emergencyContactsQuery;
-    originalEmergencyContactsRef.current = list;
-
-    setEmergencyContactsFormValues(getEmergencyContactsInitialFormValue(list));
-    setIsLoadingEmergencyContacts(false);
-  }, [emergencyContactsQuery]);
-
-  useDeepCompareEffect(() => {
-    void fetchEmergencyContacts();
-  }, [fetchEmergencyContacts]);
-
   useEffect(() => {
     const fetchStatesForCountry = async (countryId: string) => {
       const statesList = await statesQuery(countryId);
@@ -170,44 +146,6 @@ export const useAccountIndexViewModel = () => {
 
   const onChangesAccountFormValues: OnChangeFn = (values) => {
     setAccountFormValues(values);
-  };
-
-  const onChangesEmergencyContactsFormValues: OnChangeFn = (values) => {
-    setEmergencyContactsFormValues(values);
-  };
-
-  const onDeleteEmergencyContact = async (
-    data: AccountEmergencyContactModel
-  ) => {
-    return deleteEmergencyContactMutation(data);
-  };
-
-  const asyncSubmitEmergencyContacts = async (
-    data: AccountEmergencyContactModel[]
-  ) => {
-    setIsSubmittingEmergencyContacts(true);
-
-    // First, delete all existing contacts
-    const deletePromisesList = originalEmergencyContactsRef.current.map(
-      (contact) => onDeleteEmergencyContact(contact)
-    );
-    await Promise.all(deletePromisesList);
-
-    // Then, submit the new list
-    await submitEmergencyContactsMutation(data);
-
-    // Finally, fetch the new list
-    await fetchEmergencyContacts();
-
-    setIsSubmittingEmergencyContacts(false);
-  };
-
-  const onSubmitEmergencyContacts: OnSubmitFn = ({ values }) => {
-    const data = buildAccountEmergencyContactsList(
-      values[emergencyContactIds.repeaterId] as unknown as FormValues[]
-    );
-
-    void asyncSubmitEmergencyContacts(data);
   };
 
   const asyncSubmitAccountDetails = async (values: FormValues) => {
@@ -253,52 +191,10 @@ export const useAccountIndexViewModel = () => {
         stateOptions,
       },
     },
-    emergencyContactsForm: {
-      isDirty: isDirtyEmergencyContactsForm,
-      isLoading: isLoadingEmergencyContacts,
-      isSubmitting: isSubmittingEmergencyContacts,
-      onChange: onChangesEmergencyContactsFormValues,
-      onSubmit: onSubmitEmergencyContacts,
-      schema: emergencyContactFormSchema,
-      values: emergencyContactsFormValues,
-    },
+    emergencyContactsForm,
     isExternalLogin,
   };
 };
-
-function buildAccountEmergencyContactsList(
-  values: FormValues[]
-): AccountEmergencyContactModel[] {
-  const cleanupPhone = (phoneString: string) => {
-    const [phone] = phoneString.split("|");
-    return phone.replace(/[^0-9]/g, "");
-  };
-
-  return values.map((contact) => ({
-    contactId: contact[emergencyContactIds.contactId] as string,
-    email: contact[emergencyContactIds.email] as string,
-    name: contact[emergencyContactIds.name] as string,
-    phoneNumber: cleanupPhone(contact[emergencyContactIds.phone] as string),
-    surname: contact[emergencyContactIds.surname] as string,
-    stagingId: contact[emergencyContactIds.stagingId] as number,
-  }));
-}
-
-function getEmergencyContactsInitialFormValue(
-  emergencyContacts?: AccountEmergencyContactModel[] | null
-) {
-  const contacts: FormValues[] = [];
-  emergencyContacts?.forEach((contact) =>
-    contacts.push({
-      [emergencyContactIds.name]: contact.name,
-      [emergencyContactIds.surname]: contact.surname,
-      [emergencyContactIds.email]: contact.email,
-      [emergencyContactIds.phone]: contact.phoneNumber,
-    })
-  );
-
-  return { [emergencyContactIds.repeaterId]: contacts };
-}
 
 function getAccountDetailsInitialFormValue(
   accountDetails: AccountDetailsModel | null,
