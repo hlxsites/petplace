d17266109c0d68a35d3bfadc305a313f3f2c2950
Darlan Tódero ten Caten <darlantc@me.com>
2024-10-03 10:48:08 -0300

    refactor: enhanced renew membership mock data

--- react-client/src/domain/useCases/renew/MockPostRenewMembershipUseCase.ts
+++ react-client/src/domain/useCases/renew/MockPostRenewMembershipUseCase.ts
@@ -1,13 +1,14 @@
 import { PostRenewMembershipRepository } from "~/domain/repository/renew/PostRenewMembershipRepository";
-import { RenewMembershipModel } from "~/domain/models/renew/RenewMembershipModel";
 
 export class MockPostRenewMembershipUseCase
   implements PostRenewMembershipRepository
 {
-  async post(data: RenewMembershipModel): Promise<boolean> {
+  private cache = false;
+  async post(): Promise<boolean> {
     // Simulate a delay
     await new Promise((resolve) => setTimeout(resolve, 1000));
-    data;
-    return true;
+
+    this.cache = !this.cache;
+    return this.cache;
   }
 }
