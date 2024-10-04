9106953b55ccec95fb855c1cffd4cdc4580d0943
Mateus Pereira <mateusp.s@outlook.com>
2024-10-04 17:17:12 -0300

    fix: resolve conflicts

--- react-client/src/domain/useCases/user/MockAccountEmergencyContactsUseCase.ts
+++ react-client/src/domain/useCases/user/MockAccountEmergencyContactsUseCase.ts
@@ -20,19 +20,11 @@ export class MockAccountEmergencyContactUseCase
         phoneNumber: "(234) 123-4566",
         stagingId: 0,
         surname: "Thunder",
-        stagingId: 0,
       },
     ];
   }
 
   async mutate(): Promise<boolean> {
-    // Simulate a delay
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    return true;
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async delete(_data: AccountEmergencyContactModel): Promise<boolean> {
     // Simulate a delay
     await new Promise((resolve) => setTimeout(resolve, 1000));
     return true;
