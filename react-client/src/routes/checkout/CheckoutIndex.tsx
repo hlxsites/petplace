51568c9381a236d89f16e50b5fcb95e244399c9b
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-10-08 08:47:55 -0300

    feat: use petInfoUseCase to get pet's name that will be used on the checkout landing page

--- react-client/src/routes/checkout/CheckoutIndex.tsx
+++ react-client/src/routes/checkout/CheckoutIndex.tsx
@@ -5,9 +5,12 @@ import { CheckoutInfoSection } from "~/components/Membership/sections/CheckoutIn
 import { MembershipComparingPlansSection } from "~/components/Membership/sections/MembershipComparingPlansSection";
 import { MembershipHeader } from "~/components/Membership/sections/MembershipHeader";
 import { MembershipOfferSection } from "~/components/Membership/sections/MembershipOfferSection";
+import { useCheckoutIndexViewModel } from "./useCheckoutIndexViewModel";
 
 export const CheckoutIndex = () => {
+  const { petName } = useCheckoutIndexViewModel();
   const { pathname } = useLocation();
+
   const isLandingPage = pathname === "/checkout";
 
   return (
@@ -15,7 +18,7 @@ export const CheckoutIndex = () => {
       <CheckoutHeader />
       <main className="m-auto w-full px-base py-[80px] xl:w-[1080px] xl:px-0">
         <div className="grid place-items-center gap-xxxxxlarge">
-          <MembershipHeader petName="test" />
+          <MembershipHeader petName={petName ?? "your pet"} />
           <MembershipOfferSection />
           <MembershipComparingPlansSection />
           <CheckoutInfoSection isLandingPage={isLandingPage} />
