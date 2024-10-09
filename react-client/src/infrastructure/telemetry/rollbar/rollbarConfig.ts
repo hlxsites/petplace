0806f817a93a71070c227010d6a8036ef6fdaad4
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-10-09 18:47:11 -0300

    feat: improve rollbar config to log when is also staging and define environment accordingly

--- react-client/src/infrastructure/telemetry/rollbar/rollbarConfig.ts
+++ react-client/src/infrastructure/telemetry/rollbar/rollbarConfig.ts
@@ -1,11 +1,20 @@
 import { Configuration } from "rollbar";
-import { APP_VERSION, IS_PROD_URL, ROLLBAR_TOKEN } from "~/util/envUtil";
+import {
+  APP_VERSION,
+  IS_PROD_URL,
+  IS_STG_URL,
+  ROLLBAR_TOKEN,
+} from "~/util/envUtil";
 
 export const ROLLBAR_CONFIG: Configuration = {
   accessToken: ROLLBAR_TOKEN,
   captureUncaught: true,
   captureUnhandledRejections: true,
   codeVersion: APP_VERSION,
-  enabled: IS_PROD_URL,
-  environment: IS_PROD_URL ? "production" : "development",
+  enabled: IS_PROD_URL || IS_STG_URL,
+  environment: IS_PROD_URL
+    ? "production"
+    : IS_STG_URL
+      ? "staging"
+      : "development",
 };
