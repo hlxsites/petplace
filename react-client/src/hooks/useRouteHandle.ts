9422dcdafd1daeaaf72f325cf65297c5076bc172
Darlan Tódero ten Caten <darlantc@me.com>
2024-10-08 22:07:53 -0300

    fix: useRouteHandle hook

--- react-client/src/hooks/useRouteHandle.ts
+++ react-client/src/hooks/useRouteHandle.ts
@@ -1,13 +1,13 @@
 import { useMatches } from "react-router-dom";
 
-export const useRouteHandle = (
-  paramKey: string
-) => {
+export const useRouteHandle = (paramKey: string) => {
   const matches = useMatches();
 
   // We want to find the last match with a handle
   const match = matches.reverse().find((m) => !!m.handle);
+  if (!match) return null;
 
   // @ts-expect-error - Find a way to type it
-  return match.handle?.[paramKey] || null
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return match?.handle?.[paramKey] || null;
 };
