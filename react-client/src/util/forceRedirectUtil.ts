3718b980441dd54f6426b6798a43b737f1c768e9
Darlan Tódero ten Caten <darlantc@me.com>
2024-10-10 10:03:34 -0300

    refactor: changed redirect to lost pet URL

--- react-client/src/util/forceRedirectUtil.ts
+++ react-client/src/util/forceRedirectUtil.ts
@@ -2,7 +2,7 @@ import { redirectToMph } from "./mphRedirectUtil";
 
 export function redirectToLostPet() {
   // TODO: this is a temporary implementation for the first public release
-  const url = redirectToMph("petplace/auth/report/pet");
+  const url = redirectToMph("auth/report/pet");
   window.open(url);
 }
 
