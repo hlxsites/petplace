e09b58d050471204c16e05075a5ad55e4100f75f
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-10-11 10:15:12 -0300

    fix: adjust route to encode the path

--- react-client/src/util/mphRedirectUtil.ts
+++ react-client/src/util/mphRedirectUtil.ts
@@ -1,7 +1,5 @@
 import { PETPLACE_MPH_BASE_URL } from "./envUtil";
 
 export function redirectToMph(path: string) {
-  return encodeURI(
-    `${PETPLACE_MPH_BASE_URL}/external/petplacelogin?redirecturl=${path}`
-  );
+  return `${PETPLACE_MPH_BASE_URL}/external/petplacelogin?redirecturl=${encodeURIComponent(path)}`;
 }
