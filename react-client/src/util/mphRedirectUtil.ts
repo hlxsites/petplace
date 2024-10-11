2201cdf7bbdcd536e41b34d3678de011b8389d7f
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-10-11 09:46:54 -0300

    feat: make uri encoded

--- react-client/src/util/mphRedirectUtil.ts
+++ react-client/src/util/mphRedirectUtil.ts
@@ -1,5 +1,7 @@
 import { PETPLACE_MPH_BASE_URL } from "./envUtil";
 
 export function redirectToMph(path: string) {
-  return `${PETPLACE_MPH_BASE_URL}/external/petplacelogin?redirecturl=${path}`;
+  return encodeURI(
+    `${PETPLACE_MPH_BASE_URL}/external/petplacelogin?redirecturl=${path}`
+  );
 }
