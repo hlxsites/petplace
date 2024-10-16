bbb9bf00607a208ff141b119151bc4e837e08e1d
Juliana Ribeiro <juribeiro.ru@gmail.com>
2024-10-16 15:42:39 -0300

    feat: improve downloadFile function to handle external links and add cleanup step

--- react-client/src/util/downloadFunctions.ts
+++ react-client/src/util/downloadFunctions.ts
@@ -2,28 +2,41 @@ import { logError } from "~/infrastructure/telemetry/logUtils";
 
 export type DownloadFileProps = {
   blob?: Blob;
+  url?: string;
   fileName: string;
   fileType?: string;
 };
 
-export function downloadFile({ blob, fileName, fileType }: DownloadFileProps) {
-  if (!blob || !fileType) return;
+export function downloadFile({
+  blob,
+  url,
+  fileName,
+  fileType,
+}: DownloadFileProps) {
+  if (!blob && !url) return;
 
   try {
-    const url = URL.createObjectURL(blob);
+    let downloadUrl = url;
+
+    if (blob) {
+      if (!fileType) return;
+      downloadUrl = URL.createObjectURL(blob);
+    }
 
     const downloadLink = document.createElement("a");
     downloadLink.style.display = "none";
-    downloadLink.href = url;
-    downloadLink.download = `${fileName ?? "sample"}`;
+    downloadLink.href = downloadUrl!;
+    downloadLink.download = fileName;
 
     // for firefox browsers
     document.body.appendChild(downloadLink);
-
     downloadLink.click();
-
     document.body.removeChild(downloadLink);
-    URL.revokeObjectURL(url);
+
+    // Clean up the object URL if it was created
+    if (blob && downloadUrl) {
+      URL.revokeObjectURL(downloadUrl);
+    }
   } catch (error) {
     logError("Error downloading the file: ", error);
   }
